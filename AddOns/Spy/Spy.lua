dac6652e5b65ace0547a387b2b08b6a6f99dddbf
class2u <class2u@gmail.com>
2023-12-22 12:17:09 +0800

    Spy: Update to 3.10.8

--- AddOns/Spy/Spy.lua
+++ AddOns/Spy/Spy.lua
@@ -7,7 +7,7 @@ local fonts = SM:List("font")
 local _
 
 Spy = LibStub("AceAddon-3.0"):NewAddon("Spy", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceTimer-3.0")
-Spy.Version = "3.10.6"
+Spy.Version = "3.10.8"
 Spy.DatabaseVersion = "1.1"
 Spy.Signature = "[Spy]"
 Spy.ButtonLimit = 15
