1ac74abd2bd37aa8095cbcbe3e90f53609aab45a
class2u <class2u@gmail.com>
2023-12-23 12:28:40 +0800

    Threat Plates: Update to 11.1.36

--- AddOns/TidyPlates_ThreatPlates/Commands.lua
+++ AddOns/TidyPlates_ThreatPlates/Commands.lua
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------------------------------------
 local L = Addon.ThreatPlates.L
 
-Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "11.1.35"
+Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "11.1.36"
 
 local function toggleDPS()
 	if Addon.db.profile.optionRoleDetectionAutomatic then
