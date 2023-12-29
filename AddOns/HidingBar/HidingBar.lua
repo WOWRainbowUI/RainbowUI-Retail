91c60348ff86db685d909cd8b535c0df3e302e71
class2u <class2u@gmail.com>
2023-12-29 13:18:41 +0800

    更新 小地圖按鈕整合 v10.2.2

--- AddOns/HidingBar/HidingBar.lua
+++ AddOns/HidingBar/HidingBar.lua
@@ -1612,6 +1612,7 @@ do
 	local function CreateAnimationGroup(self, ...)
 		local animationGroup = getmetatable(self).__index.CreateAnimationGroup(self, ...)
 		animationGroup.Play = void
+		animationGroup.Restart = void
 		return animationGroup
 	end
 
