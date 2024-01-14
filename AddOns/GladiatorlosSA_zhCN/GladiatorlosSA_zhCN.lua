aaea46c75824e72dd62efc95c294fd36f3e3e363
class2u <class2u@gmail.com>
2024-01-14 21:38:38 +0800

    更新 敵方技能監控 (語音)-中文語音 2024-1-14

--- AddOns/GladiatorlosSA_zhCN/GladiatorlosSA_zhCN.lua
+++ AddOns/GladiatorlosSA_zhCN/GladiatorlosSA_zhCN.lua
@@ -1,4 +1,5 @@
 ﻿
 local GSA = GladiatorlosSA
 rawset (GSA.GSA_LANGUAGE, "GladiatorlosSA_zhCN\\Voice_zhCN", "國語 (女聲)")
+rawset (GSA.GSA_LOCALEPATH, "zhCN", "GladiatorlosSA_zhCN\\Voice_zhCN")
 rawset (GSA.GSA_LOCALEPATH, "zhTW", "GladiatorlosSA_zhCN\\Voice_zhCN")
