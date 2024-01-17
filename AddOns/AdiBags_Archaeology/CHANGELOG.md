66fb645d9a70d5dfd69a74126eb0c6125acff6f3
class2u <class2u@gmail.com>
2024-01-17 13:43:37 +0800

    Add AdiBags_Archaeology 2.1.1

---
+++ AddOns/AdiBags_Archaeology/CHANGELOG.md
@@ -0,0 +1,6 @@
+# AdiBags - Archaeology
+
+## [2.11](https://github.com/Myrroddin/adibags-archaeology/tree/2.11) (2023-07-11)
+[Full Changelog](https://github.com/Myrroddin/adibags-archaeology/compare/10.1-release2...2.11) [Previous Releases](https://github.com/Myrroddin/adibags-archaeology/releases)
+
+- Bump ToC for patch 10.1.5  
---
+++ AddOns/AdiBags_Archaeology/Localization.lua
@@ -0,0 +1,61 @@
+--[[
+AdiBags_Archaeology - Adds Archaeology items to AdiBags virtual groups
+Copyright © 2023 Paul Vandersypen, All Rights Reserved
+]]--
+
+local _, addon = ...
+
+-- localization table; returns English phrase if translation is not found -----
+-- see https://phanx.net/addons/tutorials/localize for details ----------------
+local L = setmetatable({}, {
+    __index = function(t, k)
+        local v = tostring(k)
+        rawset(t, k, v)
+        return v
+    end
+})
+
+local LOCALE = GetLocale()
+if LOCALE == "enUS" then
+L["Archaeology Items"] = true
+elseif LOCALE == "deDE" then
+L["Archaeology Items"] = "Archäologie-Gegenstände"
+
+elseif LOCALE == "koKR" then
+L["Archaeology Items"] = "고고학 아이템"
+
+elseif LOCALE == "ruRU" then
+L["Archaeology Items"] = "Предметы археологии"
+
+elseif LOCALE == "esES" then
+L["Archaeology Items"] = "Archaeology Items"
+
+elseif LOCALE == "esMX" then
+L["Archaeology Items"] = "Archaeology Items"
+
+elseif LOCALE == "itIT" then
+L["Archaeology Items"] = "Articoli di archeologia"
+
+elseif LOCALE == "ptBR" then
+L["Archaeology Items"] = "Itens de Arqueologia"
+
+elseif LOCALE == "zhTW" then
+L["Archaeology Items"] = "考古學物品"
+
+elseif LOCALE == "zhCN" then
+L["Archaeology Items"] = "考古项目"
+
+elseif LOCALE == "frFR" then
+L["Archaeology Items"] = "Articles d'Archéologie"
+
+end
+
+-- Replace remaining true values by their key
+for k, v in pairs(L) do
+	if v == true then
+		L[k] = k
+	end
+end
+
+-- return localization to files
+addon.L = L
