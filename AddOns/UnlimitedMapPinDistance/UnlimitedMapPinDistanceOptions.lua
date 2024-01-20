3af6b91266dbb69644f7aec29ba004d97126fe6e
class2u <class2u@gmail.com>
2024-01-20 21:15:02 +0800

    Add Unlimited Map Pin Distance 1.2.9

---
+++ AddOns/UnlimitedMapPinDistance/UnlimitedMapPinDistanceOptions.lua
@@ -0,0 +1,194 @@
+local UMPDO
+_UMPD = _UMPD
+
+-- Templates for creating UI Elements
+local function roundVal(v)
+    return floor(v*1000)/1000
+end
+local createCheckbox = function(parent,option,label,desc)
+    local cb = CreateFrame("CheckButton", "CB" .. label, parent, "UICheckButtonTemplate")
+    cb:SetChecked(option)
+    cb.text:SetText(desc)
+    return cb
+end
+local createSlider = function(parent,name,option,minVal,maxVal,step,desc)
+    local slider = CreateFrame("Slider", name, parent, "OptionsSliderTemplate")
+    local editbox = CreateFrame("EditBox", name.."box", slider, "InputBoxTemplate")
+    slider:SetMinMaxValues(minVal,maxVal)
+    slider:SetValueStep(step)
+    slider:SetObeyStepOnDrag(true)
+    slider:SetValue(option)
+    _G[name.."Text"]:SetText(desc)
+    _G[name.."Low"]:SetText(minVal)
+    _G[name.."High"]:SetText(maxVal)
+
+    editbox:ClearAllPoints()
+    editbox:SetSize(50,14)
+    editbox:SetPoint('TOP', slider, 'BOTTOM')
+    editbox:SetFontObject(GameFontHighlightSmall)
+    editbox:SetJustifyH('CENTER')
+    editbox:SetAutoFocus(false)
+    editbox:SetNumeric(true)
+    editbox:SetText(slider:GetValue())
+    editbox:SetCursorPosition(0)
+
+    slider:SetScript("OnValueChanged", function(self,v)
+        self.editbox:SetText(roundVal(v))
+    end)
+    editbox:SetScript("OnTextChanged", function(self)
+        local v = self:GetText()
+        if tonumber(v) then
+            self:GetParent():SetValue(v)
+        end
+    end)
+    editbox:SetScript("OnEnterPressed", function(self)
+        local v = self:GetText()
+        if tonumber(v) then
+            self:GetParent():SetValue(v)
+            self:ClearFocus()
+        end
+    end)
+    slider.editbox = editbox
+    return slider
+end
+-- Draw Line
+local createHeader = function(f,y,h)
+    local t = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+    t:SetText("|cffffd100"..h.."|r")
+    t:SetPoint("TOP",0,y)
+    local l = f:CreateLine()
+    l:SetColorTexture(.2,.2,.2,.9)
+    l:SetStartPoint("TOPLEFT", 8,(y-6))
+    l:SetEndPoint("TOPRIGHT", -8,(y-6))
+    l:SetThickness(2)
+end
+
+-- Init
+function UMPD_Init()
+    if not UMPD then UMPD = {} end
+    UMPD.minDistance = UMPD.minDistance or 70
+    UMPD.fadeDistance = UMPD.fadeDistance or 113
+    UMPD.maxDistance = UMPD.maxDistance or 0
+    UMPD.pinAlphaLong = UMPD.pinAlphaLong or 60
+    UMPD.pinAlphaShort = UMPD.pinAlphaShort or 100
+    UMPD.pinAlphaClamped = UMPD.pinAlphaClamped or 100
+
+    if UMPD.autoTrackPins == nil then
+        UMPD.autoTrackPins = true
+    end
+
+    if UMPD.timeDistance == nil then
+        UMPD.timeDistance = true
+    end
+
+    if UMPD.useMeters == nil then
+        UMPD.useMeters = false
+    end
+
+    if UMPD.shortNumbers == nil then
+        UMPD.shortNumbers = false
+    end
+
+    -- Frame
+    UMPDO = CreateFrame("Frame", nil, UIParent)
+    UMPDO.name = _UMPD.name
+    InterfaceOptions_AddCategory(UMPDO, _UMPD.addonName);
+
+    -- Title
+    local title = UMPDO:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+    title:SetPoint("TOPLEFT", 16, -16)
+    title:SetText(_UMPD.addonName)
+
+    -- Version
+    local version = UMPDO:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+    version:SetPoint("TOPRIGHT", -16, -16)
+    version:SetText("v".._UMPD.version)
+
+    -- Headers
+    createHeader(UMPDO, -59, "Pin Distances")
+    createHeader(UMPDO, -155, "Pin Alphas")
+    createHeader(UMPDO, -241, "Miscellaneous Options")
+    -- createHeader(UMPDO, -453, "Additional Notes")
+    createHeader(UMPDO, -532, "Credits")
+
+    -- Sliders
+    local slMinDist = createSlider(UMPDO,"minDist",UMPD.minDistance,0,10000,1,"Min Pin Distance") 
+    slMinDist:SetPoint("TOPLEFT", 16, -96)
+    slMinDist:HookScript("OnValueChanged", function(self,value)
+        UMPD.minDistance = value
+    end)
+    local slFadeDist = createSlider(UMPDO,"fadeDist",UMPD.fadeDistance,0,10000,1,"Fade Pin Distance") 
+    slFadeDist:SetPoint("TOP", 0, -96)
+    slFadeDist:HookScript("OnValueChanged", function(self,value)
+        UMPD.fadeDistance = value
+    end)
+    local slMaxDist = createSlider(UMPDO,"maxDist",UMPD.maxDistance,0,10000,1,"Max Pin Distance") -- If 0, Unlimited distance
+    slMaxDist:SetPoint("TOPRIGHT", -16, -96)
+    slMaxDist:HookScript("OnValueChanged", function(self,value)
+        UMPD.maxDistance = value
+    end)
+    local slAlphaShort = createSlider(UMPDO,"shortAlpha",UMPD.pinAlphaShort,0,100,1,"Alpha below fade distance")
+    slAlphaShort:SetPoint("TOPLEFT", 16, -192)
+    slAlphaShort:HookScript("OnValueChanged", function(self,value)
+        UMPD.pinAlphaShort = value
+    end)
+    local slAlphaLong = createSlider(UMPDO,"longAlpha",UMPD.pinAlphaLong,0,100,1,"Alpha above fade distance")
+    slAlphaLong:SetPoint("TOP", 0, -192)
+    slAlphaLong:HookScript("OnValueChanged", function(self,value)
+        UMPD.pinAlphaLong = value
+    end)
+    local slAlphaClamped = createSlider(UMPDO,"clampedAlpha",UMPD.pinAlphaLong,0,100,1,"Alpha when clamped to edge")
+    slAlphaClamped:SetPoint("TOPRIGHT", -16, -192)
+    slAlphaClamped:HookScript("OnValueChanged", function(self,value)
+        UMPD.pinAlphaClamped = value
+    end)
+
+    -- Checkboxes
+    local cbTrackPins = createCheckbox(UMPDO,UMPD.autoTrackPins,"atp","Automatically track new pins")
+    cbTrackPins:SetPoint("TOPLEFT", 8, -252)
+    cbTrackPins:HookScript("OnClick", function(self,value)
+        UMPD.autoTrackPins = self:GetChecked()
+    end)
+    local cbShowTime = createCheckbox(UMPDO,UMPD.timeDistance,"st","Show estimated time to arrive")
+    cbShowTime:SetPoint("TOPLEFT", 8, -282)
+    cbShowTime:HookScript("OnClick", function(self,value)
+        UMPD.timeDistance = self:GetChecked()
+    end)
+    local cbUseMeters = createCheckbox(UMPDO,UMPD.useMeters,"st","Use meters instead of yards")
+    cbUseMeters:SetPoint("TOPLEFT", 8, -312)
+    cbUseMeters:HookScript("OnClick", function(self,value)
+        UMPD.useMeters = self:GetChecked()
+    end)
+    local cbShortNumbers = createCheckbox(UMPDO,UMPD.shortNumbers,"st","Shorten numbers above 1000")
+    cbShortNumbers:SetPoint("TOPLEFT", 8, -342)
+    cbShortNumbers:HookScript("OnClick", function(self,value)
+        UMPD.shortNumbers = self:GetChecked()
+    end)
+
+    -- Notes
+    local notes = UMPDO:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+    notes:SetText("|cffccccccSetting your 'Max Pin Distance' to 0 (zero) will result in unlimited draw distance for pins as your max distance.|r")
+    notes:SetPoint("TOP",0,-138)
+
+    -- About
+    local about = UMPDO:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+    about:SetText("|cffccccccCreated by |cff00ff96Xamchi|cffcccccc on Tarren Mill (EU)|r")
+    about:SetPoint("BOTTOM",0,11)
+
+    -- Init Done
+    _UMPD.init = true
+    UMPD_Init = nil
+    _UMPD:USER_WAYPOINT_UPDATED()
+end
+
+-- Slash command to open options
+SlashCmdList.UMPDO = function(msg)
+	msg = msg:lower()
+	if not InCombatLockdown() then
+		InterfaceOptionsFrame_OpenToCategory(UMPDO)
+        InterfaceOptionsFrame_OpenToCategory(UMPDO)
+	else
+		DEFAULT_CHAT_FRAME:AddMessage(format("%s | Can't modify interface options in combat", _UMPD.name))
+	end
+end
+SLASH_UMPDO1 = "/umpd"
