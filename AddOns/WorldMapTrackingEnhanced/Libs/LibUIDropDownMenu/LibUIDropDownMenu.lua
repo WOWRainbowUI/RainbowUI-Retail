100a42966499061621f9976bef71f6acc0736bad
class2u <class2u@gmail.com>
2024-01-24 15:29:49 +0800

    更新 世界地圖追蹤增強 v4.28

--- AddOns/WorldMapTrackingEnhanced/Libs/LibUIDropDownMenu/LibUIDropDownMenu.lua
+++ AddOns/WorldMapTrackingEnhanced/Libs/LibUIDropDownMenu/LibUIDropDownMenu.lua
@@ -1,4 +1,4 @@
--- $Id: LibUIDropDownMenu.lua 127 2024-01-22 13:09:05Z arithmandar $
+-- $Id: LibUIDropDownMenu.lua 129 2024-01-23 15:53:42Z arithmandar $
 -- ----------------------------------------------------------------------------
 -- Localized Lua globals.
 -- ----------------------------------------------------------------------------
@@ -18,7 +18,7 @@ local GameTooltip_SetTitle, GameTooltip_AddInstructionLine, GameTooltip_AddNorma
 
 -- ----------------------------------------------------------------------------
 local MAJOR_VERSION = "LibUIDropDownMenu-4.0"
-local MINOR_VERSION = 90000 + tonumber(("$Rev: 127 $"):match("%d+"))
+local MINOR_VERSION = 90000 + tonumber(("$Rev: 129 $"):match("%d+"))
 
 
 local LibStub = _G.LibStub
@@ -1317,7 +1317,7 @@ function lib:UIDropDownMenu_AddButton(info, level)
 		_G[listFrameName.."Button"..index.."UnCheck"]:Hide();
 	end
 	button.checked = info.checked;
-	if (WoWRetail) then
+	if (WoWRetail and button.NewFeature) then
 		button.NewFeature:SetShown(button.showNewLabel); -- Retail only?
 	end
 	
