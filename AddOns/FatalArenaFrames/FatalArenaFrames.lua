660a8588ed76d915f0d3e5f3dbb2f5c6960ec6f9
class2u <class2u@gmail.com>
2024-03-15 15:01:46 +0800

    更新 隱藏內建競技場頭像 2.0.1

--- AddOns/FatalArenaFrames/FatalArenaFrames.lua
+++ AddOns/FatalArenaFrames/FatalArenaFrames.lua
@@ -1,26 +1,17 @@
-local _, ns = ...
 local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-eventFrame:RegisterEvent("ARENA_PREP_OPPONENT_SPECIALIZATIONS")
-eventFrame:RegisterEvent("ARENA_OPPONENT_UPDATE")
-eventFrame:RegisterEvent("PVP_MATCH_STATE_CHANGED")
+eventFrame:RegisterEvent("ADDON_LOADED")
 
 local hiddenFrame = CreateFrame("Frame")
 
-function ns:hideFrame(frame)
-  frame:SetScript("OnShow", frame.Hide)
-  frame:Hide()
+local function handleFrame(self, spec)
+    if InCombatLockdown() then return end
+    self:SetParent(hiddenFrame)
 end
 
-function ns:handleFrame(frame)
-  if not frame then return end
-  frame:SetParent(hiddenFrame)
-  ns:hideFrame(frame)
-end
-
-eventFrame:SetScript("OnEvent", function()
-  ns:hideFrame(hiddenFrame)
-  local arenaFrame = _G["CompactArenaFrame"]
-  ns:handleFrame(arenaFrame)
+eventFrame:SetScript("OnEvent", function(self, event, name)
+    if name ~= "FatalArenaFrames" then return end
+    if not CompactArenaFrame then return end
+    hiddenFrame:Hide()
+    hooksecurefunc(CompactArenaFrame, "UpdateVisibility", handleFrame)
 end)
+
