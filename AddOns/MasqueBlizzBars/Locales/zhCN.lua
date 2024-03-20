c1c1b15185483caff22bb29d9ed4a05b93f2bf59
class2u <class2u@gmail.com>
2024-03-20 09:48:52 +0800

    Add Masque Blizzard Bars 10.2.5.0

---
+++ AddOns/MasqueBlizzBars/Locales/zhCN.lua
@@ -0,0 +1,37 @@
+--
+-- Masque Blizzard Bars
+--
+-- Locales\zhCN.lua -- zhCN Localization File
+--
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file or at
+-- https://opensource.org/licenses/MIT.
+--
+
+-- Please use CurseForge to submit localization content for another language:
+-- https://www.curseforge.com/wow/addons/masque-blizz-bars-revived/localization
+
+local Locale = GetLocale()
+if Locale ~= "zhCN" then return end
+
+local _, Shared = ...
+local L = Shared.Locale
+
+L["Action Bar 1"] = "动作条1"
+L["Action Bar 2"] = "动作条2"
+L["Action Bar 3"] = "动作条3"
+L["Action Bar 4"] = "动作条4"
+L["Action Bar 5"] = "动作条5"
+L["Action Bar 6"] = "动作条6"
+L["Action Bar 7"] = "动作条7"
+L["Action Bar 8"] = "动作条8"
+L["Extra Ability Buttons"] = "额外技能按钮"
+L["NOTES_EXTRA_ABILITY_BUTTONS"] = "该组包括在首领战和任务中显示的额外行动按钮，以及特定区域才会出現的区域技能。有些按钮有额外的背景图像，所以方形皮肤往往效果最好。"
+L["NOTES_SPELL_FLYOUTS"] = "该组包括游戏中任何位置显示的所有弹出按钮，例如操作栏和法术书。"
+L["NOTES_VEHICLE_BAR"] = "当您进入具有技能按钮的车辆时，会显示此栏。目前无法更改离开载具的外观"
+L["Pet Bar"] = "宠物条"
+L["Pet Battle Bar"] = "宠物对战条"
+L["Possess Bar"] = "控制条"
+L["Spell Flyouts"] = "技能弹出选项"
+L["Stance Bar"] = "姿态条"
+L["Vehicle Bar"] = "载具条"
