4564a38f625c8564e1598247e6ed83bca3373ae1
class2u <class2u@gmail.com>
2024-03-21 10:37:15 +0800

    更新 世界地圖增強 10.2.20

--- AddOns/Leatrix_Maps/Leatrix_Maps_Icons.lua
+++ AddOns/Leatrix_Maps/Leatrix_Maps_Icons.lua
@@ -279,10 +279,24 @@
 		--	Dragonflight
 		----------------------------------------------------------------------
 
+		[2023] =  --[[Ohn'ahran Plains]] {
+			{"PortalN", 18.5, 52.1, L["Emerald Dream"], L["Portal"]},
+		},
+
 		[2112] =  --[[Valdrakken]] {
 			{"PortalN", 53.9, 55.0, L["Valdrakken Portals"]},
 			{"PortalH", 56.6, 38.4, L["Orgrimmar"], L["Portal"]},
 			{"PortalA", 59.7, 41.8, L["Stormwind"], L["Portal"]},
+			{"PortalN", 62.6, 57.3, L["Emerald Dream"], L["Portal"]},
+			{"PortalN", 26.1, 40.9, L["Badlands"], L["Portal"]},
+		},
+
+		[2200] =  --[[Emerald Dream]] {
+			{"PortalN", 72.8, 52.9, L["Ohn'ahran Plains"], L["Portal"]},
+		},
+
+		[2239] =  --[[Amirdrassil]] {
+			{"PortalN", 89.3, 38.7, L["Emerald Dream"], L["Portal"]},
 		},
 
 	}
