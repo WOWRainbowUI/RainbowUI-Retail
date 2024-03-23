2d0d5c6bfc4f0e235532d5766e6509c169155c95
class2u <class2u@gmail.com>
2024-03-23 10:45:56 +0800

    更新 功能資訊列 2.7.3

---
+++ AddOns/XIV_Databar_Continued/Core/changelog/2.7.3.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[2703] = {
+    version_string = "2.7.3",
+    release_date = "2024/03/22",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] TOC update for Retail 10.2.6.",
+        },
+        ["frFR"] = {
+            "[Retail] Mise à jour du TOC pour la version 10.2.6 de Retail.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
