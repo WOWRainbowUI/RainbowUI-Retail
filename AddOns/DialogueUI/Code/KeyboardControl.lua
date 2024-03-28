de630fa4b93b7c945c6509e4aa5e51082d605499
class2u <class2u@gmail.com>
2024-03-28 10:20:36 +0800

    新增 任務對話 (羊皮紙) 0.2.1

---
+++ AddOns/DialogueUI/Code/KeyboardControl.lua
@@ -0,0 +1,334 @@
+-- Press Key to select option
+-- Planned to support controller in the future
+
+local _, addon = ...
+local API = addon.API;
+local Clipboard = addon.Clipboard;
+
+
+local DEFAULT_CONTROL_KEY = "SPACE";
+
+-- Custom Settings
+local ENABLE_KEYCONTROL_IN_COMBAT = true;
+local PRIMARY_CONTROL_KEY = DEFAULT_CONTROL_KEY;
+local USE_INTERACT_KEY = false;
+------------------
+
+local InCombatLockdown = InCombatLockdown;
+local tostring = tostring;
+local type = type;
+
+local KeyboardControl = CreateFrame("Frame");
+KeyboardControl:Hide();
+KeyboardControl:SetFrameStrata("TOOLTIP");
+KeyboardControl:SetFixedFrameStrata(true);
+addon.KeyboardControl = KeyboardControl;
+
+KeyboardControl.noPropagateFrame = CreateFrame("Frame", nil, KeyboardControl);
+KeyboardControl.noPropagateFrame:SetPropagateKeyboardInput(false);
+
+function KeyboardControl:ResetKeyActions()
+    self.keyActions = {};
+end
+KeyboardControl:ResetKeyActions()
+
+function KeyboardControl:CanSetKey(key)
+    if key then
+        if type(key) == "number" then
+            return key <= 9
+        else
+            return true
+        end
+    end
+    return false
+end
+
+function KeyboardControl:SetKeyFunction(key, func)
+    if not self:CanSetKey(key) then return end;
+
+    key = tostring(key);
+
+    if not self.keyActions[key] then
+        self.keyActions[key] = {
+            obj = func,
+            type = "function",
+        };
+        return key
+    end
+end
+
+function KeyboardControl:SetKeyButton(key, buttonToClick)
+    if key == "PRIMARY" then
+        key = PRIMARY_CONTROL_KEY;
+    end
+
+    if not self:CanSetKey(key) then return end;
+
+    key = tostring(key)
+
+    if not self.keyActions[key] then
+        self.keyActions[key] = {
+            obj = buttonToClick,
+            type = "button",
+        };
+        return key
+    end
+end
+
+function KeyboardControl:OnEvent(event, ...)
+    if event == "PLAYER_REGEN_DISABLED" then
+        self:RegisterEvent("PLAYER_REGEN_ENABLED");
+    elseif event == "PLAYER_REGEN_ENABLED" then
+        if self:IsVisible() then
+
+        end
+    elseif event == "UPDATE_BINDINGS" then
+        self.bindingDirty = true;
+    end
+end
+KeyboardControl:SetScript("OnEvent", KeyboardControl.OnEvent);
+
+function KeyboardControl:OnHide()
+    self:StopListeningKeys();
+    self:UnregisterEvent("PLAYER_REGEN_DISABLED");
+    self:UnregisterEvent("PLAYER_REGEN_ENABLED");
+    self:ResetKeyActions();
+    self:StopRepeatingAction();
+end
+KeyboardControl:SetScript("OnHide", KeyboardControl.OnHide);
+
+function KeyboardControl:OnShow()
+    self:RegisterEvent("PLAYER_REGEN_DISABLED");
+
+    if self.bindingDirty then
+        self.bindingDirty = nil;
+        if USE_INTERACT_KEY then
+            local newKey = API.GetBestInteractKey();
+            if newKey and newKey ~= PRIMARY_CONTROL_KEY then
+                PRIMARY_CONTROL_KEY = newKey;
+                addon.DialogueUI:OnSettingsChanged();
+            end
+        end
+    end
+end
+
+KeyboardControl:SetScript("OnShow", KeyboardControl.OnShow);
+
+function KeyboardControl:OnKeyDown(key, fromGamePad)
+    local valid = false;
+    local processed = false;
+
+    if key == "PAD1" then
+        valid = KeyboardControl.parent:ClickFocusedObject();
+        if valid then
+            processed = true;
+        else
+            key = PRIMARY_CONTROL_KEY;
+        end
+    end
+
+    if (not processed) and (key == PRIMARY_CONTROL_KEY) and (not KeyboardControl.keyActions[key]) then
+        key = "1";
+    end
+
+    if key == "ESCAPE" then
+        valid = true;
+
+        if Clipboard:IsShown() then
+            Clipboard:Hide();
+            processed = true;
+        elseif addon.SettingsUI:IsShown() then
+            addon.SettingsUI:Hide();
+            processed = true;
+        else
+            if fromGamePad then
+
+            else
+                if KeyboardControl.parent.HideUI then
+                    local cancelPopupFirst = true;
+                    KeyboardControl.parent:HideUI(cancelPopupFirst);
+                    processed = true;
+                else
+                    KeyboardControl.parent:Hide();
+                    processed = true;
+                end
+            end
+        end
+    elseif key == "GAMEPAD_UP" then
+        valid = true;
+        processed = true;
+        KeyboardControl.parent:FocusPreviousObject();
+    elseif key == "GAMEPAD_DOWN" then
+        valid = true;
+        processed = true;
+        KeyboardControl.parent:FocusNextObject();
+    elseif key == "F1" then
+        valid = true;
+        processed = true;
+        DialogueUI_ShowSettingsFrame();
+    end
+
+    if (not processed) and KeyboardControl.keyActions[key] then
+        valid = true;
+
+        local actionType = KeyboardControl.keyActions[key].type;
+        local object = KeyboardControl.keyActions[key].obj;
+
+        if actionType == "function" then
+            object();
+        elseif actionType == "button" then
+            if object:IsEnabled() and object:IsVisible() then
+                local noFeedback = object:OnClick("GamePad");
+                if (not noFeedback) and object.PlayKeyFeedback then
+                    object:PlayKeyFeedback();
+                end
+            end
+        end
+    end
+
+    if not InCombatLockdown() then
+        KeyboardControl:SetPropagateKeyboardInput(not valid);
+    end
+end
+
+function KeyboardControl:SetParentFrame(frame)
+    self.parent = frame;
+    self:SetParent(frame);
+    self:Show();
+
+    self:StopListeningKeys();
+
+    local listener;
+
+    if InCombatLockdown() then
+        if ENABLE_KEYCONTROL_IN_COMBAT then
+            listener = self.noPropagateFrame;
+        end
+    else
+        listener = self;
+    end
+
+    if listener then
+        listener:SetScript("OnKeyDown", self.OnKeyDown);
+        listener:SetScript("OnGamePadButtonDown", self.OnGamePadButtonDown);
+        listener:SetScript("OnGamePadButtonUp", self.OnGamePadButtonUp)
+        listener:EnableGamePadButton(true);
+        listener:EnableKeyboard(true);
+    end
+end
+
+function KeyboardControl:StopListeningKeys()
+    self:SetScript("OnKeyDown", nil);
+    self.noPropagateFrame:SetScript("OnKeyDown", nil);
+
+    self:SetScript("OnGamePadButtonDown", nil);
+    self.noPropagateFrame:SetScript("OnGamePadButtonDown", nil);
+    self:SetScript("OnGamePadButtonUp", nil);
+    self.noPropagateFrame:SetScript("OnGamePadButtonUp", nil);
+
+    self:EnableGamePadButton(false);
+    self.noPropagateFrame:EnableGamePadButton(false);
+
+    self:EnableKeyboard(false);
+    self.noPropagateFrame:EnableKeyboard(false);
+end
+
+function KeyboardControl.GetPrimaryControlKey()
+    return PRIMARY_CONTROL_KEY
+end
+
+
+do  --GamePad/Controller
+    local KeyRemap = {
+        PAD2 = "ESCAPE",
+        PADDUP = "GAMEPAD_UP",
+        PADDDOWN = "GAMEPAD_DOWN",
+        PADFORWARD = "F1",  --Toggle Settings
+        PADBACK = "ESCAPE",
+        PADDLEFT = "GAMEPAD_UP",
+        PADDRIGHT = "GAMEPAD_DOWN",
+    };
+
+    local RepeatableButton = {
+        PADDUP = true,
+        PADDDOWN = true,
+        PADDLEFT = true,
+        PADDRIGHT = true,
+    };
+
+    local REPEAT_INTERVAL = 0.125;
+
+    function KeyboardControl:OnGamePadButtonDown(button)
+        --print("|cFF8cd964"..button);
+        self:StopRepeatingAction();
+
+        if button == "PAD1" then
+
+        elseif button == "PAD4" then
+            local TooltipFrame = addon.SharedTooltip;
+            if TooltipFrame and TooltipFrame:IsShown() then
+                TooltipFrame:ToggleAlternateInfo()
+            end
+        else
+            if RepeatableButton[button] then
+                self:RepeatAction(KeyRemap[button]);
+            end
+            button = KeyRemap[button];
+        end
+
+        if button then
+            KeyboardControl:OnKeyDown(button, true);
+        end
+    end
+
+    function KeyboardControl:OnGamePadButtonUp(button)
+        --print("|cFF8cd964"..button);
+        self:StopRepeatingAction();
+    end
+
+    local function RepeatGamePadButton_OnUpdate(self, elapsed)
+        self.repeatElapsed = self.repeatElapsed + elapsed;
+        if self.repeatElapsed >= REPEAT_INTERVAL then
+            self.repeatElapsed = 0;
+            if self.repeatButton then
+                KeyboardControl:OnKeyDown(self.repeatButton, true);
+            else
+                self:StopRepeatingAction();
+            end
+        end
+    end
+
+    function KeyboardControl:RepeatAction(button)
+        self.repeatElapsed = -0.375;
+        self.repeatButton = button;
+        self:SetScript("OnUpdate", RepeatGamePadButton_OnUpdate);
+    end
+
+    function KeyboardControl:StopRepeatingAction()
+        self:SetScript("OnUpdate", nil);
+        self.repeatElapsed = nil;
+    end
+end
+
+
+do  --Settings
+    local function Settings_PrimaryControlKey(dbValue)
+        local newKey;
+
+        if dbValue == 1 then
+            newKey = DEFAULT_CONTROL_KEY;
+            KeyboardControl:UnregisterEvent("UPDATE_BINDINGS");
+            USE_INTERACT_KEY = false;
+        elseif dbValue == 2 then
+            newKey = API.GetBestInteractKey();
+            KeyboardControl:RegisterEvent("UPDATE_BINDINGS");
+            USE_INTERACT_KEY = true;
+        end
+
+        if newKey and newKey ~= PRIMARY_CONTROL_KEY then
+            PRIMARY_CONTROL_KEY = newKey;
+            addon.DialogueUI:OnSettingsChanged();
+        end
+    end
+    addon.CallbackRegistry:Register("SettingChanged.PrimaryControlKey", Settings_PrimaryControlKey);
+end
---
+++ AddOns/DialogueUI/Locales/zhTW.lua
@@ -0,0 +1,152 @@
+
+if not (GetLocale() == "zhTW") then return end;
+
+
+local _, addon = ...
+local L = addon.L;
+
+L["Quest Frequency Daily"] = DAILY or "每日";
+L["Quest Frequency Weekly"] = WEEKLY or "每週";
+
+L["Quest Type Repeatable"] = "可重覆";
+L["Quest Type Trivial"] = "低等級";    --Low-level quest
+L["Quest Type Dungeon"] = LFG_TYPE_DUNGEON or "地城";
+L["Quest Type Raid"] = LFG_TYPE_RAID or "團隊";
+L["Quest Type Covenant Calling"] = "誓盟使命";
+
+L["Accept"] = ACCEPT or "接受";
+L["Continue"] = CONTINUE or "繼續";
+L["Complete Quest"] = COMPLETE_QUEST or "完成任務";
+L["Incomplete"] = INCOMPLETE or "未完成";
+L["Cancel"] = CANCEL or "取消";
+L["Goodbye"] = GOODBYE or "再見";
+L["Decline"] = DECLINE or "拒絕";
+L["OK"] = "OK";
+L["Quest Objectives"] = OBJECTIVES_LABEL or "目標";   --We use the shorter one, not QUEST_OBJECTIVES
+L["Reward"] = REWARD or "獎勵";
+L["Rewards"] = REWARDS or "獎勵";
+L["War Mode Bonus"] = WAR_MODE_BONUS or "戰爭模式加成";
+L["Honor Points"] = HONOR_POINTS or "榮譽";
+L["Symbol Gold"] = GOLD_AMOUNT_SYMBOL or "金";
+L["Symbol Silver"] = Silver_AMOUNT_SYMBOL or "銀";
+L["Symbol Copper"] = COPPER_AMOUNT_SYMBOL or "銅";
+L["Requirements"] = REQUIREMENTS or "需要";
+L["Current Colon"] = ITEM_UPGRADE_CURRENT or "目前:";
+L["Renown Level Label"] = RENOWN_LEVEL_LABEL or "名望 ";  --There is a space
+L["Abilities"] = ABILITIES or "能力";
+L["Traits"] = GARRISON_RECRUIT_TRAITS or "特質";
+L["Costs"] = "花費";   --The costs to continue an action, usually gold
+L["Ready To Enter"] = QUEUED_STATUS_PROPOSAL or "可以進入";
+L["Show Comparison"] = "顯示比較";   --Toggle item comparison on the tooltip
+L["Hide Comparison"] = "隱藏比較";
+L["Copy Text"] = "複製文字";
+L["To Next Level Label"] = COVENANT_MISSIONS_XP_TO_LEVEL or "到下個等級";
+L["Quest Accepted"] = "已接取任務";
+L["Quest Log Full"] = "任務日誌已滿";
+L["Quest Auto Accepted Tooltip"] = "遊戲會自動接取這個任務。";
+L["Level Maxed"] = "(最大)";   --Reached max level
+L["Paragon Reputation"] = "巔峰";
+L["Different Item Types Alert"] = "物品類型不同!";
+L["Click To Read"] = "點一下左鍵來閱讀";
+L["Item Level"] = STAT_AVERAGE_ITEM_LEVEL or "物品等級";
+
+
+--String Format
+L["Format Reputation Reward Tooltip"] = QUEST_REPUTATION_REWARD_TOOLTIP or "獎勵 %d 聲望和 %s";
+L["Format You Have X"] = "- 你有 |cffffffff%d|r 個";
+L["Format You Have X And Y In Bank"] = "- 你有 |cffffffff%d|r 個 (|cffffffff%d|r 個在銀行)";
+L["Format Suggested Players"] = QUEST_SUGGESTED_GROUP_NUM or "建議玩家數 [%d]";
+L["Format Current Skill Level"] = "目前等級: |cffffffff%d/%d|r";
+L["Format Reward Title"] = HONOR_REWARD_TITLE or "頭銜: %s";
+L["Format Follower Level Class"] = FRIENDS_LEVEL_TEMPLATE or "等級 %d %s";
+L["Format Monster Say"] = CHAT_MONSTER_SAY_GET or "%s 說: ";
+L["Format Quest Accepted"] = ERR_QUEST_ACCEPTED_S or "已接取任務: %s";
+L["Format Quest Completed"] = ERR_QUEST_COMPLETE_S or "%s 已完成。";
+L["Format Player XP"] = PET_BATTLE_CURRENT_XP_FORMAT_BOTH or  "經驗值: %d/%d (%d%%)";
+L["Format Gold Amount"] = GOLD_AMOUNT or "%d 金";
+L["Format Silver Amount"] = SILVER_AMOUNT or "%d 銀";
+L["Format Copper Amount"] = COPPER_AMOUNT or "%d 銅";
+L["Format Unit Level"] = UNIT_LEVEL_TEMPLATE or "等級 %d";
+L["Format Replace Item"] = "取代 %s";
+L["Format Item Level"] = "物品等級 %d";   --_G.ITEM_LEVEL in Classic is different
+
+
+--Settings
+L["UI"] = "介面";
+L["Camera"] = "鏡頭";
+L["Control"] = "控制";
+L["Gameplay"] = SETTING_GROUP_GAMEPLAY or "遊戲體驗";
+
+L["Quest"] = "任務";
+L["Gossip"] = "閒聊";
+L["Theme"] = "主題";
+L["Theme Desc"] = "選擇介面的顏色主題。";
+L["Theme Brown"] = "羊皮紙";
+L["Theme Dark"] = "暗色";
+L["Frame Size"] = "框架大小";
+L["Frame Size Desc"] = "設定對話介面的大小。\n\n預設: 中";
+L["Size Extra Small"] = "特別小";
+L["Size Small"] = "小";
+L["Size Medium"] = "中";
+L["Size Large"] = "大";
+L["Font Size"] = "文字大小";
+L["Font Size Desc"] = "設定介面的文字大小。\n\n預設: 12";
+L["Hide UI"] = "隱藏介面";
+L["Hide UI Desc"] = "和 NPC 互動時淡出遊戲介面。";
+L["Hide Unit Names"] = "隱藏單位名字";
+L["Hide Unit Names Desc"] = "和 NPC 互動時隱藏玩家和其他 NPC 的名字。";
+L["Show Copy Text Button"] = "顯示複製文字按鈕";
+L["Show Copy Text Button Desc"] = "在對話介面的右上方顯示複製文字按鈕。";
+L["Show Quest Type Text"] = "顯示任務類型文字";
+L["Show Quest Type Text Desc"] = "如果任務類型特殊，則在選項右側顯示任務類型。\n\n低等級任務永遠都會顯示。";
+L["Show NPC Name On Page"] = "顯示 NPC 名字";
+L["Show NPC Name On Page Desc"] = "在頁面中顯示 NPC 名字。";
+L["Simplify Currency Rewards"] = "簡化貨幣獎勵";
+L["Simplify Currency Rewards Desc"] = "貨幣獎勵顯示為小圖示並省略名稱。";
+L["Mark Highest Sell Price"] = "標示售價最高的";
+L["Mark Highest Sell Price Desc"] = "選擇獎勵時，顯示哪件商品的售價最高。";
+
+L["Camera Movement"] = "鏡頭移動";
+L["Camera Movement Off"] = "關閉";
+L["Camera Movement Zoom In"] = "拉近";
+L["Camera Movement Horizontal"] = "水平";
+L["Maintain Camera Position"] = "保持鏡頭位置";
+L["Maintain Camera Position Desc"] = "NPC 互動結束後短暫保持鏡頭位置。 啟用此選項將減少因對話之間的延遲而導致的鏡頭突然移動。";
+L["Change FOV"] = "更改視角";
+L["Change FOV Desc"] = "縮小鏡頭的可見範圍，靠近並放大 NPC。";
+L["Disable Camera Movement Instance"] = "在副本中時停用";
+L["Disable Camera Movement Instance Desc"] = "在地城和團隊中不要移動鏡頭。";
+
+L["Input Device"] = "輸入裝置";
+L["Input Device Desc"] = "會影響快速鍵圖示和介面的版面配置。";
+L["Input Device KBM"] = "鍵盤和滑鼠";
+L["Input Device Xbox"] = "Xbox";
+L["Input Device Xbox Tooltip"] = "確認按鈕: [KEY:XBOX:PAD1]\n取消按鈕: [KEY:XBOX:PAD2]";
+L["Input Device PlayStation"] = "PlayStation";
+L["Input Device PlayStation Tooltip"] = "確認按鈕: [KEY:PS:PAD1]\n取消按鈕: [KEY:PS:PAD2]";
+L["Primary Control Key"] = "確認按鈕";
+L["Primary Control Key Desc"] = "按此鍵來選擇第一個可用選項，例如「接受任務」。"
+L["Press Button To Scroll Down"] = "按下按鈕往下捲頁";
+L["Press Button To Scroll Down Desc"] = "如果內容超出可以看見的範圍，按下確認按鈕將向下捲動頁面而不是接受任務。";
+
+L["Key Space"] = "空白";
+L["Key Interact"] = "互動";
+L["Cannot Use Key Combination"] = "不支援按鈕組合";
+L["Interact Key Not Set"] = "尚未設定互動按鍵。"
+
+L["Quest Item Display"] = "顯示任務物品";
+L["Quest Item Display Desc"] = "自動顯示任務物品的說明，無需打開袋子即可使用它。";
+L["Quest Item Display Hide Seen"] = "忽略已看見的物品";
+L["Quest Item Display Hide Seen Desc"] = "忽略你的任何角色已發現過的物品。";
+L["Auto Select"] = "自動選擇";
+L["Auto Select Gossip"] = "自動選擇選項";
+L["Auto Select Gossip Desc"] = "與特定 NPC 互動時自動選擇最佳的對話選項。";
+L["Force Gossip"] = "強制閒聊";
+L["Force Gossip Desc"] = "預設情況下，遊戲有時會自動選擇第一個選項而不顯示對話框。 透過啟用強制閒聊，該對話框便會出現。";
+L["Nameplate Dialog"] = "在名條上顯示對話";
+L["Nameplate Dialog Desc"] = "如果 NPC 沒有提供選擇，則在 NPC 名條上顯示對話。\n\n此選項會修改 CVar \"SoftTarget Nameplate Interact\"。";
+
+
+--Tutorial
+L["Tutorial Settings Hotkey"] = "按下 [KEY:PC:F1] 打開設定";
+L["Tutorial Settings Hotkey Console"] = "按下 [KEY:PC:F1] 或 [KEY:CONSOLE:MENU] 打開設定";   --Use this if gamepad enabled
