1b6e188f5ee263b1492f274cbf7e6909e10e2851
class2u <class2u@gmail.com>
2024-04-14 09:51:11 +0800

    更新 掰特包 v0.1.27

--- AddOns/BetterBags/frames/classic/context.lua
+++ AddOns/BetterBags/frames/classic/context.lua
@@ -112,7 +112,7 @@ function context:CreateContextMenu(bag)
 		text = addonName..' Dev Mode',
 		--@end-debug@]==]
 		--@non-debug@
-		text = addonName..' v0.1.26',
+		text = addonName..' v0.1.27',
 		--@end-non-debug@
     isTitle = true,
     notCheckable = true
