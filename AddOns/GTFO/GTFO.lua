995f8f73a1bfb22c0853ba7fde4f9f7ce32afbda
class2u <class2u@gmail.com>
2024-05-02 16:32:45 +0800

    修正 GTFO：浩劫載入時的錯誤。

--- AddOns/GTFO/GTFO.lua
+++ AddOns/GTFO/GTFO.lua
@@ -1970,7 +1970,7 @@ function GTFO_CheckTankMode()
 			end
 		elseif ((not (GTFO.ClassicMode or GTFO.BurningCrusadeMode or GTFO.WrathMode)) and (GTFO.PlayerClass == "MONK" or GTFO.PlayerClass == "DEMONHUNTER" or GTFO.PlayerClass == "WARRIOR" or GTFO.PlayerClass == "DEATHKNIGHT" or GTFO.PlayerClass == "PALADIN")) then
 			-- Get the exact specialization role as defined by the class
-			local spec = GetSpecialization();
+			local spec = GetSpecialization and GetSpecialization() or nil; -- 暫時修正
 			if (spec and GetSpecializationRole(spec) == "TANK") then
 				--GTFO_DebugPrint("Tank spec found - tank mode activated");
 				return true;
@@ -1994,7 +1994,7 @@ function GTFO_CheckCasterMode()
 
 		if not (GTFO.ClassicMode or GTFO.BurningCrusadeMode or GTFO.WrathMode) then
 			-- Get the exact specialization role as defined by the class
-			local spec = GetSpecialization();
+			local spec = GetSpecialization and GetSpecialization() or nil; -- 暫時修正
 			if (spec) then
 				local role = GetSpecializationRole(spec);
 				if (role == "TANK") then
