57b02d65f6e1de308ed48bf620a21c2b9e8c8096
class2u <class2u@gmail.com>
2024-05-08 19:41:04 +0800

    新增 掰特包-改善名稱排序 v1.0.4

---
+++ AddOns/BetterBags_BetterSort/CHANGELOG.md
@@ -0,0 +1,8 @@
+# BetterBags - Better Sort
+
+## [v1.0.4](https://github.com/Krealle/BetterBags_BetterSort/tree/v1.0.4) (2024-05-08)
+[Full Changelog](https://github.com/Krealle/BetterBags_BetterSort/compare/v1.0.3...v1.0.4) [Previous Releases](https://github.com/Krealle/BetterBags_BetterSort/releases)
+
+- bump retail to 2.0.7  
+- Spell check  
+- Update README.md  
---
+++ AddOns/BetterBags_BetterSort/main.lua
@@ -0,0 +1,35 @@
+---@class BetterBags: AceAddon
+local BetterBags = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+---@class Localization: AceModule
+local L = BetterBags:GetModule('Localization')
+---@class Sort: AceModule
+local sort = BetterBags:GetModule('Sort')
+
+-- Override the default sort function
+---@param a Section
+---@param b Section
+---@return boolean
+function sort.SortSectionsAlphabetically(a, b)
+    local titleA = a.title:GetText()
+    local titleB = b.title:GetText()
+
+    if titleA == L:G("Recent Items") then return true end
+    if titleB == L:G("Recent Items") then return false end
+
+    if a:GetFillWidth() then return false end
+    if b:GetFillWidth() then return true end
+
+    if titleA == L:G("Free Space") then return false end
+    if titleB == L:G("Free Space") then return true end
+
+    return stripColorCode(titleA) < stripColorCode(titleB)
+end
+
+---@param text string
+---@return string
+function stripColorCode(text)
+    if string.sub(text, 1, 4) == "|cff" then
+        return string.sub(text, 11)
+    end
+    return text
+end
