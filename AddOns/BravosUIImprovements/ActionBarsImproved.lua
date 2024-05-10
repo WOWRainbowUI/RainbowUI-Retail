5fe6d55a0833df8100fe7751ac303048cea6bfdf
class2u <class2u@gmail.com>
2024-05-10 23:02:06 +0800

    更新 內建介面增強 202405100228
    # Conflicts:
    #	AddOns/BravosUIImprovements/BravosUIImprovements.lua
    #	AddOns/BravosUIImprovements/CastBarTimers.lua
    #	AddOns/BravosUIImprovements/QuickKeybindModeShortcut.lua

--- AddOns/BravosUIImprovements/ActionBarsImproved.lua
+++ AddOns/BravosUIImprovements/ActionBarsImproved.lua
@@ -1,119 +1,118 @@
 local actionBarHookSet = false
 
 local function experimentalFixPaddingMinSetting()
-	-- Set icon padding to 0
-	-- EditModePresetLayoutManager:GetModernSystemMap()[Enum.EditModeSystem.ActionBar][
-	-- 	 Enum.EditModeActionBarSystemIndices.MainBar]["settings"][4] = 0
-	-- -- verify value
-	-- for key, value in pairs(EditModePresetLayoutManager:GetModernSystemMap()[Enum.EditModeSystem.ActionBar][
-	-- 	Enum.EditModeActionBarSystemIndices.MainBar]["settings"]) do
-	-- 	print("Key: ", key, " Value: ", value)
-	-- end
-	-- EditModeSettingDisplayInfoManager.systemSettingDisplayInfo[Enum.EditModeSystem.ActionBar][5] = {
-	-- 	minValue = 0,
-	-- }
+  -- Set icon padding to 0
+  -- EditModePresetLayoutManager:GetModernSystemMap()[Enum.EditModeSystem.ActionBar][
+  -- 	 Enum.EditModeActionBarSystemIndices.MainBar]["settings"][4] = 0
+  -- -- verify value
+  -- for key, value in pairs(EditModePresetLayoutManager:GetModernSystemMap()[Enum.EditModeSystem.ActionBar][
+  -- 	Enum.EditModeActionBarSystemIndices.MainBar]["settings"]) do
+  -- 	print("Key: ", key, " Value: ", value)
+  -- end
+  -- EditModeSettingDisplayInfoManager.systemSettingDisplayInfo[Enum.EditModeSystem.ActionBar][5] = {
+  -- 	minValue = 0,
+  -- }
 
-	-- local kiddos = { MainMenuBar:GetChildren() };
-	-- for _, child in ipairs(kiddos) do
-	-- 	if child.SetPadding ~= nil then
-	-- 		 print(child:GetName(), " can set padding!")
-	-- 	else
-	-- 		print(child:GetName(), " can't set padding :(")
-	-- 		local grand_kiddos = { child:GetChildren() };
-	-- 		for _, grand_child in ipairs(grand_kiddos) do
+  -- local kiddos = { MainMenuBar:GetChildren() };
+  -- for _, child in ipairs(kiddos) do
+  -- 	if child.SetPadding ~= nil then
+  -- 		 print(child:GetName(), " can set padding!")
+  -- 	else
+  -- 		print(child:GetName(), " can't set padding :(")
+  -- 		local grand_kiddos = { child:GetChildren() };
+  -- 		for _, grand_child in ipairs(grand_kiddos) do
 
-	-- 			if grand_child.SetPadding ~= nil then
-	-- 				 print(grand_child:GetName(), " can set padding!")
-	-- 			else
-	-- 				print(grand_child:GetName(), " can't set padding :(")
-	-- 			end
-	-- 		end
-	-- 	end
-	-- end
+  -- 			if grand_child.SetPadding ~= nil then
+  -- 				 print(grand_child:GetName(), " can set padding!")
+  -- 			else
+  -- 				print(grand_child:GetName(), " can't set padding :(")
+  -- 			end
+  -- 		end
+  -- 	end
+  -- end
 
-	local layout
-	if true then
-		layout = GridLayoutUtil.CreateStandardGridLayout(stride, buttonPadding, buttonPadding, xMultiplier, yMultiplier);
-	else
-		layout = GridLayoutUtil.CreateVerticalGridLayout(stride, buttonPadding, buttonPadding, xMultiplier, yMultiplier);
-	end
-
+  local layout
+  if true then
+    layout = GridLayoutUtil.CreateStandardGridLayout(stride, buttonPadding, buttonPadding, xMultiplier, yMultiplier);
+  else
+    layout = GridLayoutUtil.CreateVerticalGridLayout(stride, buttonPadding, buttonPadding, xMultiplier, yMultiplier);
+  end
 end
 
 local function setButtonPaddingOnActionBar(actionBar, padding)
-	if padding < actionBar.minButtonPadding then
-		actionBar.minButtonPadding = padding
-	end
-	actionBar.buttonPadding = padding
+  if padding < actionBar.minButtonPadding then
+    actionBar.minButtonPadding = padding
+  end
+  actionBar.buttonPadding = padding
 
-	if actionBar.UpdateGridLayout then
-		actionBar:UpdateGridLayout()
-	end
+  if actionBar.UpdateGridLayout then
+    actionBar:UpdateGridLayout()
+  end
 end
 
 local function actionBar_OnUpdate(self, arg1, ...)
-	if self.minButtonPadding ~= 0 then
-		self.minButtonPadding = 0
-		self.buttonPadding = 0
-		-- Call show to make the changes take effect visually
-		-- self:Show()
-	end
+  if self.minButtonPadding ~= 0 then
+    self.minButtonPadding = 0
+    self.buttonPadding = 0
+    -- Call show to make the changes take effect visually
+    -- self:Show()
+  end
 end
 
 local function disableBorderOnActionBar(actionBarName)
-	for i = 0, 12 do
-		local button = _G[actionBarName .. "Button" .. i]
-		if button then
-			button:DisableDrawLayer("OVERLAY")
-		end
-	end
+  for i = 0, 12 do
+    local button = _G[actionBarName .. "Button" .. i]
+    if button then
+      button:DisableDrawLayer("OVERLAY")
+    end
+  end
 end
 
 local function enableBorderOnActionBar(actionBarName)
-	for i = 0, 12 do
-		local button = _G[actionBarName .. "Button" .. i]
-		if button then
-			button:EnableDrawLayer("OVERLAY")
-		end
-	end
+  for i = 0, 12 do
+    local button = _G[actionBarName .. "Button" .. i]
+    if button then
+      button:EnableDrawLayer("OVERLAY")
+    end
+  end
 end
 
 function BUII_ActionBarsImprovedNoPaddingEnable()
-	-- if not actionBarHookSet then
-	-- 	StanceBar:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	MainMenuBar:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	MultiBarLeft:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	MultiBarRight:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	MultiBarBottomLeft:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	MultiBarBottomRight:HookScript("OnUpdate", actionBar_OnUpdate)
-	-- 	actionBarHookSet = true
-	-- end
+  -- if not actionBarHookSet then
+  -- 	StanceBar:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	MainMenuBar:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	MultiBarLeft:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	MultiBarRight:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	MultiBarBottomLeft:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	MultiBarBottomRight:HookScript("OnUpdate", actionBar_OnUpdate)
+  -- 	actionBarHookSet = true
+  -- end
 
-	experimentalFixPaddingMinSetting()
+  experimentalFixPaddingMinSetting()
 
-	-- BUIIDatabase["no_action_bar_padding"] = true
+  -- BUIIDatabase["no_action_bar_padding"] = true
 
-	-- setButtonPaddingOnActionBar(StanceBar, 0)
-	-- setButtonPaddingOnActionBar(MainMenuBar, 0)
-	-- setButtonPaddingOnActionBar(MultiBarLeft, 0)
-	-- setButtonPaddingOnActionBar(MultiBarRight, 0)
-	-- setButtonPaddingOnActionBar(MultiBarBottomLeft, 0)
-	-- setButtonPaddingOnActionBar(MultiBarBottomRight, 0)
+  -- setButtonPaddingOnActionBar(StanceBar, 0)
+  -- setButtonPaddingOnActionBar(MainMenuBar, 0)
+  -- setButtonPaddingOnActionBar(MultiBarLeft, 0)
+  -- setButtonPaddingOnActionBar(MultiBarRight, 0)
+  -- setButtonPaddingOnActionBar(MultiBarBottomLeft, 0)
+  -- setButtonPaddingOnActionBar(MultiBarBottomRight, 0)
 
-	-- disableBorderOnActionBar("Stance")
-	-- disableBorderOnActionBar("Action")
-	-- disableBorderOnActionBar("MultiBarLeft")
-	-- disableBorderOnActionBar("MultiBarRight")
-	-- disableBorderOnActionBar("MultiBarBottomLeft")
-	-- disableBorderOnActionBar("MultiBarBottomRight")
+  -- disableBorderOnActionBar("Stance")
+  -- disableBorderOnActionBar("Action")
+  -- disableBorderOnActionBar("MultiBarLeft")
+  -- disableBorderOnActionBar("MultiBarRight")
+  -- disableBorderOnActionBar("MultiBarBottomLeft")
+  -- disableBorderOnActionBar("MultiBarBottomRight")
 end
 
 function BUII_ActionBarsImprovedNoPaddingDisable()
-	BUIIDatabase["no_action_bar_padding"] = false
-	-- enableBorderOnActionBar("Stance")
-	-- enableBorderOnActionBar("Action")
-	-- enableBorderOnActionBar("MultiBarLeft")
-	-- enableBorderOnActionBar("MultiBarRight")
-	-- enableBorderOnActionBar("MultiBarBottomLeft")
-	-- enableBorderOnActionBar("MultiBarBottomRight")
+  BUIIDatabase["no_action_bar_padding"] = false
+  -- enableBorderOnActionBar("Stance")
+  -- enableBorderOnActionBar("Action")
+  -- enableBorderOnActionBar("MultiBarLeft")
+  -- enableBorderOnActionBar("MultiBarRight")
+  -- enableBorderOnActionBar("MultiBarBottomLeft")
+  -- enableBorderOnActionBar("MultiBarBottomRight")
 end
---
+++ AddOns/BravosUIImprovements/TooltipImprovements.lua
@@ -0,0 +1,58 @@
+local enabled = nil
+
+local function BUII_AddColoredExpLine(name, tooltip)
+	if name == "艾澤拉斯" then
+		tooltip:AddLine(name, 1, 1, 1)
+	elseif name == "燃燒的遠征" then
+		tooltip:AddLine(name, 0, 1, 0)
+	elseif name == "巫妖王之怒" then
+		tooltip:AddLine(name, 0, 0.8, 1)
+	elseif name == "浩劫與重生" then
+		tooltip:AddLine(name, 0.8, 0.2, 0)
+	elseif name == "潘達利亞之迷" then
+		tooltip:AddLine(name, 0, 1, 0.6)
+	elseif name == "德拉諾之霸" then
+		tooltip:AddLine(name, 0.78, 0.61, 0.43)
+	elseif name == "軍臨天下" then
+		tooltip:AddLine(name, 0, 0.8, 0)
+	elseif name == "決戰艾澤拉斯" then
+		tooltip:AddLine(name, 0.20, 0.39, 0.67)
+	elseif name == "暗影之境" then
+		tooltip:AddLine(name, 0.6, 0.8, 1)
+	elseif name == "巨龍群島" then
+		tooltip:AddLine(name, 0, 1, 0.6)
+	elseif name == "當前賽季" then
+    -- Blizz seems to have added a Current Season tag in 10.2.7
+    -- to have the same behaviour as before the patch make it Dragonflight
+		tooltip:AddLine("巨龍群島", 0, 1, 0.6)
+	else
+		print("缺少資料片: " .. name)
+	end
+end
+
+local function BUII_TooltipImprovements_OnTooltipSetItem(tooltip, data)
+	if enabled then
+		if tooltip == GameTooltip or tooltip == ItemRefTooltip then
+			local item = select(3, tooltip:GetItem())
+			if item then
+				local expansionID = select(15, GetItemInfo(item))
+				if expansionID then
+					-- EJ_GetTierInfo needs expansionID + 1 to get the correct expansion
+					local expansionName = EJ_GetTierInfo(expansionID + 1)
+					BUII_AddColoredExpLine(expansionName, tooltip)
+				end
+			end
+		end
+	end
+end
+
+function BUII_TooltipImprovements_Enabled()
+	if enabled == nil then
+		TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, BUII_TooltipImprovements_OnTooltipSetItem)
+		enabled = true
+	end
+end
+
+function BUII_TooltipImprovements_Disable()
+	enabled = false
+end
