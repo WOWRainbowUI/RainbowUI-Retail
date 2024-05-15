e451da4f0efeec8cd7e3c0b0ec6025e269820534
class2u <class2u@gmail.com>
2024-05-15 13:41:00 +0800

    更新 功能百寶箱 10.2.27

--- AddOns/Leatrix_Plus/Leatrix_Plus.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 10.2.26 (8th May 2024)
+-- 	Leatrix Plus 10.2.27 (15th May 2024)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "10.2.26"
+	LeaPlusLC["AddonVer"] = "10.2.27"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
