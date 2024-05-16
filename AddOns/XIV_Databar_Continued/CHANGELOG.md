29a56bfd82a82f680d71157fbe39a7654c614db3
class2u <class2u@gmail.com>
2024-05-17 00:03:13 +0800

    更新 功能資訊列 2.8
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/Core/changelog/Load_Changelog.xml
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Cata.toc
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Mainline.toc
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Vanilla.toc

--- AddOns/XIV_Databar_Continued/CHANGELOG.md
+++ AddOns/XIV_Databar_Continued/CHANGELOG.md
@@ -1,9 +1,18 @@
 # XIV_Databar Continued
 
-## [2.7.3](https://github.com/ZelionGG/XIV_Databar-Continued/tree/v2.7.3) (2024-03-22)
+## [2.8](https://github.com/ZelionGG/XIV_Databar-Continued/tree/v2.8) (2024-05-16)
 
-[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v2.7.2...v2.7.3) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
+[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v2.7.3...v2.8) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
 
 ### _Retail :_
 
-- TOC update for Retail 10.2.6.
+- TOC update for Retail 10.2.7.
+
+### _Classic (SOD) :_
+
+- TOC update for Classic SOD 1.15.2.
+
+### _Cata Classic :_
+
+- Initial support for Cata Classic 4.4.0.
+- Known bug : The talent switcher is actually bugged, a fix is planned ASAP.
---
+++ AddOns/XIV_Databar_Continued/Core/changelog/2.8.lua
@@ -0,0 +1,48 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[2800] = {
+    version_string = "2.8",
+    release_date = "2024/05/16",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[正式版] 更新 TOC 的遊戲版本編號 10.2.7。",
+            "[經典版] 更新探索賽季 TOC 的遊戲版本編號 1.15.2。",
+        },
+        ["enUS"] = {
+            "[Retail] TOC update for Retail 10.2.7.",
+            "[Classic] TOC update for Classic SOD 1.15.2.",
+        },
+        ["frFR"] = {
+            "[Retail] Mise à jour du TOC pour la version 10.2.7 de Retail.",
+            "[Classic] Mise à jour du TOC pour la version 1.15.2 de Classic - Saison de la découverte.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[浩劫與重生經典版] 初次支援 4.4.0。",
+            "[浩劫與重生經典版] 已知問題 : 天賦切換有問題，已計劃會盡快修復。",
+        },
+        ["enUS"] = {
+            "[Cata Classic] Initial support for Cata Classic 4.4.0.",
+            "[Cata Classic] Known bug : The talent switcher is actually bugged, a fix is planned ASAP.",
+        },
+        ["frFR"] = {
+            "[Cata Classic] Mise en compatibilité initiale pour Cata Classic 4.4.0",
+            "[Cata Classic] Bug connu : Le sélecteur de talents est actuellement non fonctionnel, un correctif sortira dès que possible.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
