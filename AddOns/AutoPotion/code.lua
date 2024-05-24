b86cbb502ccd197f7755d54033f25b436ac28407
class2u <class2u@gmail.com>
2024-05-24 18:44:04 +0800

    更新 一鍵吃糖 3.4.0

--- AddOns/AutoPotion/code.lua
+++ AddOns/AutoPotion/code.lua
@@ -2,6 +2,8 @@ local addonName, ham = ...
 local macroName = "AutoPotion"
 local isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 local isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
+local isCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
+
 
 ham.myPlayer = ham.Player.new()
 
