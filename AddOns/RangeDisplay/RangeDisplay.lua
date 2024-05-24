7e5641c8044bcc5fce4b7a91507e66c2b12569c2
class2u <class2u@gmail.com>
2024-05-24 10:17:20 +0800

    更新 顯示距離 6.1.0
    # Conflicts:
    #	AddOns/RangeDisplay/RangeDisplay_Cata.toc
    #	AddOns/RangeDisplay/RangeDisplay_TBC.toc
    #	AddOns/RangeDisplay/RangeDisplay_Vanilla.toc
    #	AddOns/RangeDisplay/RangeDisplay_Wrath.toc
    #	AddOns/RangeDisplay/libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
    #	AddOns/RangeDisplay/libs/LibDualSpec-1.0/LibDualSpec-1.0.toc
    #	AddOns/RangeDisplay/libs/LibRangeCheck-3.0/LibRangeCheck-3.0.toc
    #	AddOns/RangeDisplay/libs/LibRangeCheck-3.0/LibRangeCheck-3.0_Vanilla.toc
    #	AddOns/RangeDisplay/libs/LibRangeCheck-3.0/LibRangeCheck-3.0_Wrath.toc
    #	AddOns/RangeDisplay_Options/RangeDisplay_Options_TBC.toc
    #	AddOns/RangeDisplay_Options/RangeDisplay_Options_Wrath.toc

--- AddOns/RangeDisplay/RangeDisplay.lua
+++ AddOns/RangeDisplay/RangeDisplay.lua
@@ -8,7 +8,7 @@ License: Public Domain
 
 local AppName, RangeDisplay = ...
 local OptionsAppName = AppName .. "_Options"
-local VERSION = AppName .. "-v6.0.7"
+local VERSION = AppName .. "-v6.1.0"
 
 local rc = LibStub("LibRangeCheck-3.0")
 local LSM = LibStub:GetLibrary("LibSharedMedia-3.0", true)
