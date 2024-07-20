0dc05181be3cc2602276256c1fca551acb760779
class2u <class2u@gmail.com>
2024-07-21 00:10:32 +0800

    修正 塑形外觀預覽：11.0 相容性

--- AddOns/AppearanceTooltip/config.lua
+++ AddOns/AppearanceTooltip/config.lua
@@ -206,7 +206,10 @@ do
     -- customRaceDropdown:SetPoint("LEFT", customModel.Text, "RIGHT", 12, -2)
     -- customGenderDropdown:SetPoint("TOPLEFT", customRaceDropdown, "TOPRIGHT", 4, 0)
 
-    InterfaceOptions_AddCategory(panel)
+    -- InterfaceOptions_AddCategory(panel)
+	local category = Settings.RegisterCanvasLayoutCategory(panel, panel.name)
+	category.ID = panel.name
+	Settings.RegisterAddOnCategory(category)
 end
 
 -- Overlay config
@@ -252,13 +255,16 @@ do
     encounterjournal:SetPoint("TOPLEFT", loot, "BOTTOMLEFT", 0, -4)
 	setjournal:SetPoint("TOPLEFT", encounterjournal, "BOTTOMLEFT", 0, -4)
 
-    InterfaceOptions_AddCategory(panel)
+    -- InterfaceOptions_AddCategory(panel)
+	local category = Settings.RegisterCanvasLayoutCategory(panel, panel.name)
+	category.ID = panel.name
+	Settings.RegisterAddOnCategory(category)
 end
 
 -- Slash handler
 SlashCmdList.APPEARANCETOOLTIP = function(msg)
-    InterfaceOptionsFrame_OpenToCategory("塑形預覽")
-    InterfaceOptionsFrame_OpenToCategory("塑形預覽")
+    -- InterfaceOptionsFrame_OpenToCategory("塑形預覽")
+    Settings.OpenToCategory(myname)("塑形預覽")
 end
 SLASH_APPEARANCETOOLTIP1 = "/appearancetooltip"
 SLASH_APPEARANCETOOLTIP2 = "/aptip"
