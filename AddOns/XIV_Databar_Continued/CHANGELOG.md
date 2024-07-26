6c7005bdbde145facd00e221cde7f43565991688
class2u <class2u@gmail.com>
2024-07-26 21:18:00 +0800

    更新 功能資訊列 3.0.2

--- AddOns/XIV_Databar_Continued/CHANGELOG.md
+++ AddOns/XIV_Databar_Continued/CHANGELOG.md
@@ -1,13 +1,9 @@
 # XIV_Databar Continued
 
-## [3.0.1](https://github.com/ZelionGG/XIV_Databar-Continued/tree/v3.0.1) (2024-07-24)
+## [3.0.2](https://github.com/ZelionGG/XIV_Databar-Continued/tree/v3.0.2) (2024-07-25)
 
-[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v3.0...v3.0.1) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
+[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v3.0.1...v3.0.2) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
 
 ### _Retail :_
 
-- Micromenu module fix for Retail 11.0.0.
-
-### _Classic SoD :_
-
-- Micromenu module fix (Guild button) for Classic SOD 1.15.3.
+- Currency module fix for Retail 11.0.0.
---
+++ AddOns/XIV_Databar_Continued/Core/changelog/3.0.2.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3020] = {
+    version_string = "3.0.2",
+    release_date = "2024/07/25",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] [Currency] module fix for Retail 11.0.0.",
+        },
+        ["frFR"] = {
+            "[Retail] Correctif du module [Monnaie] pour la version 11.0.0 de Retail.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
