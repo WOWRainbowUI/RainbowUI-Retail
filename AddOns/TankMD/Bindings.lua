d74a038b283d759df0480a68bb8ce229fe1e2a13
class2u <class2u@gmail.com>
2024-08-15 07:18:34 +0800

    更新 一鍵誤導 v3.0.4

--- AddOns/TankMD/Bindings.lua
+++ AddOns/TankMD/Bindings.lua
@@ -7,13 +7,14 @@ local TankMD = AceAddon:GetAddon("TankMD")
 
 local _, class = UnitClass("player")
 local spellId = TankMD:GetMisdirectSpellID(class)
-local spell = GetSpellInfo(spellId)
+local GetSpellName = GetSpellInfo or C_Spell.GetSpellName
+local spellName = GetSpellName(spellId)
 local roleKey = TankMD:GetMisdirectTargetRole(class)
 
 _G["BINDING_HEADER_TANKMD"] = L.title
 
-_G["BINDING_NAME_CLICK TankMDButton1:LeftButton"] = L.to_first:format(spell, L[roleKey])
-_G["BINDING_NAME_CLICK TankMDButton2:LeftButton"] = L.to_second:format(spell, L[roleKey])
-_G["BINDING_NAME_CLICK TankMDButton3:LeftButton"] = L.to_third:format(spell, L[roleKey])
-_G["BINDING_NAME_CLICK TankMDButton4:LeftButton"] = L.to_fourth:format(spell, L[roleKey])
-_G["BINDING_NAME_CLICK TankMDButton5:LeftButton"] = L.to_fifth:format(spell, L[roleKey])
+_G["BINDING_NAME_CLICK TankMDButton1:LeftButton"] = L.to_first:format(spellName, L[roleKey])
+_G["BINDING_NAME_CLICK TankMDButton2:LeftButton"] = L.to_second:format(spellName, L[roleKey])
+_G["BINDING_NAME_CLICK TankMDButton3:LeftButton"] = L.to_third:format(spellName, L[roleKey])
+_G["BINDING_NAME_CLICK TankMDButton4:LeftButton"] = L.to_fourth:format(spellName, L[roleKey])
+_G["BINDING_NAME_CLICK TankMDButton5:LeftButton"] = L.to_fifth:format(spellName, L[roleKey])
--- AddOns/TankMD/CHANGELOG.md
+++ AddOns/TankMD/CHANGELOG.md
@@ -1,7 +1,6 @@
 # TankMD
 
-## [v3.0.2](https://github.com/Oppzippy/TankMD/tree/v3.0.2) (2024-07-24)
-[Full Changelog](https://github.com/Oppzippy/TankMD/compare/v3.0.1...v3.0.2) [Previous Releases](https://github.com/Oppzippy/TankMD/releases)
+## [v3.0.4](https://github.com/Oppzippy/TankMD/tree/v3.0.4) (2024-08-14)
+[Full Changelog](https://github.com/Oppzippy/TankMD/compare/v3.0.3...v3.0.4) [Previous Releases](https://github.com/Oppzippy/TankMD/releases)
 
-- Update Interface version (#14)  
-    Co-authored-by: Oppzippy <Oppzippy@users.noreply.github.com>  
+- Fix misdirect bindings for 11.0.2  
