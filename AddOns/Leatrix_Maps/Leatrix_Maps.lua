088da5282ea0bb867c1688e9c7a12289d7391a01
class2u <class2u@gmail.com>
2024-08-14 20:00:33 +0800

    更新 世界地圖增強 11.0.04

--- AddOns/Leatrix_Maps/Leatrix_Maps.lua
+++ AddOns/Leatrix_Maps/Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 11.0.02 (31st July 2024)
+	-- 	Leatrix Maps 11.0.04 (14th August 2024)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaConfigList = {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "11.0.02"
+	LeaMapsLC["AddonVer"] = "11.0.04"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
