624b04ebf52a49b1a7532929ad633ba14c6ebeaa
class2u <class2u@gmail.com>
2024-08-17 10:46:00 +0800

    更新 重新載入按鈕 v2.2

--- AddOns/ReloadUI/ReloadUI.lua
+++ AddOns/ReloadUI/ReloadUI.lua
@@ -1,28 +1,32 @@
--- ReloadUI
 -- Made by Sharpedge_Gaming
--- v2.1 - 10.2.5
-
+-- v2.2 - 11.0.2
+--[[
 local function CreateGameMenuButton()
     local button = CreateFrame("Button", "GameMenuButtonReloadButton", GameMenuFrame, "GameMenuButtonTemplate")
     button:SetText(RELOADUI)
+    
+    button:SetSize(200, 30)  
+    
     button:SetScript("OnClick", function()
         PlaySound(SOUNDKIT.IG_MAINMENU_LOGOUT)
         ReloadUI()
     end)
+	
+	 button:GetFontString():SetFont(STANDARD_TEXT_FONT, 15) 
 
-    if GameMenuFrame_UpdateVisibleButtons then
-        hooksecurefunc("GameMenuFrame_UpdateVisibleButtons", function()
-            GameMenuFrame:SetHeight(GameMenuFrame:GetHeight() + 25)
-            local point, relativeTo, relativePoint, x, y = GameMenuButtonLogout:GetPoint(1)
-            if relativeTo and relativeTo ~= button then
-                button:SetPoint(point, relativeTo, relativePoint, x, y - 1)
-            end
+    GameMenuFrame:HookScript("OnShow", function()
+        GameMenuFrame:SetHeight(GameMenuFrame:GetHeight() + 25)
+
+        if GameMenuButtonLogout then
+            button:SetPoint("TOPLEFT", GameMenuFrame, "TOPLEFT", 28, -315)  
             GameMenuButtonLogout:ClearAllPoints()
-            GameMenuButtonLogout:SetPoint("TOP", button, "BOTTOM", 0, -1)
-        end)
-    end
+            GameMenuButtonLogout:SetPoint("TOP", button, "BOTTOM", 0, -1)  
+        else
+            button:SetPoint("TOPLEFT", GameMenuFrame, "TOPLEFT", 28, -115)
+        end
+    end)
 end
-
+--]]
 local function CreateSettingsPanelButton()
     local button = CreateFrame("Button", "SettingsPanelReloadUI", SettingsPanel, "UIPanelButtonTemplate")
     button:SetText(RELOADUI)
@@ -36,7 +40,7 @@ local function CreateSettingsPanelButton()
 end
 
 local function InitializeButtons()
-    CreateGameMenuButton()
+    -- CreateGameMenuButton()
     CreateSettingsPanelButton()
 end
 
@@ -44,10 +48,13 @@ local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("ADDON_LOADED")
 eventFrame:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1 == "ReloadUI" then
-        InitializeButtons()
+        C_Timer.After(0.1, InitializeButtons)
         self:UnregisterEvent("ADDON_LOADED")
     end
 end)
+
+
+
 
 
 
