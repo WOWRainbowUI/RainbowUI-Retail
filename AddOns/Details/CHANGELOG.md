af99f46ae8bf495f6631936580c4a7437b15f884
class2u <class2u@gmail.com>
2024-08-20 09:41:29 +0800

    更新 Details.12879.159
    # Conflicts:
    #	AddOns/Details/Details.toc
    #	AddOns/Details/locales/Details-esES.lua
    #	AddOns/Details_Compare2/Details_Compare2.toc
    #	AddOns/Details_RaidCheck/Details_RaidCheck-Wrath.toc
    #	AddOns/Details_RaidCheck/Details_RaidCheck.toc
    #	AddOns/Details_RaidCheck/Details_RaidCheck_Cata.toc
    #	AddOns/Details_RaidCheck/Details_RaidCheck_Wrath.toc
    #	AddOns/Details_Streamer/Details_Streamer-Wrath.toc
    #	AddOns/Details_Streamer/Details_Streamer.lua
    #	AddOns/Details_Streamer/Details_Streamer_Cata.toc
    #	AddOns/Details_Streamer/Details_Streamer_Mainline.toc
    #	AddOns/Details_Streamer/Details_Streamer_Wrath.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat-Wrath.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat_Cata.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat_Vanilla.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat_Wrath.toc
    #	AddOns/Details_Vanguard/Details_Vanguard-Wrath.toc
    #	AddOns/Details_Vanguard/Details_Vanguard_Cata.toc
    #	AddOns/Details_Vanguard/Details_Vanguard_Mainline.toc
    #	AddOns/Details_Vanguard/Details_Vanguard_Wrath.toc

--- AddOns/Details/CHANGELOG.md
+++ AddOns/Details/CHANGELOG.md
@@ -1,8 +1,8 @@
 # Details! Damage Meter
 
-## [Details.12877.159](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.12877.159) (2024-08-17)
-[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.12869.159...Details.12877.159) 
+## [Details.12879.159](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.12879.159) (2024-08-19)
+[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.12878.159...Details.12879.159) 
 
-- Fixed an issue with player names cache getting 'Unknown' from UnitName()  
-- removed debug prints  
-- a few fixes  
+- Fixed Streamer plugin bug  
+- Open Raid update  
+- Removed deprecated code and fixes  
--- AddOns/Details/startup.lua
+++ AddOns/Details/startup.lua
@@ -377,25 +377,6 @@ function Details222.StartUp.StartMeUp()
 
 	--check is this is the first run of this version
 	if (Details.is_version_first_run) then
-		local breakdownData = Details.breakdown_spell_tab
-		if (breakdownData) then
-			local spellContainerHeaders = breakdownData.spellcontainer_headers
-			if (spellContainerHeaders) then
-				if (spellContainerHeaders.overheal) then
-					spellContainerHeaders.overheal.enabled = true
-					spellContainerHeaders.overheal.width = 70
-				end
-			end
-
-			local targetContainerHeaders = breakdownData.targetcontainer_headers
-			if (targetContainerHeaders) then
-				if (targetContainerHeaders.overheal) then
-					targetContainerHeaders.overheal.enabled = true
-					targetContainerHeaders.overheal.width = 70
-				end
-			end
-		end
-
 		local lowerInstanceId = Details:GetLowerInstanceNumber()
 		if (lowerInstanceId) then
 			lowerInstanceId = Details:GetInstance(lowerInstanceId)
@@ -432,6 +413,20 @@ function Details222.StartUp.StartMeUp()
 					GarrisonMissionTutorialFrame:Hide()
 				end)
 				GarrisonMissionTutorialFrame:Hide()
+			end
+			if (addOnName == "Blizzard_VoidStorageUI") then
+				VoidStorageBorderFrameMouseBlockFrame:HookScript("OnShow", function(self)
+					VoidStorageBorderFrameMouseBlockFrame:Hide();
+					VoidStoragePurchaseFrame:Hide();
+					VoidStorageBorderFrame.Bg:Hide();
+
+					if (not CanUseVoidStorage()) then
+						VoidStoragePurchaseFrame:Show();
+					end
+				end)
+				VoidStorageBorderFrameMouseBlockFrame:Hide();
+				VoidStoragePurchaseFrame:Hide();
+				VoidStorageBorderFrame.Bg:Hide();
 			end
 		end)
 	end
