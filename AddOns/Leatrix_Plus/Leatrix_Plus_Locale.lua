5398084adcd5238971852d689be1b86173d69458
class2u <class2u@gmail.com>
2024-08-23 21:21:27 +0800

    更新 功能百寶箱 11.0.05
    # Conflicts:
    #	AddOns/Leatrix_Plus/Leatrix_Plus_Locale.lua

--- AddOns/Leatrix_Plus/Leatrix_Plus_Locale.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus_Locale.lua
@@ -2428,4 +2428,5 @@ L["Cannot be used with Easy Frames"] = "此功能無法與 '暴雪頭像' 插件
 L["Manage vehicle"] = "管理坐騎座位"
 L["Mute custom sounds"] = "自訂音效靜音"
 L["Keep audio synced"] = "使用系統音效裝置"
+L["Block requested invites"] = "封鎖請求加入隊伍"
 end
