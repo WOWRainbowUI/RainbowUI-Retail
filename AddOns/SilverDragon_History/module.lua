ef67e5ed5e675b519bdb453994442e08933bfa13
class2u <class2u@gmail.com>
2024-08-25 16:06:17 +0800

    調整 稀有怪獸與牠們的產地：改為預設不顯示空的歷史記錄小視窗。

--- AddOns/SilverDragon_History/module.lua
+++ AddOns/SilverDragon_History/module.lua
@@ -24,7 +24,7 @@ function module:OnInitialize()
 			grow = true,
 			-- locked = true,
 			relative = true,
-			empty = true,
+			empty = false, -- 更改預設值
 			combat = false,
 			othershard = "dim", -- show / dim / hide
 			sources = {
