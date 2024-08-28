21ffdcb4cdd7db3d8797c692d267e370ea5e8a17
class2u <class2u@gmail.com>
2024-08-28 17:35:15 +0800

    更新 嗜血音樂 11.0.2：新增 11 版和 NPC 一起打副本時，NPC 施放的嗜血法術。

--- AddOns/EnhBloodlust/hawayconfig.lua
+++ AddOns/EnhBloodlust/hawayconfig.lua
@@ -10,20 +10,20 @@ EnhBloodlustConfig = {};
 local config = EnhBloodlustConfig;
 
 -- Bloodlust, Heroism, Time Warp, Ancient Hysteria
-config.spells = {441076, 390386, 2825, 80353, 264667, 32182, 381301}
+config.spells = {428941, 441076, 390386, 2825, 80353, 264667, 32182, 381301}
 
 config.sound = {
 	-- 加入音樂檔案路徑，一行一首歌，結尾加上逗號，會隨機播放。
 	-- 最後一首歌 (最後一行) 的結尾不要加逗號。
 	-- 每一行前面加上兩條橫線的歌曲不會播放。
-	-- "Interface\\AddOns\\EnhBloodlust\\音樂檔案名稱.副檔名",
-	"Interface/AddOns/EnhBloodlust/IDOLxBling-Bang-Bang-Born.mp3"
+	-- "Interface/AddOns/EnhBloodlust/音樂檔案名稱.副檔名",
+	"Interface/AddOns/EnhBloodlust/music.mp3"
 }
 
 config.soundShort = {
 	-- 喚能師套裝觸發15秒嗜血的短音效，寫法和上述相同。
 	-- 不要播放的話，將下面這一行改為 "" 即可。
-	"Interface/AddOns/EnhBloodlust/超棒der哭哭睡_short_15s.mp3"
+	"Interface/AddOns/EnhBloodlust/short.mp3"
 }
 
 -- 音樂長度建議40秒，剛好是嗜血的時間。
