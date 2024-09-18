f75d1394ce664ef828924aebfb3649503e362d43
class2u <class2u@gmail.com>
2024-09-18 13:12:13 +0800

    更新 Details! 戰鬥統計 13066.161

--- AddOns/Details/startup.lua
+++ AddOns/Details/startup.lua
@@ -70,8 +70,8 @@ function Details222.StartUp.StartMeUp()
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --initialize
 
-	--make an encounter journal cache
-	C_Timer.After(1, Details222.EJCache.CreateEncounterJournalDump)
+	--make an encounter journal cache. the cache will load before this if any function tries to get information from the cache
+	C_Timer.After(3, Details222.EJCache.CreateEncounterJournalDump)
 
 	--plugin container
 	Details:CreatePluginWindowContainer()
@@ -279,6 +279,10 @@ function Details222.StartUp.StartMeUp()
 		Details.listener:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 		Details.listener:RegisterEvent("PLAYER_ENTERING_WORLD")
 
+		if (C_EventUtils.IsEventValid("SCENARIO_COMPLETED")) then
+			Details.listener:RegisterEvent("SCENARIO_COMPLETED")
+		end
+
 		Details.listener:RegisterEvent("ENCOUNTER_START")
 		Details.listener:RegisterEvent("ENCOUNTER_END")
 
@@ -336,7 +340,6 @@ function Details222.StartUp.StartMeUp()
 			Details:SendEvent("GROUP_ONLEAVE")
 		end
 
-		Details.last_zone_type = "INIT"
 		Details.parser_functions:ZONE_CHANGED_NEW_AREA()
 		Details.AnnounceStartup = nil
 	end
