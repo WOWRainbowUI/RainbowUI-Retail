13476ef87f38152e50bacc29870537ce1cdc3b22
class2u <class2u@gmail.com>
2024-09-18 22:30:36 +0800

    更新 功能百寶箱 11.0.09

--- AddOns/Leatrix_Plus/Leatrix_Plus.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 11.0.08 (11th September 2024)
+-- 	Leatrix Plus 11.0.09 (18th September 2024)
 ----------------------------------------------------------------------
 
 --	01:Functions 02:Locks,  03:Restart 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "11.0.08"
+	LeaPlusLC["AddonVer"] = "11.0.09"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
