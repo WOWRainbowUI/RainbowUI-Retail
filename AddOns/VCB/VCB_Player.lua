781d95a960ded80cce56b9cacdb48062d6cf34d3
class2u <class2u@gmail.com>
2024-10-17 20:37:39 +0800

    更新 內建施法條增強 1.53

--- AddOns/VCB/VCB_Player.lua
+++ AddOns/VCB/VCB_Player.lua
@@ -514,7 +514,7 @@ PlayerCastingBarFrame:HookScript("OnUpdate", function(self)
 	end
 	VCBnameText:SetText(self.Text:GetText())
 	if self.barType == "channel" and VCBrPlayer["Ticks"] == "Show" then
-		vcbShowTicks(arg3)
+		vcbShowTicks(VCBarg3)
 	else
 		vcbHideTicks()
 	end
