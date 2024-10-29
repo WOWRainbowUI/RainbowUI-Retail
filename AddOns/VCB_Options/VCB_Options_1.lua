d585554664d60f67ef6e37a5a19e84904e17c455
class2u <class2u@gmail.com>
2024-10-29 10:09:04 +0800

    更新 內建施法條增強 1.58

--- AddOns/VCB_Options/VCB_Options_1.lua
+++ AddOns/VCB_Options/VCB_Options_1.lua
@@ -448,14 +448,15 @@ end)
 -- naming --
 vcbOptions1Box7PopOut1Choice0.Text:SetText("Default Color")
 vcbOptions1Box7PopOut1Choice1.Text:SetText("Class' Color")
-vcbOptions1Box7PopOut1Choice2.Text:SetText("Spell School Color")
+vcbOptions1Box7PopOut1Choice2.Text:SetText("Faction Color")
+vcbOptions1Box7PopOut1Choice3.Text:SetText("Spell School Color")
 -- parent & sort --
-for i = 1, 2, 1 do
+for i = 1, 3, 1 do
 	_G["vcbOptions1Box7PopOut1Choice"..i]:SetParent(vcbOptions1Box7PopOut1Choice0)
 	_G["vcbOptions1Box7PopOut1Choice"..i]:SetPoint("TOP", _G["vcbOptions1Box7PopOut1Choice"..i-1], "BOTTOM", 0, 0)
 end
 -- clicking --
-for i = 0, 2, 1 do
+for i = 0, 3, 1 do
 	_G["vcbOptions1Box7PopOut1Choice"..i]:HookScript("OnClick", function(self, button, down)
 		if button == "LeftButton" and down == false then
 			VCBrPlayer["Color"] = self.Text:GetText()
