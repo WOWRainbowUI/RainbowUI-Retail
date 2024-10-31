0ed711c1d4d09a3ecfe19312f289cd43cd4f29c8
class2u <class2u@gmail.com>
2024-10-31 11:58:38 +0800

    更新 一鍵誤導/偷天/啟動 v3.2.2

--- AddOns/TankMD/Locales/ruRU.lua
+++ AddOns/TankMD/Locales/ruRU.lua
@@ -6,33 +6,20 @@ L.TANK = _G['TANK']
 L.HEALER = _G['HEALER']
 L.DAMAGER = _G['DAMAGER']
 
---[[Translation missing --]]
---[[ L["button_target"] = "Button %d: %s"--]] 
---[[Translation missing --]]
---[[ L["general"] = "General"--]] 
---[[Translation missing --]]
---[[ L["main_tanks_only"] = "Main Tanks Only"--]] 
---[[Translation missing --]]
---[[ L["prioritize_focus"] = "Prioritize Focus"--]] 
---[[Translation missing --]]
---[[ L["prioritize_focus_desc"] = "If enabled, your focus will be the highest priority. Otherwise, your focus will be ignored."--]] 
---[[Translation missing --]]
---[[ L["prioritize_main_tanks"] = "Prioritize Main Tanks, Include Tanks"--]] 
---[[Translation missing --]]
---[[ L["profiles"] = "Profiles"--]] 
---[[Translation missing --]]
---[[ L["tank_role_only"] = "Tank Role Only"--]] 
---[[Translation missing --]]
---[[ L["tank_selection_method"] = "Tank Selection Method"--]] 
---[[Translation missing --]]
---[[ L["tanks_and_main_tanks"] = "Tanks and Main Tanks"--]] 
+L["button_target"] = "Кнопка %d: %s"
+L["general"] = "Общие"
+L["main_tanks_only"] = "Только основные танки"
+L["prioritize_focus"] = "Приоритет фокуса"
+L["prioritize_focus_desc"] = "Если этот параметр включен, Ваш фокус будет иметь высший приоритет. В противном случае Ваш фокус будет проигнорирован."
+L["prioritize_main_tanks"] = "Приоритет для основных танков, включить танков"
+L["profiles"] = "Профили"
+L["tank_role_only"] = "Только роль танка"
+L["tank_selection_method"] = "Метод выбора танка"
+L["tanks_and_main_tanks"] = "Танки и основные танки"
 L["title"] = "TankMD"
---[[Translation missing --]]
---[[ L["to_fifth"] = "%s to fifth %s"--]] 
+L["to_fifth"] = "%s к пятому %s"
 L["to_first"] = "%s к первому %s"
---[[Translation missing --]]
---[[ L["to_fourth"] = "%s to fourth %s"--]] 
+L["to_fourth"] = "%s к четвёртому %s"
 L["to_second"] = "%s ко второму %s"
---[[Translation missing --]]
---[[ L["to_third"] = "%s to third %s"--]] 
+L["to_third"] = "%s к третьему %s"
 
