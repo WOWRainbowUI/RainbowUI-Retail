357242cd1671066374bba9a4acfc7171045b506d
class2u <class2u@gmail.com>
2024-11-02 18:06:40 +0800

    更新 威力血條 12.0.12

--- AddOns/TidyPlates_ThreatPlates/Commands.lua
+++ AddOns/TidyPlates_ThreatPlates/Commands.lua
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------------------------------------
 local L = Addon.ThreatPlates.L
 
-Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "12.0.11"
+Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "12.0.12"
 
 local function toggleDPS()
 	if Addon.db.profile.optionRoleDetectionAutomatic then
