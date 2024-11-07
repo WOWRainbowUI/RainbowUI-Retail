f9c25833585e8c81097a2f211299621152d9cf2b
class2u <class2u@gmail.com>
2024-11-07 09:13:45 +0800

    更新 大型聊天輸入框 1.0.12

--- AddOns/InputInput/Main.lua
+++ AddOns/InputInput/Main.lua
@@ -581,9 +581,15 @@ local ChatLabels = {
 function HideEuiBorder(editBox)
 	if ElvUI then
 		---@diagnostic disable-next-line: undefined-field
-		editBox:SetBackdropBorderColor(0, 0, 0, 0)
+		if editBox.SetBackdropBorderColor then
+			---@diagnostic disable-next-line: undefined-field
+			editBox:SetBackdropBorderColor(0, 0, 0, 0)
+		end
 		---@diagnostic disable-next-line: undefined-field
-		editBox:SetBackdropColor(0, 0, 0, 0)
+		if editBox.SetBackdropColor then
+			---@diagnostic disable-next-line: undefined-field
+			editBox:SetBackdropColor(0, 0, 0, 0)
+		end
 		-- editBox:StripTextures()
 		---@diagnostic disable-next-line: undefined-field
 		if editBox.shadow then
@@ -597,6 +603,17 @@ function HideEuiBorder(editBox)
 			---@diagnostic disable-next-line: undefined-field
 			editBox.characterCount:Hide()
 		end
+	end
+end
+
+function HideLS_GLASSBorder(editBox)
+	if ls_Glass then
+		LoadPostion(editBox)
+		---@diagnostic disable-next-line: undefined-field
+		editBox.Backdrop:SetBackdropColor(0, 0, 0, 0)
+		---@diagnostic disable-next-line: undefined-field
+		editBox.Backdrop:SetBackdropBorderColor(0, 0, 0, 0)
+		editBox:SetTextInsets(10, 10, 0, 0)
 	end
 end
 
@@ -953,7 +970,10 @@ local function eventSetup(editBox, bg, border, backdropFrame2, resizeButton, tex
 			M.HISTORY:simulateInputChange(text, self:GetInputLanguage())
 		end
 		last_text = text
-		lastChannel = self:GetAttribute("chatType")
+		-- lastChannel = self:GetAttribute("chatType")
+		if ls_Glass then
+			-- editBox:SetTextInsets(10, 10, 0, 0)
+		end
 	end)
 	editBox:HookScript("OnInputLanguageChanged", function(self)
 		II_LANG:SetText(_G["INPUT_" .. self:GetInputLanguage()])
@@ -970,7 +990,7 @@ local function eventSetup(editBox, bg, border, backdropFrame2, resizeButton, tex
 			end
 		elseif key == "UP" then
 			-- 上滚历史消息
-			if not ElvUI then
+			if not ElvUI or ls_Glass then
 				if historyIndex > 1 then
 					historyIndex = historyIndex - 1
 					local h = messageHistory[historyIndex]
@@ -979,7 +999,7 @@ local function eventSetup(editBox, bg, border, backdropFrame2, resizeButton, tex
 				end
 			end
 		elseif key == "DOWN" then
-			if not ElvUI then
+			if not ElvUI or ls_Glass then
 				-- 下滚历史消息
 				if historyIndex < #messageHistory then
 					historyIndex = historyIndex + 1
@@ -1012,6 +1032,7 @@ local function eventSetup(editBox, bg, border, backdropFrame2, resizeButton, tex
 	-- 设置焦点获得事件处理函数
 	editBox:HookScript("OnEditFocusGained", function(self)
 		HideEuiBorder(self)
+		HideLS_GLASSBorder(editBox)
 		-- Elvui 会重置输入框的位置大小
 		if ElvUI then
 			LoadSize(scale, self, backdropFrame2, channel_name, II_TIP, II_LANG)
