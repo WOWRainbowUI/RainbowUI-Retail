2f092e48d4317bd7accb1e0071ec75e86846a43f
class2u <class2u@gmail.com>
2024-11-07 09:03:58 +0800

    更新 一鍵吃糖/喝紅水 3.7.6
    # Conflicts:
    #	AddOns/AutoPotion/AutoPotion.toc
    #	AddOns/AutoPotion/AutoPotion_Cata.toc
    #	AddOns/AutoPotion/AutoPotion_Vanilla.toc
    #	AddOns/AutoPotion/AutoPotion_Wrath.toc

--- AddOns/AutoPotion/Core/Spells.lua
+++ AddOns/AutoPotion/Core/Spells.lua
@@ -5,7 +5,7 @@ local isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 ham.crimsonVialSpell = 185311
 ham.renewal = 108238
 ham.exhilaration = 109304
-ham.fortitudeOfTheBear = 272679
+ham.fortitudeOfTheBear = 388035
 ham.bitterImmunity = 383762
 ham.desperatePrayer = 19236
 ham.expelHarm = 322101
