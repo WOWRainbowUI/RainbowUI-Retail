3451668d8327c00f7d92ba3f48a024935212e653
Dololo <class2u@gmail.com>
2024-11-21 13:11:07 +0800

    更新 編輯模式擴充包 11.0-031

--- AddOns/EditModeExpanded/RegisteredSystemFrames.lua
+++ AddOns/EditModeExpanded/RegisteredSystemFrames.lua
@@ -82,12 +82,14 @@ function addon:registerSecureFrameHideable(frame)
     local onResetFunctionHide = lib:RegisterCustomCheckbox(frame, HIDE,
         function()
             hidden = true
+            if InCombatLockdown() then return end
             if not EditModeManagerFrame.editModeActive then
                 hide()
             end
         end,
         function()
             hidden = false
+            if InCombatLockdown() then return end
             show()
         end,
         "HidePermanently")
@@ -167,13 +169,16 @@ local parentparent = CreateFrame("Frame", nil, UIParent)
 local parent = CreateFrame("Frame", nil, parentparent)
 
 function addon.unlinkClassResourceFrame(frame)
+    local wasChecked
     lib:RegisterCustomCheckbox(frame, L["UNLINK_CLASS_RESOURCE_DESCRIPTION"], 
         --onChecked
         function()
             frame:SetParent(parent)
+            wasChecked = true
         end,
         --onUnchecked
         function()
+            if not wasChecked then return end
             frame:SetParent(PlayerFrameBottomManagedFramesContainer)
         end
     )
