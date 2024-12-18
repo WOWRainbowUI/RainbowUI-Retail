9db47bcbbb3475f6c612665123b29fa88c052b20
Dololo <class2u@gmail.com>
2024-12-18 11:03:39 +0800

    更新 飛行速度條 0.5.1

--- AddOns/DragonRider/Libs/LibAdvFlight-1.0/LibAdvFlight-1.0.lua
+++ AddOns/DragonRider/Libs/LibAdvFlight-1.0/LibAdvFlight-1.0.lua
@@ -1,6 +1,6 @@
 assert(LibStub, "LibStub not found.");
 
-local major, minor = "LibAdvFlight-1.0", 2;
+local major, minor = "LibAdvFlight-1.0", 3;
 
 ---@class LibAdvFlight-1.0
 local LibAdvFlight = LibStub:NewLibrary(major, minor);
@@ -13,7 +13,7 @@ end
 --- These can be registered using the API functions below, or by just registering a callback w/ the global EventRegistry
 ---@class LibAdvFlightEvents
 local Events = {
-    ADV_FLYING_ENABLED = "ADV_FLYING_ENABLED",                              -- Player is able to adv fly (on mount)
+    ADV_FLYING_ENABLED = "ADV_FLYING_ENABLED",                              -- Player is able to adv fly (has mounted up)
     ADV_FLYING_DISABLED = "ADV_FLYING_DISABLED",                            -- Player is no longer able to adv fly (dismounted, no flying zone)
     ADV_FLYING_ENABLE_STATE_CHANGED = "ADV_FLYING_ENABLE_STATE_CHANGED",    -- Player's adv flying enable state has changed - args: isAdvFlyEnabled
 
@@ -25,6 +25,17 @@ local Events = {
     VIGOR_CHANGED = "VIGOR_CHANGED",                                        -- Player's vigor has changed - args: vigorCurrent
 };
 LibAdvFlight.Events = Events;
+
+
+-- Some enum values for standardization
+---@enum LibAdvFlightOptionalSpell
+local OPTIONAL_SPELLS = {
+    SwitchFlightStyle = 436854,
+    LightningRush = 447982,
+    RideAlong = 447959
+};
+
+LibAdvFlight.OptionalSpells = OPTIONAL_SPELLS;
 
 --------------
 
@@ -56,7 +67,7 @@ end
 function EventFrame:OnUpdate()
     local isGliding, canGlide, forwardSpeed = C_PlayerInfo.GetGlidingInfo();
 
-    -- we basically are just going off of the changes in our stored state to trigger events
+    -- we are just going off of the changes in our stored state to trigger events
     -- because the relevant events (PLAYER_CAN_GLIDE_CHANGED and PLAYER_IS_GLIDING_CHANGED)
     -- are unreliable
 
@@ -125,8 +136,6 @@ end
 --------------
 -- public state accessors
 
-local SWITCH_FLIGHT_STYLE_SPELL = 436854;
-
 ---Returns true if the player has unlocked Skyriding
 ---@return boolean advFlyUnlocked
 function LibAdvFlight.IsAdvFlightUnlocked()
@@ -136,7 +145,37 @@ end
 --- Returns true if the player has unlocked the ability to switch flight styles
 ---@return boolean canSwitchFlightStyles
 function LibAdvFlight.CanSwitchFlightStyles()
-    return IsPlayerSpell(SWITCH_FLIGHT_STYLE_SPELL);
+    return LibAdvFlight.HasTalentChoice(OPTIONAL_SPELLS.SwitchFlightStyle);
+end
+
+--- Returns true if the player has chosen the lightning rush talent in the Skyriding skill tree
+---@return boolean canSwitchFlightStyles
+function LibAdvFlight.HasLightningRush()
+    return LibAdvFlight.HasTalentChoice(OPTIONAL_SPELLS.LightningRush);
+end
+
+--- Returns true if the player has enabled the ability to carry friends with ride along
+---@return boolean canSwitchFlightStyles
+function LibAdvFlight.HasRideAlongEnabled()
+    return LibAdvFlight.HasTalentChoice(OPTIONAL_SPELLS.RideAlong);
+end
+
+---@param choice LibAdvFlightOptionalSpell
+---@return boolean hasTalent
+function LibAdvFlight.HasTalentChoice(choice)
+    return IsPlayerSpell(choice)
+end
+
+local LIGHTNING_RUSH_AURA_SPELLID = 418590;
+
+---@return number charges Number of lightning rush charges
+function LibAdvFlight.GetLightningRushCharges()
+    local auraData = C_UnitAuras.GetPlayerAuraBySpellID(LIGHTNING_RUSH_AURA_SPELLID);
+    if not auraData then
+        return 0;
+    end
+
+    return auraData.applications;
 end
 
 ---Returns true if the player is on a Skyriding mount and can Skyride
