4a8187e852cc510ea1c42c80bc9e5fe3f870e425
Dololo <class2u@gmail.com>
2024-12-18 21:29:37 +0800

    更新 功能百寶箱 11.0.23
    # Conflicts:
    #	AddOns/Leatrix_Plus/Leatrix_Plus.toc
    #	AddOns/Leatrix_Plus/Leatrix_Plus_Locale.lua

--- AddOns/Leatrix_Plus/Leatrix_Plus.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 11.0.22 (12th December 2024)
+-- 	Leatrix Plus 11.0.23 (18th December 2024)
 ----------------------------------------------------------------------
 
 --	01:Functions 02:Locks,  03:Restart 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "11.0.22"
+	LeaPlusLC["AddonVer"] = "11.0.23"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
