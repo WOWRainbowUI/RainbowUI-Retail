dbfd64e4b60be8cba678fd879f5e3d8b3bb333a8
Dololo <class2u@gmail.com>
2024-12-20 20:10:56 +0800

    更新 一鍵分解物品 110000.127-Release
    # Conflicts:
    #	AddOns/Molinari/locale/deDE.lua
    #	AddOns/Molinari/locale/esES.lua
    #	AddOns/Molinari/locale/esMX.lua
    #	AddOns/Molinari/locale/frFR.lua
    #	AddOns/Molinari/locale/itIT.lua
    #	AddOns/Molinari/locale/koKR.lua
    #	AddOns/Molinari/locale/ptBR.lua
    #	AddOns/Molinari/locale/ruRU.lua
    #	AddOns/Molinari/locale/zhTW.lua

--- AddOns/Molinari/locale/zhTW.lua
+++ AddOns/Molinari/locale/zhTW.lua
@@ -1,9 +1,5 @@
 local L = select(2, ...).L('zhTW')
 
--- config
-L['ALT key'] = ALT_KEY_TEXT
-L['ALT + CTRL key'] = ALT_KEY_TEXT .. ' + ' .. CTRL_KEY
-L['ALT + SHIFT key'] = ALT_KEY_TEXT .. ' + ' .. SHIFT_KEY
 L['Modifier to activate %s'] = '分解時要按住的輔助鍵'
 
 -- 自行加入
