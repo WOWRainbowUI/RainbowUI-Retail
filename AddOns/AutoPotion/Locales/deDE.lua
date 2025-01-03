4954d56e021efdbf2199a66ae80e5eb30ddee8d4
Dololo <class2u@gmail.com>
2025-01-03 09:32:41 +0800

    更新 一鍵吃糖/喝紅水

--- AddOns/AutoPotion/Locales/deDE.lua
+++ AddOns/AutoPotion/Locales/deDE.lua
@@ -2,12 +2,12 @@ local L = LibStub("AceLocale-3.0"):NewLocale("AutoPotion", "deDE")
 if not L then return end
 
 -- InterfaceOptionsFrame
---L["Addon Behaviour"] = "Addon Behaviour"
---L["Auto Potion Settings"] = "Auto Potion Settings"
+--L["Addon Behaviour"] = "Addon Verhalten"
+--L["Auto Potion Settings"] = "Auto Potion Einstellungen"
 --L["Cavedweller's Delight"] = "Cavedweller's Delight"
---L["Class/Racial Spells"] = "Class/Racial Spells"
+--L["Class/Racial Spells"] = "Klassen/Rassen Zauber"
 --L["Configure the behavior of the addon. IE: if you want to include class spells"] = "Configure the behavior of the addon. IE: if you want to include class spells"
---L["Current Priority"] = "Current Priority"
+--L["Current Priority"] = "Aktuelle Piorität"
 --L["Heartseeking Health Injector (tinker)"] = "Heartseeking Health Injector (tinker)"
 --L["Include /stopcasting in the macro"] = "Include /stopcasting in the macro"
 --L["Includes the shortest Cooldown in the reset Condition of Castsequence. !!USE CAREFULLY!!"] = "Includes the shortest Cooldown in the reset Condition of Castsequence. !!USE CAREFULLY!!"
@@ -18,7 +18,7 @@ if not L then return end
 --L["Potion of Withering Vitality"] = "Potion of Withering Vitality"
 --L["Prioritize health potions over a healthstone."] = "Prioritize health potions over a healthstone."
 --L["Reset successful!"] = "Reset successful!"
---L["Reset to Default"] = "Reset to Default"
+--L["Reset to Default"] = "Zurücksetzen"
 --L["The Settings of AutoPotion were reset due to breaking changes."] = "The Settings of AutoPotion were reset due to breaking changes."
 --L["Useful for casters."] = "Useful for casters."
 
