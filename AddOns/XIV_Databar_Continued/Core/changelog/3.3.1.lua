e682c93c2eec554460c5219af98369126c8143eb
Dololo <class2u@gmail.com>
2025-01-10 11:46:06 +0800

    更新 功能資訊列 3.3.1
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Mainline.toc

---
+++ AddOns/XIV_Databar_Continued/Core/changelog/3.3.1.lua
@@ -0,0 +1,36 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3310] = {
+    version_string = "3.3.1",
+    release_date = "2025/01/08",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[正式服] 更新 TOC 遊戲版本編號 11.0.7 (以及預備 11.1)。"
+        },
+        ["enUS"] = {
+            "[Retail] TOC update for Retail 11.0.7 (and 11.1 prep)."
+        },
+        ["frFR"] = {
+            "[Retail] Mise à jour du TOC pour Retail 11.0.7 (et préparation pour le patch 11.1)."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
