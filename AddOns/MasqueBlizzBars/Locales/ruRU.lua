d3a056f2d8a956b1403b6bacdf68e612a2154f37
Dololo <class2u@gmail.com>
2025-01-30 10:21:02 +0800

    更新 按鈕外觀-內建快捷列 11.1.0.0

---
+++ AddOns/MasqueBlizzBars/Locales/ruRU.lua
@@ -0,0 +1,39 @@
+--
+-- Masque Blizzard Bars
+--
+-- Locales\ruRU.lua -- ruRU Localization File
+--
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file or at
+-- https://opensource.org/licenses/MIT.
+--
+
+-- Please use CurseForge to submit localization content for another language:
+-- https://www.curseforge.com/wow/addons/masque-blizz-bars-revived/localization
+
+local Locale = GetLocale()
+if Locale ~= "ruRU" then return end
+
+local _, Shared = ...
+local L = Shared.Locale
+
+L["Action Bar 1"] = "Панель действий 1"
+L["Action Bar 2"] = "Панель действий 2"
+L["Action Bar 3"] = "Панель действий 3"
+L["Action Bar 4"] = "Панель действий 4"
+L["Action Bar 5"] = "Панель действий 5"
+L["Action Bar 6"] = "Панель действий 6"
+L["Action Bar 7"] = "Панель действий 7"
+L["Action Bar 8"] = "Панель действий 8"
+L["Extra Ability Buttons"] = "Кнопки дополнительных способностей"
+L["Pet Bar"] = "Панель питомца"
+L["Pet Battle Bar"] = "Панель боевого питомца"
+L["Possess Bar"] = "Панель контроля"
+L["Spell Flyouts"] = "Всплывающие окна с заклинаниями"
+L["Stance Bar"] = "Панель стойки"
+L["Vehicle Bar"] = "Панель транспортного средства"
+
+L["NOTES_EXTRA_ABILITY_BUTTONS"] = "В эту группу входит кнопка дополнительного действия, отображаемая во время сражений и заданий, а также все кнопки зональных способностей, отображаемые для способностей, основанных на местоположении. Некоторые кнопки имеют дополнительные фоновые изображения, обрамляющие их, поэтому квадратные скины, как правило, работают лучше всего."
+L["NOTES_SPELL_FLYOUTS"] = "В эту группу входят все всплывающие окна, отображаемые в любой точке игры, такие как панели действий и книга заклинаний."
+L["NOTES_VEHICLE_BAR"] = "Эта панель отображается, когда вы используете транспортное средство со способностями. Кнопка выхода в настоящее время не может быть скрыта."
+
