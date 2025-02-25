4635c655b50b13c372b824f2f40aaaf688bfba50
Dololo <class2u@gmail.com>
2025-02-25 12:02:42 +0800

    更新 大型聊天輸入框 v1.1.0-release-02

--- AddOns/InputInput/Locales/ruRU.lua
+++ AddOns/InputInput/Locales/ruRU.lua
@@ -1,3 +1,112 @@
 local W, M, U, D, G, LOCALE = unpack((select(2, ...)))
 local L = {}
 LOCALE['ruRU'] = L
+
+-- emoji
+L["angel"] = "ангел"
+L["angry"] = "злой"
+L["biglaugh"] = "громкий смех"
+L["clap"] = "аплодисменты"
+L["cool"] = "круто"
+L["cry"] = "плач"
+L["cutie"] = "милашка"
+L["despise"] = "презрение"
+L["dreamsmile"] = "мечтательная улыбка"
+L["embarrass"] = "смущение"
+L["evil"] = "зло"
+L["excited"] = "возбуждённый"
+L["faint"] = "обморок"
+L["fight"] = "бой"
+L["flu"] = "грипп"
+L["freeze"] = "заморозка"
+L["frown"] = "нахмуриться"
+L["greet"] = "приветствие"
+L["grimace"] = "гримаса"
+L["growl"] = "рычание"
+L["happy"] = "счастливый"
+L["heart"] = "сердце"
+L["horror"] = "ужас"
+L["ill"] = "болезнь"
+L["innocent"] = "невинный"
+L["kongfu"] = "кунг-фу"
+L["love"] = "любовь"
+L["mail"] = "почта"
+L["makeup"] = "макияж"
+L["mario"] = "марио"
+L["meditate"] = "медитация"
+L["miserable"] = "несчастный"
+L["okay"] = "окей"
+L["pretty"] = "красивый"
+L["puke"] = "рвота"
+L["shake"] = "тряска"
+L["shout"] = "крик"
+L["shuuuu"] = "шшшш"
+L["shy"] = "застенчивый"
+L["sleep"] = "сон"
+L["smile"] = "улыбка"
+L["suprise"] = "сюрприз"
+L["surrender"] = "сдача"
+L["sweat"] = "пот"
+L["tear"] = "слеза"
+L["tears"] = "слёзы"
+L["think"] = "думать"
+L["titter"] = "хихикать"
+L["ugly"] = "уродливый"
+L["victory"] = "победа"
+L["volunteer"] = "волонтёр"
+L["wronged"] = "обиженный"
+L["wrong"] = "неправильно"
+L["right"] = "правильно"
+L["question"] = "вопрос"
+L["skull"] = "череп"
+L["sheep"] = "овца"
+L["Star"] = "Звезда"
+L["Circle"] = "Круг"
+L["Diamond"] = "Ромб"
+L["Triangle"] = "Треугольник"
+L["Moon"] = "Луна"
+L["Square"] = "Квадрат"
+L["Cross"] = "Крест"
+L["Skull"] = "Череп"
+
+-- 主界面 main frame
+L['unknown'] = "неизвестно"
+L['TO'] = "К"
+
+-- 设置 options
+L['Show Chat'] = "Показывать чат"
+L['Show channel Name'] = 'Показывать название канала'
+L['Show Timestamp'] = "Показывать временную метку"
+L['Show bg'] = "Показывать фон сообщения"
+L['Default Setting'] = "По умолчанию"
+L['Enable InputInput_Libraries_zh'] = 'Включить %s'
+L['Need To Reload'] = 'Требуется перезагрузка'
+L['Do you want to reload the addOnes'] = 'Вы хотите перезагрузить аддоны?'
+L['Yes'] = 'Да'
+L['No'] = 'Нет'
+L['Chinese word processing module can make input prompts more intelligent'] = 'Модуль обработки китайских слов может сделать подсказки более интеллектуальными'
+L['Show multi tip'] = 'Показывать несколько подсказок'
+L['Disable Login Information'] = 'Отключить информацию о входе'
+
+-- Login Information
+L['Login Information 1'] = "Вы используете аддон %s для улучшения ввода. Если вы столкнётесь с проблемами, вы можете оставить отзыв через %s или на сайте аддона %s"
+L['Login Information 2'] = "Введите %s или %s, чтобы открыть %sпанель настроек%s"
+
+L['Not enabled, enter/ii to enable'] = 'Не включено, введите %s, чтобы включить'
+
+L['New Version Discovered'] = '%s Обнаружена новая версия %s'
+
+L['READ ME'] = '- перемещение (Shift+ЛКМ), масштабирование (Shift+правая стрелка), сброс (Shift+ПКМ), в актуальной версии можно войти в режим редактирования для редактирования\
+- Tab для переключения канала\
+- История сообщений （↑ ↓）\
+- автоматическое добавление (Control+цифры)\
+- отмена (Control+Z) повтор (Control+Shift+Z)'
+
+L['scale'] = 'масштаб'
+
+L['InputBox'] = 'Поле ввода чата'
+
+L['TiktokLive'] = 'Tiktok live (только Китай)'
+
+L['nextNoTip'] = "Больше не показывать подсказки"
+L['UpdateTip'] = "Сочетание клавиш для автоматического добавления（|cffF56C6CControl+Enter|r）|cffF56C6Cбольше не используется|r и заменено на（|cff409EFFControl+цифра1|r），Это связано с конфликтом сочетания клавиш с командой \"/\". Хотя это недружественное изменение, чтобы не конфликтовать с интерфейсом по умолчанию Blizzard, пожалуйста, поймите!"
