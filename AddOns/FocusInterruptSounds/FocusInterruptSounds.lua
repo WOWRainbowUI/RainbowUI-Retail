c2d3a7c6a17f53215f0079c0f7cd717bd5c7c020
Dololo <class2u@gmail.com>
2025-03-01 10:43:55 +0800

    更新 斷法提醒與通報 r157

--- AddOns/FocusInterruptSounds/FocusInterruptSounds.lua
+++ AddOns/FocusInterruptSounds/FocusInterruptSounds.lua
@@ -1206,7 +1206,7 @@ function FocusInterruptSounds:COMBAT_LOG_EVENT_UNFILTERED(event)
 	) then
 		if (nil ~= self.strAntiCCSpellName
 			and self:FIsPlayerSpellAvailable(self.strAntiCCSpellName)
-			and (IsActiveBattlefieldArena() or 1 == IsSpellInRange(self.str30YardSpellName, strTarget))
+			and (IsActiveBattlefieldArena() or 1 == C_Spell.IsSpellInRange(self.str30YardSpellName, strTarget))
 		) then
 			self:CheckAndPrintMessage(strSourceName .. " is casting CC: |cffffcc44" .. varParam2 .. "|r.");
 			if (not fHandled) then
