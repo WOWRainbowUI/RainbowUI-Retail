65be078688c7e2595921aa15c892e7df5cb53725
Dololo <class2u@gmail.com>
2025-03-02 09:47:38 +0800

    更新 戰寵助手 11.1.1

--- AddOns/PetTracker/libs/WildAddon-1.0/WildAddon-1.0.lua
+++ AddOns/PetTracker/libs/WildAddon-1.0/WildAddon-1.0.lua
@@ -5,7 +5,7 @@ for events and has behavior changes plus additional features I wanted that were
 incompatible with Ace.
 ]]--
 
-local Lib = LibStub:NewLibrary('WildAddon-1.1', 1)
+local Lib = LibStub:NewLibrary('WildAddon-1.1', 3)
 if not Lib then return end
 
 
@@ -82,7 +82,7 @@ function Embeds:UnregisterEvent(event)
 end
 
 function Embeds:ContinueOn(event, call, ...)
-	EventUtil.RegisterOnceFrameEventAndCallback(event, self[call or event] or call, self, ...)
+	EventUtil.RegisterOnceFrameEventAndCallback(event, GenerateClosure(self[call or event] or call, self), ...)
 end
 
 function Embeds:RegisterSignal(event, call, ...)
