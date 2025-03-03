9854305be15f8ea6e255b73f1ab06fc55fa97bfe
Dololo <class2u@gmail.com>
2025-03-03 08:01:39 +0800

    更新 敵方技能監控 (血條) 110100.0-release
    # Conflicts:
    #	AddOns/NameplateCooldowns/NameplateCooldowns.toc

--- AddOns/NameplateCooldowns/NameplateCooldowns.lua
+++ AddOns/NameplateCooldowns/NameplateCooldowns.lua
@@ -12,7 +12,7 @@ local Trinkets = addonTable.Trinkets;
 local Reductions = addonTable.Reductions;
 
 --@non-debug@
-local buildTimestamp = "110007.1-release";
+local buildTimestamp = "110100.0-release";
 --@end-non-debug@
 
 -- Libraries
