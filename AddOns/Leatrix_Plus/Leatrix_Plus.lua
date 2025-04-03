6eabfd17caa4d0fa1960c486b1d41c73ee174e1f
Dololo <class2u@gmail.com>
2025-04-03 09:21:07 +0800

    更新 功能百寶箱 11.1.07

--- AddOns/Leatrix_Plus/Leatrix_Plus.lua
+++ AddOns/Leatrix_Plus/Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 11.1.06 (27th March 2025)
+-- 	Leatrix Plus 11.1.07 (2nd April 2025)
 ----------------------------------------------------------------------
 
 --	01:Functions 02:Locks,  03:Restart 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "11.1.06"
+	LeaPlusLC["AddonVer"] = "11.1.07"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
