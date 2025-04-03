341bcb7f9e567fd990477fabc0c0ba9d7b931f29
Dololo <class2u@gmail.com>
2025-04-03 09:22:01 +0800

    更新 功能資訊列 3.5.4

---
+++ AddOns/XIV_Databar_Continued/Core/changelog/3.5.4.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3540] = {
+    version_string = "3.5.4",
+    release_date = "2025/04/02",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Fixed The MOTHERLODE!! teleport in the [Travel] Mythic+ module."
+        },
+        ["frFR"] = {
+            "[Retail] Correction de la téléportation vers Le Filon dans les Téléportations Mythiques+ du module [Voyage].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
