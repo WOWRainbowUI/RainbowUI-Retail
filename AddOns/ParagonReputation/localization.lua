f00b0b6cb6ebaff160379f21d83c9041a6d23593
Dololo <class2u@gmail.com>
2025-04-23 10:06:17 +0800

    更新 巔峰聲望條 1.63

--- AddOns/ParagonReputation/localization.lua
+++ AddOns/ParagonReputation/localization.lua
@@ -1,5 +1,5 @@
 		-------------------------------------------------
-		-- Paragon Reputation 1.62 by Fail US-Ragnaros --
+		-- Paragon Reputation 1.63 by Fail US-Ragnaros --
 		-------------------------------------------------
 
 		  --[[	  Special thanks to Ammako for
