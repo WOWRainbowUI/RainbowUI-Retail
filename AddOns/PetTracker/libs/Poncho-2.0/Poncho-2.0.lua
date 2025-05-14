9bcc5b0792372f8ef4b9a465f8e3bf41c9579447
Dololo <class2u@gmail.com>
2025-05-14 10:02:26 +0800

    更新 戰寵助手 11.1.7
    # Conflicts:
    #	AddOns/PetTracker/PetTracker-Mainline.toc

--- AddOns/PetTracker/libs/Poncho-2.0/Poncho-2.0.lua
+++ AddOns/PetTracker/libs/Poncho-2.0/Poncho-2.0.lua
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with Poncho. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-local Lib = LibStub:NewLibrary('Poncho-2.0', 5)
+local Lib = LibStub:NewLibrary('Poncho-2.0', 6)
 if not Lib then return end
 
 local setmetatable, getmetatable, assert, type, tinsert, tremove = setmetatable, getmetatable, assert, type, tinsert, tremove
@@ -33,16 +33,16 @@ local ClassMeta =  {
   end
 }
 
-local SuperCall = {
-  __index = function(tmp, key)
-    local var = ClassMeta.__index(tmp.class, key) or tmp.frame.__base[key]
+local SuperCall = setmetatable({}, {
+  __index = function(super, key)
+    local var = ClassMeta.__index(super.class, key) or super.frame.__base[key]
     if type(var) == 'function' then
-      return function(tmp, ...)  return var(tmp.frame, ...) end
+      return function(super, ...)  return var(super.frame, ...) end
     end
 
     return var
   end
-}
+})
 
 
 --[[ Static ]]--
@@ -157,7 +157,8 @@ end
 
 function Base:Super(class)
   assert(class, 'No argument #1 to `:Super`')
-  return setmetatable({class = class, frame = self}, SuperCall)
+  SuperCall.class, SuperCall.frame = class, self
+  return SuperCall
 end
 
 
