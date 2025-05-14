b8764fa39e9b04f388e3701dc4b1aeb10640a604
Dololo <class2u@gmail.com>
2025-05-14 10:00:43 +0800

    更新 多角色物品統計 200
    # Conflicts:
    #	AddOns/Syndicator/Syndicator_Cata.toc
    #	AddOns/Syndicator/Syndicator_Mists.toc
    #	AddOns/Syndicator/Syndicator_Vanilla.toc
    #	AddOns/Syndicator/Syndicator_Wrath.toc

--- AddOns/Syndicator/Search/Utilities.lua
+++ AddOns/Syndicator/Search/Utilities.lua
@@ -16,21 +16,33 @@ function Syndicator.Search.GetExpansionInfo(itemID)
       return itemVersionDetails.major, itemVersionDetails.minor, itemVersionDetails.patch
     end
   end
-  if Syndicator.Constants.IsClassic and ATTC and ATTC.SearchForField then
-    local attResults = ATTC.SearchForField("itemID", itemID)
-    if #attResults > 0 then
-      local parent = attResults[1]
-      while parent and parent.awp == nil do -- awp: short for added with patch
-        parent = parent.parent
+  if ATTC and ATTC.SearchForField then
+    local results = ATTC.SearchForField("itemID", itemID)
+    if #results > 0 then
+      local classID = select(6, C_Item.GetItemInfoInstant(itemID))
+      local parent = results[1]
+      local id
+      if classID == Enum.ItemClass.Consumable or classID == Enum.ItemClass.Armor or classID == Enum.ItemClass.Weapon or classID == Enum.ItemClass.Miscellaneous or classID == Enum.ItemClass.Reagent then
+        while parent do
+          if parent.awp then -- awp: short for added with patch
+            id = parent.awp
+          end
+          parent = parent.parent
+        end
+      else
+        while parent and not id do
+          if parent.awp then -- awp: short for added with patch
+            id = parent.awp
+          end
+          parent = parent.parent
+        end
       end
-      local id = parent and parent.awp
-      if not id then
-        return
+      if id then
+        local major = math.floor(id / 10000)
+        local minor = math.floor((id % 10000) / 100)
+        local patch = math.floor(id % 100)
+        return major, minor, patch
       end
-      local major = math.floor(id / 10000)
-      local minor = math.floor((id % 10000) / 100)
-      local patch = math.floor(id % 100)
-      return major, minor, patch
     end
   end
 end
