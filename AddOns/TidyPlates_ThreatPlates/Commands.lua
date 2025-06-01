cd2c483545d8a68e9c23d9cc820cd70df498f465
Dololo <class2u@gmail.com>
2025-06-02 05:50:43 +0800

    更新 威力血條 12.2.1

--- AddOns/TidyPlates_ThreatPlates/Commands.lua
+++ AddOns/TidyPlates_ThreatPlates/Commands.lua
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------------------------------------
 local L = Addon.ThreatPlates.L
 
-Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "12.2.0"
+Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "12.2.1"
 
 local function toggleDPS()
 	if Addon.db.profile.optionRoleDetectionAutomatic then
--- AddOns/TidyPlates_ThreatPlates/Database.lua
+++ AddOns/TidyPlates_ThreatPlates/Database.lua
@@ -644,7 +644,7 @@ end
 local CurrentVersion = VersionToNumber(Addon.ThreatPlates.Meta("version"))
 
 function TidyPlatesThreat:VersionIsAtLeast(min_version)
-  if CurrentVersion == 0 then return true end -- Always return true in development (version = "12.2.0")
+  if CurrentVersion == 0 then return true end -- Always return true in development (version = "12.2.1")
 
   local min_version_no, _ = VersionToNumber(min_version)
   return min_version_no > 0 and CurrentVersion >= min_version_no
