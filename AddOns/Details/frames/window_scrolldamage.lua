a1e9b9b8b70b97ba413e78ebf16a894c770697d2
Dololo <class2u@users.noreply.github.com>
2025-06-17 15:53:17 +0800

    更新 Details! 戰鬥統計 20250616.13616.165
    # Conflicts:
    #	AddOns/Details/locales/Details-frFR.lua
    #	AddOns/Details_Compare2/Details_Compare2.toc
    #	AddOns/Details_DataStorage/Details_DataStorage.toc
    #	AddOns/Details_EncounterDetails/Details_EncounterDetails_Mainline.toc
    #	AddOns/Details_RaidCheck/Details_RaidCheck.toc
    #	AddOns/Details_Streamer/Details_Streamer_Mainline.toc
    #	AddOns/Details_TinyThreat/Details_TinyThreat.toc
    #	AddOns/Details_Vanguard/Details_Vanguard_Mainline.toc

--- AddOns/Details/frames/window_scrolldamage.lua
+++ AddOns/Details/frames/window_scrolldamage.lua
@@ -309,6 +309,7 @@ local targetDummiesIds = {
 	[225982] = true, --Cleave Dummy - Dornogal
 	[225977] = true, --Dungeoneer's Tanking dummy - Dornogal
 	[225976] = true, --Normal Tanking dummy - Dornogal
+	[225985] = true, --Kelpfist - Dornogal 
 }
 
 targetDummieHandle:SetScript("OnEvent", function(_, _, unit)
