df006efd40b2ced5ade661b322206c1b2c44b777
Dololo <class2u@users.noreply.github.com>
2025-06-18 22:08:05 +0800

    更新 功能資訊列 3.6.1
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Mainline.toc

---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/3.6.1.lua
@@ -0,0 +1,36 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3610] = {
+    version_string = "3.6.1",
+    release_date = "2025/06/18",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {
+            "[Retail] 更新 TOC 以支援 [11.1.7]。",
+        },
+        ["enUS"] = {
+            "[Retail] TOC update for [11.1.7].",
+        },
+        ["frFR"] = {
+            "[Retail] Mise à jour du TOC pour le patch [11.1.7].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
