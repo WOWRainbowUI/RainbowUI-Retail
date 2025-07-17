3d4f42c081c5bb15148a52597005b796c540a86f
Dololo <class2u@users.noreply.github.com>
2025-07-17 10:04:22 +0800

    更新 功能資訊列 3.7.1
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/Libs/AceDB-3.0/AceDB-3.0.lua

---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/3.7.1.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3710] = {
+    version_string = "3.7.1",
+    release_date = "2025/07/16",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Global] XIV Databar Continued is now translated in Brazilian Portuguese (thank you [BrunoKrugel]).",
+        },
+        ["frFR"] = {
+            "[Global] XIV Databar Continued est maintenant traduit en portugais brésilien (merci [BrunoKrugel]).",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
