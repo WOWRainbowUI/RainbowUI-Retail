4836e388cb6f46fbd53e7e42fd27443731723af6
Dololo <class2u@users.noreply.github.com>
2025-08-05 20:34:48 +0800

    更新 敵方技能監控 (血條) 110107.6-release
    # Conflicts:
    #	AddOns/NameplateCooldowns/locales/koKR.lua

--- AddOns/NameplateCooldowns/NameplateCooldowns.lua
+++ AddOns/NameplateCooldowns/NameplateCooldowns.lua
@@ -12,7 +12,7 @@ local Trinkets = addonTable.Trinkets;
 local Reductions = addonTable.Reductions;
 
 --@non-debug@
-local buildTimestamp = "110107.5-release";
+local buildTimestamp = "110107.6-release";
 --@end-non-debug@
 
 -- Libraries
