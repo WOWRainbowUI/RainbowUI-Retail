03abcb11a689f1b1eaf1ab804451157b2434edda
Dololo <class2u@users.noreply.github.com>
2025-08-08 08:06:38 +0800

    更新 戰寵助手 11.2
    # Conflicts:
    #	AddOns/PetTracker/PetTracker-Mainline.toc
    #	AddOns/PetTracker_Battle/PetTracker_Battle-Mainline.toc
    #	AddOns/PetTracker_Battle/PetTracker_Battle-Mists.toc
    #	AddOns/PetTracker_Config/PetTracker_Config-Mainline.toc
    #	AddOns/PetTracker_Config/PetTracker_Config-Mists.toc
    #	AddOns/PetTracker_Journal/PetTracker_Journal-Mainline.toc
    #	AddOns/PetTracker_Journal/PetTracker_Journal-Mists.toc

--- AddOns/PetTracker/libs/C_Everywhere/C_Everywhere.lua
+++ AddOns/PetTracker/libs/C_Everywhere/C_Everywhere.lua
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 This file is part of C_Everywhere.
 --]]
 
-local C = LibStub:NewLibrary('C_Everywhere', 14)
+local C = LibStub:NewLibrary('C_Everywhere', 16)
 if C then
 	wipe(C)
 else
@@ -41,8 +41,8 @@ setmetatable(C, {__index = function(C, space)
 end})
 
 -- specifics
-local function null(space, k)
-	space[k] = space.rawfind(k) or nop
+local function stub(space, k, func)
+	space[k] = space.rawfind(k) or func or nop
 end
 
 local function pack(space, k, args)
@@ -86,14 +86,17 @@ pack(C.CurrencyInfo, 'GetBackpackCurrencyInfo', 'name, quantity, iconFileID, cur
 pack(C.CurrencyInfo, 'GetBasicCurrencyInfo', 'name, description, icon, quality, displayAmount, actualAmount')
 pack(C.CurrencyInfo, 'GetCurrencyInfo', 'name, quantity, iconFileID, quantityEarnedThisWeek, maxWeeklyQuantity, maxQuantity, discovered, quality')
 pack(C.CurrencyInfo, 'GetCurrencyListInfo', 'name, isHeader, isHeaderExpanded, isTypeUnused, isShowInBackpack, quantity, iconFileID, maxQuantity, canEarnPerWeek, quantityEarnedThisWeek, discovered')
-null(C.CurrencyInfo, 'IsAccountTransferableCurrency')
-null(C.CurrencyInfo, 'IsAccountWideCurrency')
-null(C.QuestLog, 'IsQuestFlaggedCompletedOnAccount')
-pack(C.Spell, 'GetSpellInfo', 'name, rank, iconID, castTime, minRange, maxRange, spellID, originalIconID')
+stub(C.CurrencyInfo, 'IsAccountTransferableCurrency')
+stub(C.CurrencyInfo, 'IsAccountWideCurrency')
+stub(C.QuestLog, 'IsQuestFlaggedCompletedOnAccount')
+pack(C.Spell, 'GetSpellInfo', 'name, rank, iconID, castTime, minRange, maxRange, spellID, originalIconID') 
 
-C.Bank.CanViewBank = C.Bank.CanViewBank or function(v) return v == 0 end
-C.QuestLog.IsComplete = C.QuestLog.IsComplete or function(id) return select(6, GetQuestLogTitle(GetQuestLogIndexByID(id))) == 1 end
-C.QuestLog.GetTitleForQuestID = C.QuestLog.GetQuestInfo
+stub(C.QuestLog, 'IsComplete', function(id) return select(6, GetQuestLogTitle(GetQuestLogIndexByID(id))) == 1 end)
+stub(C.Bank, 'PurchaseBankTab', PurchaseSlot)
+stub(C.Bank, 'CanUseBank', function(i) return i == 0 end)
+stub(C.Bank, 'CanViewBank', function(i) return i == 0 end)
+stub(C.Bank, 'FetchNextPurchasableBankTabData', function(i) return {tabCost = i == 0 and GetBankSlotCost() or C.Bank.FetchNextPurchasableBankTabCost(i)} end)
+
 C.Item.GetItemIconByID = GetItemIcon
 C.Item.IsDressableItemByID = IsDressableItem
 C.GossipInfo.SelectActiveQuest = SelectGossipActiveQuest
@@ -101,6 +104,7 @@ C.GossipInfo.SelectAvailableQuest = SelectGossipAvailableQuest
 C.GossipInfo.GetNumAvailableQuests = GetNumGossipAvailableQuests
 C.GossipInfo.GetNumActiveQuests = GetNumGossipActiveQuests
 C.GossipInfo.GetText = GetGossipText
+C.QuestLog.GetTitleForQuestID = C.QuestLog.GetQuestInfo
 
 if not C_TooltipInfo then
 	local tip = C_EverywhereTip or CreateFrame('GameTooltip', 'C_EverywhereTip', UIParent, 'GameTooltipTemplate')
