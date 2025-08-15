6e47494f4aec3c281a6a8231dcba76167e632b7a
Dololo <class2u@users.noreply.github.com>
2025-08-15 11:44:28 +0800

    更新 按鈕外觀 11.2.0

--- AddOns/Masque/Masque.lua
+++ AddOns/Masque/Masque.lua
@@ -118,7 +118,7 @@ Core.WOW_VERSION = WOW_VERSION
 Core.WOW_RETAIL = WOW_RETAIL
 
 -- Add-On Info
-Core.Version = "11.1.7"
+Core.Version = "11.2.0"
 Core.Discord = "https://discord.gg/7MTWRgDzz8"
 
 Core.Authors = {
