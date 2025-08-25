14fa49f454edb3afa4c48c06ce2132ff208c7c4e
Dololo <class2u@users.noreply.github.com>
2025-08-25 10:06:43 +0800

    更新 專業製造模擬器 19.9.0

--- AddOns/CraftSim/CHANGELOG.md
+++ AddOns/CraftSim/CHANGELOG.md
@@ -1,18 +1,10 @@
 # CraftSim
 
-## [19.8.8](https://github.com/derfloh205/CraftSim/tree/19.8.8) (2025-08-06)
-[Full Changelog](https://github.com/derfloh205/CraftSim/compare/19.8.7...19.8.8) [Previous Releases](https://github.com/derfloh205/CraftSim/releases)
+## [19.9.0](https://github.com/derfloh205/CraftSim/tree/19.9.0) (2025-08-22)
+[Full Changelog](https://github.com/derfloh205/CraftSim/compare/19.8.9...19.9.0) [Previous Releases](https://github.com/derfloh205/CraftSim/releases)
 
-- old world  
-- buffs  
-- fix comparison, Legion training recipes match "BASE" string, causing comparison error (#814)  
-- Fixes detection of Phial of Bountiful Seasons (#845)  
-- collab update  
-- Update esMX.lua (#853)  
-    * Update esMX.lua  
-    Add ingenuity localization for esMX  
-    * Update esES.lua  
-    Swap ingenuity and resourcefulness localizations  
-- news and version update  
-- fix container indexing (#857)  
-- Concentration Curve Data and Optional Reagent Data Update  
+- removed debugs  
+- [Feat] Add item level retrieval and optimize gear selection logic (#867)  
+- Merged Pull Request for Tool Detection Fix  
+- enum.Backpack (#862)  
+    check for tool in bags as well  
--- AddOns/CraftSim/Modules/CraftQueue/CraftQueue.lua
+++ AddOns/CraftSim/Modules/CraftQueue/CraftQueue.lua
@@ -380,7 +380,11 @@ function CraftSim.CRAFTQ:QueueWorkOrders()
                                             end
 
                                             if queueAble then
-                                                if withinKPCost(recipeData.averageProfitCached) and withinMaxPatronOrderCost(recipeData.priceData.craftingCosts) then
+                                                if CraftSim.DB.OPTIONS:Get("CRAFTQUEUE_WORK_ORDERS_ONLY_PROFITABLE") and
+                                                recipeData.averageProfitCached <= 0	then
+                                                    -- skip: not profitable
+                                                elseif withinKPCost(recipeData.averageProfitCached) and
+                                                withinMaxPatronOrderCost(recipeData.priceData.craftingCosts) then
                                                     CraftSim.CRAFTQ:AddRecipe { recipeData = recipeData }
                                                 end
                                             end
@@ -561,6 +565,7 @@ function CraftSim.CRAFTQ:QueueFavorites()
                 highestProfit = true,
             },
             optimizeConcentration = true,
+            optimizeGear = true,
             optimizeConcentrationProgressCallback = function(conProgress)
                 queueFavoritesButton:SetText(string.format("%.0f%% - %s %s %s - %.0f%%",
                     progress,
