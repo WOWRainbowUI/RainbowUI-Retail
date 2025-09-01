35753f8abfde0de5d5a0783935277da1f92c423c
Dololo <class2u@users.noreply.github.com>
2025-09-01 10:14:47 +0800

    更新 顯示距離 v6.2.2

--- AddOns/RangeDisplay/RangeDisplay.lua
+++ AddOns/RangeDisplay/RangeDisplay.lua
@@ -8,7 +8,7 @@ License: Public Domain
 
 local AppName, RangeDisplay = ...
 local OptionsAppName = AppName .. "_Options"
-local VERSION = AppName .. "-v6.2.0"
+local VERSION = AppName .. "-v6.2.2"
 
 local rc = LibStub("LibRangeCheck-3.0")
 local LSM = LibStub:GetLibrary("LibSharedMedia-3.0", true)
