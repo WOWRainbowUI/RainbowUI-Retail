3c923484c8ee0f3cd3893dea15b356e6b00e92e8
Dololo <class2u@users.noreply.github.com>
2025-09-10 09:11:17 +0800

    更新 採集助手 1.50.4
    # Conflicts:
    #	AddOns/!Ace3/HereBeDragons/HereBeDragons.toc
    #	AddOns/GatherMate2/Config.lua
    #	AddOns/GatherMate2/GatherMate2.toc
    #	AddOns/GatherMate2/Libs/AceDB-3.0/AceDB-3.0.lua
    #	AddOns/GatherMate2/Libs/AceGUI-3.0/widgets/AceGUIContainer-TreeGroup.lua
    #	AddOns/GatherMate2/Libs/HereBeDragons/HereBeDragons-2.0.lua
    #	AddOns/GatherMate2/Locales/GatherMate2-deDE.lua

--- AddOns/GatherMate2/CHANGELOG.md
+++ AddOns/GatherMate2/CHANGELOG.md
@@ -1,11 +1,9 @@
 # GatherMate2
 
-## [1.50.2.2](https://github.com/Nevcairiel/GatherMate2/tree/1.50.2.2) (2024-08-30)
-[Full Changelog](https://github.com/Nevcairiel/GatherMate2/compare/1.50.1...1.50.2.2) [Previous Releases](https://github.com/Nevcairiel/GatherMate2/releases)
+## [1.50.4](https://github.com/Nevcairiel/GatherMate2/tree/1.50.4) (2025-09-09)
+[Full Changelog](https://github.com/Nevcairiel/GatherMate2/compare/1.50.3.1...1.50.4) [Previous Releases](https://github.com/Nevcairiel/GatherMate2/releases)
 
-- Added Herbs & Mines transparent icons .tga  
-- Added "Webbed Ore Deposit"  
-    https://www.wowhead.com/object=430335/webbed-ore-deposit  
-- Re-add Kul Tiras to the continent list  
-    It was accidentally removed in f57a97882a993bf4628f860fe599796d54b9333c  
-    Fixes #29  
+- Add 11.2 Herb and Mine nodes  
+- Update TOC for 11.2 and 11.2.5  
+- Update packager reference  
+- Update TOC  
--- AddOns/GatherMate2/Config.lua
+++ AddOns/GatherMate2/Config.lua
@@ -1142,29 +1142,13 @@ local faqOptions = {
 local acr = LibStub("AceConfigRegistry-3.0")
 local acd = LibStub("AceConfigDialog-3.0")
 
-local function findPanel(name, parent)
-	for i, button in next, InterfaceOptionsFrameAddOns.buttons do
-		if button.element then
-			if name and button.element.name == name then return button
-			elseif parent and button.element.parent == parent then return button
-			end
-		end
-	end
-end
 function Config:OnInitialize()
 	db = GatherMate.db.profile
 
 	self.importHelper = ImportHelper
 
 	acr:RegisterOptionsTable("GatherMate 2", generalOptions)
-	local options = acd:AddToBlizOptions("GatherMate 2", L["GatherMate 2"])
-
-	if InterfaceOptionsFrameAddOns then
-		options:HookScript("OnShow", function()
-			local p = findPanel(L["GatherMate 2"])
-			if p and p.element.collapsed then OptionsListButtonToggle_OnClick(p.toggle) end
-		end)
-	end
+	acd:AddToBlizOptions("GatherMate 2", L["GatherMate 2"])
 
 	acr:RegisterOptionsTable("GM2/Minimap", minimapOptions)
 	acd:AddToBlizOptions("GM2/Minimap", L["Minimap"], L["GatherMate 2"])
@@ -1185,11 +1169,7 @@ function Config:OnInitialize()
 	acd:AddToBlizOptions("GM2/FAQ", "FAQ", L["GatherMate 2"])
 
 	local function openOptions()
-		if Settings and Settings.OpenToCategory then
-			Settings.OpenToCategory(L["GatherMate 2"])
-		else
-			InterfaceOptionsFrame_OpenToCategory(L["GatherMate 2"])
-		end
+		Settings.OpenToCategory(L["GatherMate 2"])
 	end
 
 	SLASH_GatherMate21 = "/gathermate"
@@ -1243,7 +1223,7 @@ function Config:CheckAutoImport()
 		if verline and v["autoImport"] then
 			local dataVersion = tonumber(verline:match("%d+"))
 			if dataVersion and dataVersion > v["lastImport"] then
-				local loaded, reason = LoadAddOn(k)
+				local loaded, reason = C_AddOns.LoadAddOn(k)
 				if loaded then
 					local addon = LibStub("AceAddon-3.0"):GetAddon(k)
 					local filter = nil
