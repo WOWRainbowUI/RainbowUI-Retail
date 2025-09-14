949a28f45b6a07dea93c411e4b15a34f029cc3cf
Dololo <class2u@users.noreply.github.com>
2025-09-14 21:58:58 +0800

    更新 功能資訊列 3.7.3

--- AddOns/XIV_Databar_Continued/CHANGELOG.md
+++ AddOns/XIV_Databar_Continued/CHANGELOG.md
@@ -1,9 +1,9 @@
 # XIV_Databar Continued
 
-## [3.7.2](https://github.com/ZelionGG/XIV_Databar-Continued/releases/tag/3.7.2) (2025-07-22)
+## [3.7.3](https://github.com/ZelionGG/XIV_Databar-Continued/releases/tag/3.7.3) (2025-09-14)
 
-[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v3.7.1...v3.7.2) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
+[Full Changelog](https://github.com/ZelionGG/XIV_Databar-Continued/compare/v3.7.2...v3.7.3) [Previous Releases](https://github.com/ZelionGG/XIV_Databar-Continued/releases)
 
 ### _Retail :_
 
-- Season 3 teleports are now ready for **August 12th (US)** / **13th (EU/China)**, dungeons from the **Mythic+ Teleports** part of the **Travel** module will automatically update to the Season 3 lineup on that date.
+- Added **Cosmic Hearthstone** and **P.O.S.T. Master's Express Hearthstone** to the **Travel** module.
---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/3.7.3.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3730] = {
+    version_string = "3.7.3",
+    release_date = "2025/09/14",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Added [Cosmic Hearthstone] and [P.O.S.T. Master's Express Hearthstone] to the [Travel] module.",
+        },
+        ["frFR"] = {
+            "[Retail] Ajout des [Pierre de foyer cosmique] et [Pierre de foyer expresse du maître de P.O.S.T.E.] au module [Voyage].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
