776054158b379e3a059b438f5d569eb2215f04d0
Dololo <class2u@users.noreply.github.com>
2025-10-10 11:12:43 +0800

    更新 功能資訊列 3.7.5
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/Libs/AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
    #	AddOns/XIV_Databar_Continued/Libs/AceGUI-3.0/widgets/AceGUIWidget-EditBox.lua
    #	AddOns/XIV_Databar_Continued/Libs/AceGUI-3.0/widgets/AceGUIWidget-MultiLineEditBox.lua

---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/3.7.5.lua
@@ -0,0 +1,36 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3750] = {
+    version_string = "3.7.5",
+    release_date = "2025/10/09",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Updated for patch 11.2.5.",
+            "[Midnight Alpha] Initial release for patch 12.0 alpha.",
+        },
+        ["frFR"] = {
+            "[Retail] Mis à jour pour le patch 11.2.5.",
+            "[Midnight Alpha] Préparation pour l'alpha 12.0.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
