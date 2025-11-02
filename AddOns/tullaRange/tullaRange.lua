ad64faa22f3cc2d5e7ed5b0a95baebde00a3aa02
Dololo <class2u@users.noreply.github.com>
2025-11-02 15:16:53 +0800

    Revert "更新 射程著色 11.2.0"
    This reverts commit c48128cd5863e308113ee11543ea3dc65671cc51.

--- AddOns/tullaRange/tullaRange.lua
+++ AddOns/tullaRange/tullaRange.lua
@@ -4,47 +4,76 @@
 
 local AddonName, Addon = ...
 
-Addon.Name = AddonName
+Addon.frame = CreateFrame("Frame", nil, SettingsPanel)
+Addon.frame.owner = Addon
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --------------------------------------------------------------------------------
 
-EventUtil.ContinueOnAddOnLoaded(AddonName, GenerateClosure(function(self, addonName)
-    -- load the settings ui when the settings panel is first shown
-    self.frame = CreateFrame("Frame", nil, SettingsPanel)
-    self.frame.owner = self
+-- addon intially loaded
+function Addon:OnLoad()
+	self.frame:SetScript("OnEvent", function(frame, event, ...)
+		local owner = frame.owner
+		local func = owner[event]
 
-    self.frame:SetScript("OnShow", function(frame)
-        C_AddOns.LoadAddOn(addonName .. "_Config")
-        frame:SetScript("OnShow", nil)
-    end)
+		if func then
+			func(owner, event, ...)
+		end
+	end)
 
-    self.sets = self:GetOrCreateSettings()
+	-- load the options menu when the settings frame is loaded
+	self.frame:SetScript("OnShow", function(frame)
+		C_AddOns.LoadAddOn(AddonName .. "_Config")
+		frame:SetScript("OnShow", nil)
+	end)
 
-    -- add a launcher for the addons tray
-    if AddonCompartmentFrame then
-        AddonCompartmentFrame:RegisterAddon{
-            text = C_AddOns.GetAddOnMetadata(addonName, "Title"),
-            icon = C_AddOns.GetAddOnMetadata(addonName, "IconTexture"),
-            func = function()
-                if C_AddOns.LoadAddOn(addonName .. '_Config') then
-                    Settings.OpenToCategory(addonName)
-                end
-            end,
-        }
-    end
+	-- register any events we need to watch
+	self.frame:RegisterEvent("ADDON_LOADED")
+	self.frame:RegisterEvent("PLAYER_LOGOUT")
 
-    -- enable the addon, this is defined in classic/modern
-    if type(self.Enable) == "function" then
-        self:Enable()
-    else
-        print(addonName, " - Unable to enable for World of Warcraft version", (GetBuildInfo()))
-    end
+	-- drop this method, as we won't need it again
+	self.OnLoad = nil
+	_G[AddonName] = self
+end
 
-    -- unload settings when we're done
-    EventUtil.RegisterOnceFrameEventAndCallback("PLAYER_LOGOUT", function () self:TrimSettings() end)
-end, Addon))
+-- when the addon finishes loading
+function Addon:ADDON_LOADED(event, addonName)
+	if addonName ~= AddonName then return end
+
+	-- initialize settings
+	self.sets = self:GetOrCreateSettings()
+
+	-- add a launcher for the addons tray
+	if AddonCompartmentFrame then
+		AddonCompartmentFrame:RegisterAddon{
+			text = C_AddOns.GetAddOnMetadata(addonName, "Title"),
+			icon = C_AddOns.GetAddOnMetadata(addonName, "IconTexture"),
+			func = function()
+				if C_AddOns.LoadAddOn(addonName .. '_Config') then
+					Settings.OpenToCategory(addonName)
+				end
+			end,
+		}
+	end
+
+	-- enable the addon, this is defined in classic/modern
+	if type(self.Enable) == "function" then
+		self:Enable()
+	else
+		print(addonName, " - Unable to enable for World of Warcraft version", (GetBuildInfo()))
+	end
+
+	-- get rid of the handler, as we don't need it anymore
+	self.frame:UnregisterEvent(event)
+	self[event] = nil
+end
+
+function Addon:PLAYER_LOGOUT(event)
+	self:TrimSettings()
+	self.frame:UnregisterEvent(event)
+	self[event] = nil
+end
 
 --------------------------------------------------------------------------------
 -- Settings management
@@ -54,44 +83,44 @@ end, Addon))
 local DB_KEY = "TULLARANGE_COLORS"
 
 local function copyDefaults(tbl, defaults)
-    for k, v in pairs(defaults) do
-        if type(v) == "table" then
-            tbl[k] = copyDefaults(tbl[k] or {}, v)
-        elseif tbl[k] == nil then
-            tbl[k] = v
-        end
-    end
+	for k, v in pairs(defaults) do
+		if type(v) == "table" then
+			tbl[k] = copyDefaults(tbl[k] or {}, v)
+		elseif tbl[k] == nil then
+			tbl[k] = v
+		end
+	end
 
-    return tbl
+	return tbl
 end
 
 local function removeDefaults(tbl, defaults)
-    for k, v in pairs(defaults) do
-        if type(tbl[k]) == "table" and type(v) == "table" then
-            removeDefaults(tbl[k], v)
-            if next(tbl[k]) == nil then
-                tbl[k] = nil
-            end
-        elseif tbl[k] == v then
-            tbl[k] = nil
-        end
-    end
+	for k, v in pairs(defaults) do
+		if type(tbl[k]) == "table" and type(v) == "table" then
+			removeDefaults(tbl[k], v)
+			if next(tbl[k]) == nil then
+				tbl[k] = nil
+			end
+		elseif tbl[k] == v then
+			tbl[k] = nil
+		end
+	end
 
-    return tbl
+	return tbl
 end
 
 -- gets or creates the addon settings database
 -- populates it with defaults
 function Addon:GetOrCreateSettings()
-    local sets = _G[DB_KEY]
+	local sets = _G[DB_KEY]
 
-    if not sets then
-        sets = {}
+	if not sets then
+		sets = {}
 
-        _G[DB_KEY] = sets
-    end
+		_G[DB_KEY] = sets
+	end
 
-    return copyDefaults(sets, self:GetDefaultSettings())
+	return copyDefaults(sets, self:GetDefaultSettings())
 end
 
 -- removes any entries from the settings database that equate to default settings
@@ -103,23 +132,23 @@ function Addon:TrimSettings()
 end
 
 function Addon:GetDefaultSettings()
-    return {
-        -- how frequently we want to update, in seconds
-        updateDelay = TOOLTIP_UPDATE_TIME,
+	return {
+		-- how frequently we want to update, in seconds
+		updateDelay = TOOLTIP_UPDATE_TIME,
 
-        -- enable range coloring on pet actions
-        petActions = true,
+		-- enable range coloring on pet actions
+		petActions = true,
 
-        -- enable flash animations
-        flashAnimations = true,
-        flashDuration = ATTACK_BUTTON_FLASH_TIME * 2,
+		-- enable flash animations
+		flashAnimations = true,
+		flashDuration = ATTACK_BUTTON_FLASH_TIME * 2,
 
-        -- default colors (r, g, b, a, desaturate)
-        normal = { 1, 1, 1, 1, desaturate = false },
-        oor = { 1, 0.3, 0.1, 1, desaturate = true },
-        oom = { 0.1, 0.3, 1, 1, desaturate = true },
-        unusable = { 0.4, 0.4, 0.4, 1, desaturate = false }
-    }
+		-- default colors (r, g, b, a, desaturate)
+		normal = {1, 1, 1, 1, desaturate = false},
+		oor = {1, 0.3, 0.1, 1, desaturate = true},
+		oom = {0.1, 0.3, 1, 1, desaturate = true},
+		unusable = {0.4, 0.4, 0.4, 1, desaturate = false}
+	}
 end
 
 --------------------------------------------------------------------------------
@@ -127,26 +156,27 @@ end
 --------------------------------------------------------------------------------
 
 function Addon:GetUpdateDelay()
-    return self.sets.updateDelay
+	return self.sets.updateDelay
 end
 
 function Addon:GetFlashDuration()
-    return self.sets.flashDuration
+	return self.sets.flashDuration
 end
 
 function Addon:HandlePetActions()
-    return self.sets.petActions
+	return self.sets.petActions
 end
 
 function Addon:HandleAttackAnimations()
-    return self.sets.flashAnimations
+	return self.sets.flashAnimations
 end
 
 function Addon:GetColor(state)
-    local color = self.sets[state]
-    if color then
-        return color[1], color[2], color[3], color[4], color.desaturate
-    end
+	local color = self.sets[state]
+	if color then
+		return color[1], color[2], color[3], color[4], color.desaturate
+	end
 end
 
-_G[AddonName] = Addon
+-- load the addon
+Addon:OnLoad()
