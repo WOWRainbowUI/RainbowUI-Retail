d490eb5b210edef889a3481f410c418b1727e575
Dololo <class2u@users.noreply.github.com>
2025-11-11 17:04:11 +0800

    更新 敵方技能監控 (血條) 110205.1-release
    # Conflicts:
    #	AddOns/NameplateCooldowns/libs/AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
    #	AddOns/NameplateCooldowns/libs/AceDB-3.0/AceDB-3.0.lua
    #	AddOns/NameplateCooldowns/libs/AceGUI-3.0/widgets/AceGUIContainer-TreeGroup.lua
    #	AddOns/NameplateCooldowns/libs/AceGUI-3.0/widgets/AceGUIWidget-EditBox.lua
    #	AddOns/NameplateCooldowns/libs/AceGUI-3.0/widgets/AceGUIWidget-MultiLineEditBox.lua

--- AddOns/NameplateCooldowns/NameplateCooldowns.lua
+++ AddOns/NameplateCooldowns/NameplateCooldowns.lua
@@ -12,7 +12,7 @@ local Trinkets = addonTable.Trinkets;
 local Reductions = addonTable.Reductions;
 
 --@non-debug@
-local buildTimestamp = "110205.0-release";
+local buildTimestamp = "110205.1-release";
 --@end-non-debug@
 
 -- Libraries
