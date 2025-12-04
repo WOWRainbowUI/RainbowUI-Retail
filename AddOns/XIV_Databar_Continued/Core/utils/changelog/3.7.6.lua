d44a27d62690b4e63deee02059c6f61df18af4e6
Dololo <class2u@users.noreply.github.com>
2025-12-04 08:39:17 +0800

    更新 功能資訊列 3.7.6
    # Conflicts:
    #	AddOns/XIV_Databar_Continued/Libs/AceGUI-3.0/widgets/AceGUIContainer-TreeGroup.lua
    #	AddOns/XIV_Databar_Continued/Mainline/modules/travel.lua
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Mainline.toc
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Mists.toc
    #	AddOns/XIV_Databar_Continued/XIV_Databar_Continued_Vanilla.toc

---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/3.7.6.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3760] = {
+    version_string = "3.7.6",
+    release_date = "2025/12/03",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Updated for patch 11.2.7.",
+        },
+        ["frFR"] = {
+            "[Retail] Mis à jour pour le patch 11.2.7.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
