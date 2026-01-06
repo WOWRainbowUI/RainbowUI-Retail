8d796e4f07afb33f31603525bf7871a13be7a9a2
Dololo <class2u@users.noreply.github.com>
2026-01-06 10:02:55 +0800

    更新 達美樂快捷列 11.2.14

--- AddOns/Dominos/bars/menuBar.lua
+++ AddOns/Dominos/bars/menuBar.lua
@@ -17,7 +17,9 @@ if MicroMenu then
             local button = select(i, ...)
 
             -- always reparent the button
-            -- button:SetParent(Addon.ShadowUIParent)
+            if Addon:IsBuild("retail") then
+                button:SetParent(Addon.ShadowUIParent)
+            end
 
             -- ...but only display it on our bar if it was already enabled
             if button:IsShown() then
@@ -190,7 +192,7 @@ function MenuBar:IsMenuButtonEnabled(button)
     end
 end
 
-if Addon:IsBuild("retail", "bcc") then
+if Addon:IsBuild("retail", "tbc") then
     function MenuBar:Layout()
         for _, button in pairs(MicroButtons) do
             button:Hide()
