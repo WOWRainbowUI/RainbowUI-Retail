d78be37cba6ef2856b95e495c502e0820a0c264b
Dololo <class2u@users.noreply.github.com>
2026-01-08 08:55:55 +0800

    更新 世界地圖增強 11.2.27

--- AddOns/Leatrix_Maps/Leatrix_Maps.lua
+++ AddOns/Leatrix_Maps/Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 11.2.25 (24th December 2025)
+	-- 	Leatrix Maps 11.2.27 (7th January 2026)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaConfigList = {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "11.2.25"
+	LeaMapsLC["AddonVer"] = "11.2.27"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
