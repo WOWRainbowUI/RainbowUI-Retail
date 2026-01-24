c9a4bddc9d738afc53a17eb89355cd74c1d3fac0
Dololo <class2u@users.noreply.github.com>
2026-01-24 14:22:13 +0800

    更新 通知面板增強 120000.03

--- AddOns/ls_Toasts/core/changelog.lua
+++ AddOns/ls_Toasts/core/changelog.lua
@@ -1,14 +1,12 @@
-local _, addonTable = ...
-local E = addonTable.E
+local _, addon = ...
+local E = addon.E
 
 -- Lua
 local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added an option to disable the glow/shine visual effects on toasts. It can be toggled for each toast type individually. You can disable them at /LST > Toast Types > Toast Type > Visual Effects.
-
 ### Loot (Common)
 
-- Added a way to white-/blacklist items by ID. These lists will ignore the quality threshold setting. However, you can't whitelist items that got filtered out by the "Legacy Equipment" pre-filter.
+- Fixed an issue where secret player GUID could break the toast. Some toasts will be missing, but overall it shouldn't be too bad.
 ]]
