2cb7b2ccd23f3d0205f3faf6ed9dde76263eb73f
Dololo <class2u@users.noreply.github.com>
2026-01-24 17:18:26 +0800

    移除 冷卻時間

--- AddOns/OmniCC/effects/none.lua
+++
@@ -1,8 +0,0 @@
--- an effect that does nothing
-local AddonName, Addon = ...
-local L = LibStub("AceLocale-3.0"):GetLocale(AddonName)
-
-local NoopEffect = Addon.FX:Create("none", L.None)
-
-function NoopEffect:Run()
-end
