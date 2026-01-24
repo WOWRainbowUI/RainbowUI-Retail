5dc232ca327477ada5a597c58dc55ece512fe7d2
Dololo <class2u@users.noreply.github.com>
2026-01-24 17:23:25 +0800

    移除 OmniCC

--- AddOns/OmniCC/localization/deDE.lua
+++
@@ -1,7 +0,0 @@
-local AddonName = ...
-local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "deDE")
-if not L then return end
-
-L.Updated = "Aktualisiert auf v%s"
-L.Pulse = "Pulse"
-L.Shine = "Shine"
