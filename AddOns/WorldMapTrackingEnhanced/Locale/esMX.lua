73eb6e663a97a6f84506c920a9cf68f7bf4a073b
Dololo <class2u@users.noreply.github.com>
2026-01-25 02:01:17 +0800

    移除 WorldMapTrackingEnhanced

--- AddOns/WorldMapTrackingEnhanced/Locale/esMX.lua
+++
@@ -1,9 +0,0 @@
--- $Id: esMX.lua 20 2017-05-12 17:39:21Z arith $
-
-local L = LibStub("AceLocale-3.0"):NewLocale("WorldMapTrackingEnhanced", "esMX", false)
-
-if not L then return end
-
-if L then
-
-end
--- AddOns/WorldMapTrackingEnhanced/Locale/itIT.lua
+++
@@ -1,9 +0,0 @@
--- $Id: itIT.lua 20 2017-05-12 17:39:21Z arith $
-
-local L = LibStub("AceLocale-3.0"):NewLocale("WorldMapTrackingEnhanced", "itIT", false)
-
-if not L then return end
-
-if L then
-
-end
