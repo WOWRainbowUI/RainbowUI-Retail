210028c2da3ec8a4ea1f0f9f46444e26c68406d8
Dololo <class2u@users.noreply.github.com>
2026-01-29 09:49:48 +0800

    更新 快速複製文字 v1.1.41
    # Conflicts:
    #	AddOns/CopyAnything/CopyAnything_Cata.toc
    #	AddOns/CopyAnything/CopyAnything_Mists.toc
    #	AddOns/CopyAnything/CopyAnything_TBC.toc
    #	AddOns/CopyAnything/CopyAnything_Vanilla.toc
    #	AddOns/CopyAnything/CopyAnything_Wrath.toc

--- AddOns/CopyAnything/CHANGELOG.md
+++ AddOns/CopyAnything/CHANGELOG.md
@@ -1,7 +1,10 @@
 # Copy Anything
 
-## [v1.1.40](https://github.com/Oppzippy/CopyAnything/tree/v1.1.40) (2026-01-27)
-[Full Changelog](https://github.com/Oppzippy/CopyAnything/compare/v1.1.39...v1.1.40) [Previous Releases](https://github.com/Oppzippy/CopyAnything/releases)
+## [v1.1.41](https://github.com/Oppzippy/CopyAnything/tree/v1.1.41) (2026-01-28)
+[Full Changelog](https://github.com/Oppzippy/CopyAnything/compare/v1.1.40...v1.1.41) [Previous Releases](https://github.com/Oppzippy/CopyAnything/releases)
 
-- Remove luacheck  
-- Fix error due to reading secret errors in Midnight  
+- fix type that can be nil  
+- Fix secret value issues on retail  
+    This makes use of `canaccessvalue` before touching anything returned  
+    from Frame methods to ensure it's not secret.  
+    Also refactor to use iterators instead of tables  
--- AddOns/CopyAnything/FontStringFinder.lua
+++ AddOns/CopyAnything/FontStringFinder.lua
@@ -3,86 +3,53 @@ local L = addon.L
 
 local MouseIsOver, EnumerateFrames = MouseIsOver, EnumerateFrames
 
---------------------------------------------------------------------------------
--- Special cases for frames that shouldn't be handled normally
---
-
-local specificCases
-do
-	local function filterVisible(region)
-		return region:IsVisible()
-	end
-
-	specificCases = {
-		function(frame)         -- Chat frame
-			if frame.GetMessageInfo then -- chat frame
-				local messages = {}
-				for i = 1, frame:GetNumMessages() do
-					messages[i] = frame:GetMessageInfo(i)
-				end
-				return table.concat(messages, "\n")
-			end
-		end,
-		function(frame) -- Default
-			local fontStrings = addon:GetChildFontStrings(frame)
-			fontStrings = addon.tableUtils.Filter(fontStrings, filterVisible)
-			if #fontStrings > 500 then
-				addon:Print(L.tooManyFontStrings:format(500))
-				return
-			end
-			return addon:FontStringsToString(fontStrings)
-		end,
-	}
+---@return boolean
+local function canAccessValueCompat(value)
+	return WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE or canaccessvalue(value)
 end
+
+---@return boolean
+local function canAccessAllValuesCompat(...)
+	return WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE or canaccessallvalues(...)
+end
+
+---@param value Frame
+---@return boolean
+local function isAnchoringSecretCompat(value)
+	return WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and value:IsAnchoringSecret()
+end
+
+---@param value Frame
+---@return boolean
+local function hasAnySecretAspectCompat(value)
+	return WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and value:HasAnySecretAspect()
+end
+
 
 --------------------------------------------------------------------------------
 -- Search by font string
 --
 
-do
-	-- Iterates through all font strings in existence and returns those meeting a condition.
-	-- @param condition Function that returns a truthy value if the font string should be returned.
-	-- @return table of font strings that met the condition.
-	local function GetGlobalFontStrings(condition)
-		local fontStrings = {}
-		local frame = EnumerateFrames()
-		while frame do
-			local regions = { frame:GetRegions() }
-			for _, region in next, regions do
-				-- much faster to check if GetText exists rather than to use GetObjectType and check if FontString
-				if region.GetText and condition(region) then
-					fontStrings[#fontStrings + 1] = region
+---@return string mouseoverText all text under the cursor.
+function addon:GetMouseoverText()
+	local fontStringsIter = addon:GetDirectChildFontStrings(self:IterateFrames())
+	local function mouseoverFontStringsIter()
+		local fontString = fontStringsIter()
+		while fontString do
+			if fontString:IsVisible() then
+				-- No way of knowing if the region is restricted, so just skip this one
+				-- if it is restricted or has any other error.
+				local status, isMouseOver = pcall(function()
+					return MouseIsOver(fontString)
+				end)
+				if status and canAccessValueCompat(isMouseOver) and isMouseOver then
+					return fontString
 				end
 			end
-			frame = EnumerateFrames(frame)
+			fontString = fontStringsIter()
 		end
-		return fontStrings
 	end
-
-	local function filter(fontString)
-		local isSecret = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and fontString:IsAnchoringSecret()
-		if not isSecret and fontString:IsVisible() then
-			-- No way of knowing if the region is restricted, so just skip this one
-			-- if it is restricted or has any other error.
-			local status, isMouseOver = pcall(function()
-				return MouseIsOver(fontString)
-			end)
-			return status and isMouseOver
-		end
-		return false
-	end
-
-	-- Returns all font strings under the cursor.
-	-- @return Table of font strings.
-	function addon:GetMouseoverFontStrings()
-		return GetGlobalFontStrings(filter)
-	end
-end
-
--- Returns all text under the cursor.
--- @return string of all text under the cursor.
-function addon:GetMouseoverText()
-	return self:FontStringsToString(self:GetMouseoverFontStrings())
+	return self:FontStringsToString(mouseoverFontStringsIter)
 end
 
 --------------------------------------------------------------------------------
@@ -90,18 +57,6 @@ end
 --
 
 do
-	local function GetGlobalFrames(condition)
-		local frames = {}
-		local frame = EnumerateFrames()
-		while frame do
-			if condition(frame) then
-				frames[#frames + 1] = frame
-			end
-			frame = EnumerateFrames(frame)
-		end
-		return frames
-	end
-
 	-- Parent frame names that don't contain the word parent
 	local blacklist = {
 		UIParent = true,
@@ -110,34 +65,54 @@ do
 		DetailsAuraPanel = true,
 		ElvUF_PetBattleFrameHider = true,
 		TimerTracker = true,
+		PetFrame = true,
 	}
+
 	setmetatable(blacklist, {
 		__index = function(t, key)
 			return type(key) == "string" and key:lower():find("parent")
 		end,
 	})
 
-	local function filter(frame)
-		local parent = frame:GetParent()
-		return frame:IsVisible()
-			and MouseIsOver(frame)
-			and (parent and blacklist[parent:GetName()] or parent == nil)
-			and not blacklist[frame:GetName()]
-	end
-
-	-- Returns all top level frames under the cursor.
-	-- @return table of frames.
+	---Returns all top level frames under the cursor.
+	---@return fun(): Frame? iter
 	function addon:GetMouseoverFrames()
-		return GetGlobalFrames(filter)
+		local frameIter = self:IterateFrames()
+		return function()
+			local frame = frameIter()
+			while frame do
+				-- No way of knowing if the region is restricted, so just skip this one
+				-- if it is restricted or has any other error.
+				local status, isMouseOver = pcall(function()
+					local parent = frame:GetParent()
+					local isVisible = frame:IsVisible()
+					local isMouseOver = not isAnchoringSecretCompat(frame)
+						and not hasAnySecretAspectCompat(frame)
+						and MouseIsOver(frame)
+					local parentName = parent and parent:GetName()
+					local name = frame:GetName()
+
+					if canAccessAllValuesCompat(isVisible, isMouseOver, parentName, name) then
+						return isVisible
+							and isMouseOver
+							and (parentName and blacklist[parentName] or parent == nil)
+							and not blacklist[name]
+					end
+				end)
+				if status and isMouseOver then
+					return frame
+				end
+				frame = frameIter()
+			end
+		end
 	end
 end
 
--- Returns all text from all frames under the cursor.
--- @return string containing all text from frames under the cursor.
+---Returns all text from all frames under the cursor.
+---@return string containing all text from frames under the cursor.
 function addon:GetMouseoverFramesText()
-	local frames = self:GetMouseoverFrames()
 	local texts = {}
-	for _, frame in ipairs(frames) do
+	for frame in self:GetMouseoverFrames() do
 		texts[#texts + 1] = self:GetSpecificFrameText(frame)
 	end
 	return table.concat(texts, "\n")
@@ -146,6 +121,8 @@ end
 --------------------------------------------------------------------------------
 -- Search by mouse focus
 --
+
+---@return string
 function addon:GetMouseFocusText()
 	local frames
 	if GetMouseFoci then
@@ -167,88 +144,97 @@ end
 --
 
 -- Returns text from a specific frame and its children.
--- @return string containing text from frames
+---@param frame Frame
+---@return string
 function addon:GetSpecificFrameText(frame)
-	for _, case in ipairs(specificCases) do
-		local text = case(frame)
-		if text then
-			return text
+	local fontStringIter = addon:GetChildFontStrings(frame)
+	local function visibleFontStrings()
+		local fontString = fontStringIter()
+		while fontString do
+			local isVisible = fontString:IsVisible()
+			if canAccessValueCompat(isVisible) and isVisible then
+				return fontString
+			end
+
+			fontString = fontStringIter()
 		end
 	end
+	return addon:FontStringsToString(visibleFontStrings)
 end
 
 --------------------------------------------------------------------------------
 -- Helper functions
 --
 
-do
-	local function getRegions(frame)
-		return { frame:GetRegions() }
-	end
-
-	local function isFontString(region)
-		return region.GetText
-	end
-
-	-- Returns font strings that are a direct child of the supplied frames.
-	-- @param frames table of frames to search.
-	-- @return table of font strings that are direct children of the supplied frames.
-	function addon:GetDirectChildFontStrings(frames)
-		local regions = self.tableUtils.Map(frames, getRegions)
-		regions = self.tableUtils.Flatten(regions, 1)
-		regions = self.tableUtils.Filter(regions, isFontString)
-		return regions
-	end
-end
-
 -- Concatenates the text of a table of font strings into one string.
 -- font strings are separated with \n.
--- @param fontStrings table of FontStrings.
--- @return concatenated text of all font strings.
-function addon:FontStringsToString(fontStrings)
+---@param fontStringsIter fun(): FontString?
+---@return string
+function addon:FontStringsToString(fontStringsIter)
 	local texts = {}
-	local foundOne = false
-	for _, fs in ipairs(fontStrings) do
-		local isSecret = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and fs:IsAnchoringSecret()
-		if not isSecret then
-			local text = fs:GetText()
-			if text then
-				foundOne = true
-				texts[#texts + 1] = text
-			end
+	for fontString in fontStringsIter do
+		local text = fontString:GetText()
+		if canAccessValueCompat(text) and text then
+			texts[#texts + 1] = text
 		end
 	end
-	return foundOne and table.concat(texts, "\n")
+	return texts[1] and table.concat(texts, "\n")
 end
 
 do
-	-- Returns a table containing all children of a frame.
-	-- Does not include the frame itself.
-	-- @param frame Frame to scan recirsively for children. Will not be included in the returned table.
-	-- @return table of all children.
-	local function GetAllChildren(frame)
-		local children = { frame:GetChildren() }
-		local count = #children
-		for i = 1, count do
-			local child = children[i]
-			local subChildren = GetAllChildren(child)
-			if subChildren then
-				for _, subChild in ipairs(subChildren) do
-					children[#children + 1] = subChild
+	---Iterator of all children, grandchildren, etc. Does not include the frame itself.
+	---@param frame Frame Frame to scan recirsively for children.
+	---@return fun(): Frame? iter
+	local function GetChildrenRecursive(frame)
+		return coroutine.wrap(function()
+			local children = { frame:GetChildren() }
+			local count = #children
+			for i = 1, count do
+				local child = children[i]
+				coroutine.yield(child)
+				for subChild in GetChildrenRecursive(child) do
+					coroutine.yield(subChild)
+				end
+			end
+		end)
+	end
+
+	---@param frame Frame to scan for font strings.
+	---@return fun(): FontString? iter Iterator of FontStrings
+	function addon:GetChildFontStrings(frame)
+		local childFramesIter = GetChildrenRecursive(frame)
+		local framesIter = coroutine.wrap(function()
+			coroutine.yield(frame)
+			for childFrame in childFramesIter do
+				coroutine.yield(childFrame)
+			end
+		end)
+		local fontStrings = self:GetDirectChildFontStrings(framesIter)
+		return fontStrings
+	end
+end
+
+-- Returns font strings that are a direct child of any of the supplied frames.
+---@param framesIter fun(): Frame? frames to search.
+---@return fun(): FontString? iter FontStrings that are direct children of the supplied frames.
+function addon:GetDirectChildFontStrings(framesIter)
+	return coroutine.wrap(function()
+		for frame in framesIter do
+			local regions = { frame:GetRegions() }
+			for _, region in next, regions do
+				if region.GetText then
+					coroutine.yield(region)
 				end
 			end
 		end
-		return children
-	end
+	end)
+end
 
-	-- Returns all child font strings of a frame. This includes child of child, etc.
-	-- and not just direct children.
-	-- @param frame Frame to scan for font strings.
-	-- @return table of font strings.
-	function addon:GetChildFontStrings(frame)
-		local frames = GetAllChildren(frame)
-		table.insert(frames, 1, frame)
-		local fontStrings = self:GetDirectChildFontStrings(frames)
-		return fontStrings
+---@return fun(): Frame? iter
+function addon:IterateFrames()
+	local frame = nil
+	return function()
+		frame = EnumerateFrames(frame)
+		return frame
 	end
 end
