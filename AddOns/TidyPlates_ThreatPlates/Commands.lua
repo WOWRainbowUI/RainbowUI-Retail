f7c5b03f6fc8ceaf8ff7d52921df6d17cda5c0eb
Dololo <class2u@users.noreply.github.com>
2026-02-11 18:59:46 +0800

    更新 威力血條 13.0.0-beta11

--- AddOns/TidyPlates_ThreatPlates/Commands.lua
+++ AddOns/TidyPlates_ThreatPlates/Commands.lua
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------------------------------------
 local L = Addon.L
 
-Addon.DEBUG = Addon.Meta("version") == "13.0.0-beta10"
+Addon.DEBUG = Addon.Meta("version") == "13.0.0-beta11"
 
 local function toggleDPS()
   if Addon.db.profile.optionRoleDetectionAutomatic then
