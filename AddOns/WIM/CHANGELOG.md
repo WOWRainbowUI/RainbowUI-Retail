6033e733019d2df6b75a7afdf849364f816aadbb
Dololo <class2u@users.noreply.github.com>
2026-02-20 08:45:45 +0800

    更新 魔獸世界即時通 3.16.3

--- AddOns/WIM/CHANGELOG.md
+++ AddOns/WIM/CHANGELOG.md
@@ -1,6 +1,8 @@
 # WIM
 
-## [3.16.2](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/tree/3.16.2) (2026-02-18)
-[Full Changelog](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/compare/3.16.1...3.16.2) [Previous Releases](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/releases)
+## [3.16.3](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/tree/3.16.3) (2026-02-19)
+[Full Changelog](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/compare/3.16.2...3.16.3) [Previous Releases](https://github.com/Legacy-of-Sylvanaar/wow-instant-messenger/releases)
 
-- Fix: Fix scoping of ChatEdit\_Update #219  
+- Improve item and player linking #218  
+- Add support for /R and /REPLY (known issue, needs to add support to transfer over message if from macro  
+- Fix: lastTellTarget and lastToldTarget were accessed in reverse order  
--- AddOns/WIM/Sources/Hooks.lua
+++ AddOns/WIM/Sources/Hooks.lua
@@ -32,14 +32,19 @@ end
 -- end
 
 
-if _G.ChatFrameUtil and _G.ChatFrameUtil.InsertLink then
-	hooksecurefunc(
-		_G.ChatFrameUtil and _G.ChatFrameUtil.InsertLink and _G.ChatFrameUtil or _G,
-		_G.ChatFrameUtil and _G.ChatFrameUtil.InsertLink and "InsertLink" or "ChatEdit_InsertLink",
-		function(text)
-			if not WIM.EditBoxInFocus or not text then return end
+local lastLinkInserted;
+hooksecurefunc(
+	_G.ChatFrameUtil and _G.ChatFrameUtil.InsertLink and _G.ChatFrameUtil or _G,
+	_G.ChatFrameUtil and _G.ChatFrameUtil.InsertLink and "InsertLink" or "ChatEdit_InsertLink",
+	function(text)
+		if not WIM.EditBoxInFocus or not text then return end
 
-			WIM.EditBoxInFocus:Insert(text);
-		end
-	);
-end
+		-- avoid double links.
+		if lastLinkInserted == text then return end
+		lastLinkInserted = text;
+
+		_G.C_Timer.After(0.1, function() lastLinkInserted = nil end);
+
+		WIM.EditBoxInFocus:Insert(text);
+	end
+);
