40bc5f2b306b337dd05e0a611a8c22b9a0c91d58
Dololo <class2u@users.noreply.github.com>
2026-02-27 11:18:53 +0800

    新增 地圖標記-至暗之夜

---
+++ AddOns/HandyNotes_Midnight/localization/zhCN.lua
@@ -0,0 +1,109 @@
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('zhCN')
+if not L then return end
+
+-------------------------------------------------------------------------------
+------------------------------------ COMMON -----------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_delve_rewards'] = '地下堡奖励'
+L['options_icons_delve_rewards_desc'] = '在提示中显示 {location:地下堡} 的奖励。'
+
+L['options_icons_stormarion_assault'] = '斯托玛兰突袭战'
+L['options_icons_stormarion_assault_desc'] = '在提示中显示 {location:斯托玛兰突袭战} 的奖励。'
+
+L['options_icons_abundance_rewards'] = '丰饶'
+L['options_icons_abundance_rewards_desc'] = '在提示中显示 {location:丰饶} 的奖励。'
+
+L['skyriding_glyph'] = '驭空术魔符'
+L['options_icons_skyriding_glyph'] = '驭空术魔符'
+L['options_icons_skyriding_glyph_desc'] = '显示全部驭空术魔符的位置。'
+
+L['midnight_telescope'] = '望远镜'
+L['options_icons_telescope'] = '{achievement:62057}'
+L['options_icons_telescope_desc'] = '显示 {achievement:62057} 成就中全部10个望远镜的位置。'
+
+L['options_icons_midnight_lore_hunter'] = '{achievement:62104}'
+L['options_icons_midnight_lore_hunter_desc'] = '显示 {achievement:62104} 成就中剧情猎人的位置。'
+
+L['options_icons_profession_treasures'] = '专业宝藏'
+L['options_icons_profession_treasures_desc'] = '显示给予专业知识的宝藏位置。'
+
+L['options_icons_safari'] = '{achievement:61091}'
+L['options_icons_safari_desc'] = '显示 {achievement:61091} 成就中战斗宠物的位置。'
+
+-------------------------------------------------------------------------------
+-------------------------------- EVERSONG WOODS --------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_ever_painting'] = '{achievement:62185}'
+L['options_icons_ever_painting_desc'] = '显示 {achievement:62185} 成就中画作的位置。'
+L['options_icons_runestone_rush'] = '{achievement:61961}'
+L['options_icons_runestone_rush_desc'] = '显示 {achievement:61961} 成就中符文石的位置。'
+
+L['eversong_woods_stone_vat_of_wine_note'] = '需要从 {npc:251405} 处获得10个 {item:256232} 和1个 {item:256397}。\n\n踩葡萄然后加入酵母。'
+L['on_flying_platform'] = '在飞行平台上'
+L['gift_of_the_phoenix_note'] = '从凤凰身上收集5个余烬，这些凤凰在使用凤凰之赐后出现。'
+L['triple_locked_safebox_note'] = '需要3个隐藏的保险箱钥匙。从箱子旁边的紫色火把处获取它们。'
+L['incomplete_book_of_sonnets'] = '未完成十四行诗集'
+
+-------------------------------------------------------------------------------
+----------------------------------- HARANDAR -----------------------------------
+-------------------------------------------------------------------------------
+
+L['glowing_moth'] = '发光飞蛾'
+L['options_icons_glowing_moth'] = '{achievement:61052}'
+L['options_icons_glowing_moth_desc'] = '显示 {achievement:61052} 成就中发光飞蛾的位置。'
+
+L['sporespawned_cache_note'] = '与 {dot:Red}{object:真菌棒槌} 互动以获得 {spell:1266347} 增益，然后使用它敲响 {object:响菌丝锣}。宝藏将出现在锣旁边。'
+L['fungal_mallet'] = '真菌棒槌'
+L['impenatrably_sealed_gourd_note'] = '从附近的 {object:Dangling Jug} 中拾取 {item:260251} 和从附近的 {object:Hanging Flask} 中拾取 {item:260250} \n与 {object:Durable Vase} 互动并添加液体以获得 {item:260266} \n与宝藏互动以获得奖励。'
+
+L['options_icons_more_than_just_thier_roots'] = '{achievement:62188}'
+L['options_icons_more_than_just_thier_roots_desc'] = '显示 {achievement:62188} 成就中 NPC 的位置。'
+L['more_than_just_thier_roots_note'] = '与以下位置的 NPC 交谈：'
+L['chonon_note'] = nil
+L['funnid_note'] = nil
+L['kawayn_note'] = nil
+
+L['altar_of_innocence'] = nil
+L['altar_of_vigor'] = nil
+L['altar_of_wisdom'] = nil
+L['altar_of_innocence_note'] = nil
+L['altar_of_vigor_note'] = nil
+L['altar_of_wisdom_note'] = nil
+
+-------------------------------------------------------------------------------
+---------------------------------- VOIDSTORM ----------------------------------
+-------------------------------------------------------------------------------
+
+L['bloody_sack_note'] = '从附近的骨堆中收集 {object:Dripping Meat}，以喂养 {object:Forgotten Oubliette}。'
+L['malignant_chest_note'] = '激活洞穴中的 {object:Malignant Nodes} 以解锁宝箱。'
+L['malignant_node'] = 'Malignant Node'
+
+-------------------------------------------------------------------------------
+---------------------------------- ZUL'AMAN ----------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_frog_princess'] = '{achievement:62201}'
+L['options_icons_frog_princess_desc'] = '显示 {achievement:62201} 成就中青蛙的位置。'
+
+L['options_icons_song_seeker'] = '{achievement:61455}'
+L['options_icons_song_seeker_desc'] = '显示 {achievement:61455} 成就中寻歌者的位置。'
+
+L['options_icons_spiritpaw_marathon'] = '{achievement:62202}'
+L['options_icons_spiritpaw_marathon_desc'] = '显示 {achievement:62202} 成就中灵爪奔行者的位置。'
+L['spiritpaw_marathon_note'] = '和 {npc:258938} 交谈。'
+
+-------------------------------------------------------------------------------
+---------------------------------- ARCANTINA ----------------------------------
+-------------------------------------------------------------------------------
+
+L['share_a_drink_note'] = '从奥术秘社的 {npc:250495} 处购买 {item:251039} 并与所有种族分享。'
+
+-------------------------------------------------------------------------------
+----------------------------------- DELVES ------------------------------------
+-------------------------------------------------------------------------------
+
+L['sturdy_chest'] = '坚固宝箱'
+L['sturdy_chest_suffix'] = '已找到坚固宝箱'
---
+++ AddOns/HandyNotes_Midnight/localization/zhTW.lua
@@ -0,0 +1,109 @@
+local ADDON_NAME, ns = ...
+local L = ns.NewLocale('zhTW')
+if not L then return end
+
+-------------------------------------------------------------------------------
+------------------------------------ COMMON -----------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_delve_rewards'] = '探究獎勵'
+L['options_icons_delve_rewards_desc'] = '在提示顯示 {location:探究} 的獎勵.'
+
+L['options_icons_stormarion_assault'] = '風瑪利昂襲擊'
+L['options_icons_stormarion_assault_desc'] = '在提示中顯示 {location:風瑪利昂襲擊} 的獎勵。'
+
+L['options_icons_abundance_rewards'] = '豐足'
+L['options_icons_abundance_rewards_desc'] = '在提示中顯示 {location:豐足} 的獎勵。'
+
+L['skyriding_glyph'] = '天空騎術雕紋'
+L['options_icons_skyriding_glyph'] = '天空騎術雕紋'
+L['options_icons_skyriding_glyph_desc'] = '顯示所有天空騎術雕紋的位置.'
+
+L['midnight_telescope'] = nil
+L['options_icons_telescope'] = '{achievement:62057}'
+L['options_icons_telescope_desc'] = nil
+
+L['options_icons_midnight_lore_hunter'] = '{achievement:62104}'
+L['options_icons_midnight_lore_hunter_desc'] = nil
+
+L['options_icons_profession_treasures'] = '專業技能寶藏'
+L['options_icons_profession_treasures_desc'] = '顯示會給予專業技能知識的寶藏位置.'
+
+L['options_icons_safari'] = '{achievement:61091}'
+L['options_icons_safari_desc'] = '顯示成就 {achievement:61091} 所需的戰寵位置'
+
+-------------------------------------------------------------------------------
+-------------------------------- EVERSONG WOODS --------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_ever_painting'] = '{achievement:62185}'
+L['options_icons_ever_painting_desc'] = nil
+L['options_icons_runestone_rush'] = '{achievement:61961}'
+L['options_icons_runestone_rush_desc'] = nil
+
+L['eversong_woods_stone_vat_of_wine_note'] = nil
+L['on_flying_platform'] = nil
+L['gift_of_the_phoenix_note'] = nil
+L['triple_locked_safebox_note'] = nil
+L['incomplete_book_of_sonnets'] = nil
+
+-------------------------------------------------------------------------------
+----------------------------------- HARANDAR -----------------------------------
+-------------------------------------------------------------------------------
+
+L['glowing_moth'] = nil
+L['options_icons_glowing_moth'] = '{achievement:61052}'
+L['options_icons_glowing_moth_desc'] = nil
+
+L['sporespawned_cache_note'] = nil
+L['fungal_mallet'] = nil
+L['impenatrably_sealed_gourd_note'] = nil
+
+L['options_icons_more_than_just_thier_roots'] = '{achievement:62188}'
+L['options_icons_more_than_just_thier_roots_desc'] = nil
+L['more_than_just_thier_roots_note'] = nil
+L['chonon_note'] = nil
+L['funnid_note'] = nil
+L['kawayn_note'] = nil
+
+L['altar_of_innocence'] = nil
+L['altar_of_vigor'] = nil
+L['altar_of_wisdom'] = nil
+L['altar_of_innocence_note'] = nil
+L['altar_of_vigor_note'] = nil
+L['altar_of_wisdom_note'] = nil
+
+-------------------------------------------------------------------------------
+---------------------------------- VOIDSTORM ----------------------------------
+-------------------------------------------------------------------------------
+
+L['bloody_sack_note'] = nil
+L['malignant_chest_note'] = nil
+L['malignant_node'] = nil
+
+-------------------------------------------------------------------------------
+---------------------------------- ZUL'AMAN ----------------------------------
+-------------------------------------------------------------------------------
+
+L['options_icons_frog_princess'] = '{achievement:62201}'
+L['options_icons_frog_princess_desc'] = nil
+
+L['options_icons_song_seeker'] = '{achievement:61455}'
+L['options_icons_song_seeker_desc'] = nil
+
+L['options_icons_spiritpaw_marathon'] = '{achievement:62202}'
+L['options_icons_spiritpaw_marathon_desc'] = nil
+L['spiritpaw_marathon_note'] = nil
+
+-------------------------------------------------------------------------------
+---------------------------------- ARCANTINA ----------------------------------
+-------------------------------------------------------------------------------
+
+L['share_a_drink_note'] = nil
+
+-------------------------------------------------------------------------------
+----------------------------------- DELVES ------------------------------------
+-------------------------------------------------------------------------------
+
+L['sturdy_chest'] = '結實的箱子'
+L['sturdy_chest_suffix'] = '結實的箱子已發現'
