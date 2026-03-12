a17368bf9639afbb96b5379171a5807340d887a9
Dololo <class2u@users.noreply.github.com>
2026-03-12 09:54:04 +0800

    更新 功能資訊列 4.8.1

---
+++ AddOns/XIV_Databar_Continued/Core/utils/changelog/Old/v4/4.8.lua
@@ -0,0 +1,61 @@
+---@class XIVBar
+local XIVBar = select(2, ...);
+
+XIVBar.Changelog[4800] = {
+    version_string = "4.8",
+    release_date = "2026/03/09",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] The War Within Season 3 is now flagged as over. The [Mythic+ Teleports] button will show all available teleports until the next season starts, and a new [Hide button during off-season] option is available in the [Travel] module to allow hiding the button instead."
+        },
+        ["frFR"] = {
+            "[Retail] La saison 3 de War Within est désormais marquée comme terminée. Le bouton [Téléportations Mythique+] affichera toutes les téléportations disponibles jusqu'au début de la prochaine saison. Une nouvelle option [Masquer le bouton pendant l’entre-saison] est disponible dans le module [Voyage] pour cacher le bouton à la place."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] A new [Show unlearned teleports] option is now available (enabled by default) in the [Travel] module to list unlearned [Mythic+ Teleports].",
+            "[Global] It is now possible to right-click on the [Currency] button to open the [Currency] module options.",
+            "[Global] The [Game Menu] button now shows a tooltip that explains the different click actions available."
+        },
+        ["frFR"] = {
+            "[Retail] Une nouvelle option [Afficher les téléportations non apprises] est désormais disponible (activée par défaut) dans le module [Voyage] pour lister les [Téléportations Mythique+] non apprises.",
+            "[Global] Il est désormais possible de faire un clic droit sur le bouton [Monnaie] pour ouvrir les options du module [Monnaie].",
+            "[Global] Le bouton [Menu du jeu] affiche désormais une infobulle décrivant les différentes actions possibles selon le clic."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Global] The [Reputation] module tooltip now uses the Blizzard's global string for the Left-Button description.",
+        },
+        ["frFR"] = {
+            "[Global] La bulle d'aide du module [Réputation] utilise désormais la chaîne globale de Blizzard pour la description du clic gauche.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Global] Fixed LUA errors caused by the Guild [Message of the Day] in the [Micromenu] module.",
+            "[Global] Fixed LUA errors caused by the [Currency] module."
+        },
+        ["frFR"] = {
+            "[Global] Correction des erreurs LUA causées par le [Message du jour] de la guilde dans le module [Micromenu].",
+            "[Global] Correction des erreurs LUA causées par le module [Monnaie]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
