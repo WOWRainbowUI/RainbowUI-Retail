6d6c9377c16165760b703920f6c2116058524ac0
Dololo <class2u@users.noreply.github.com>
2026-03-19 10:17:03 +0800

    更新 地板傷害警報 6.1.1

--- AddOns/GTFO/Spells/GTFO_Fail_MN.lua
+++ AddOns/GTFO/Spells/GTFO_Fail_MN.lua
@@ -29,6 +29,50 @@ GTFO.SpellID["1257563"] = {
   --instance = 2858; 
 };
 
+--- *****************
+--- * The Voidspire *
+--- ******************
+
+GTFO.SpellID["1258883"] = {
+  --desc = "Void Fall (Imperator Averzian)";
+  sound = 3;
+  --instance = 2912; 
+  encounter = 3306;
+  applicationOnly = true;
+};
+
+GTFO.SpellID["1259186"] = {
+  --desc = "Blisterburst (Vorasius)";
+  sound = 3;
+  --instance = 2912; 
+  encounter = 3177;
+  applicationOnly = true;
+};
+
+GTFO.SpellID["1241844"] = {
+  --desc = "Smashed (Vorasius)";
+  sound = 3;
+  tankSound = 0;
+  --instance = 2912; 
+  encounter = 3177;
+  applicationOnly = true;
+};
+
+GTFO.SpellID["1264467"] = {
+  --desc = "Tail Lash (Vaelgor)";
+  sound = 3;
+  --instance = 2912; 
+  encounter = 3177;
+  applicationOnly = true;
+};
+
+GTFO.SpellID["1265152"] = {
+  --desc = "Impale (Ezzorak)";
+  sound = 3;
+  --instance = 2912; 
+  encounter = 3177;
+  applicationOnly = true;
+};
 
 end
 
--- AddOns/GTFO/Spells/GTFO_Spells_MN.lua
+++ AddOns/GTFO/Spells/GTFO_Spells_MN.lua
@@ -17,6 +17,157 @@ GTFO.SpellID["1225385"] = {
   --instance = 2858; 
 };
 
+GTFO.SpellID["1243988"] = {
+  --desc = "Binding Fissure (Lu'ashal);
+  sound = 1;
+  --instance = 0; 
+};
+
+--- **********************
+--- * Magister's Terrace *
+--- **********************
+
+GTFO.SpellID["1214089"] = {
+  --desc = "Arcane Residue (Arcanotron Custos)";
+  sound = 1;
+  instance = 2811; 
+};
+
+--- ******************
+--- * Voidscar Arena *
+--- ******************
+
+GTFO.SpellID["1248130"] = {
+  --desc = "Unstable Singularity (Overseer Charonus)";
+  sound = 1;
+  instance = 2923; 
+};
+
+--- ********************
+--- * Windrunner Spire *
+--- ********************
+
+GTFO.SpellID["472118"] = {
+  --desc = "Ignited Embers (Emberdawn)";
+  sound = 1;
+  instance = 2805; 
+  test = true;
+};
+
+GTFO.SpellID["468924"] = {
+  --desc = "Bladestorm (Commander Kroluk)";
+  sound = 1;
+  instance = 2805; 
+};
+
+GTFO.SpellID["472777"] = {
+  --desc = "Gunk Splatter (Latch)";
+  sound = 1;
+  instance = 2805; 
+};
+
+--- *****************
+--- * The Dreamrift *
+--- *****************
+
+
+GTFO.SpellID["1245919"] = {
+  --desc = "Alndust Essence (Chimaerus)";
+  sound = 1;
+  --instance = 2939;
+  encounter = 3306;
+};
+
+
+--- *****************
+--- * The Voidspire *
+--- ******************
+
+GTFO.SpellID["1284786"] = {
+  --desc = "Shadow Phalanx (Imperator Averzian)";
+  sound = 1;
+  --instance = 2912;
+  encounter = 3306;
+};
+
+GTFO.SpellID["1280101"] = {
+  --desc = "Void Breath (Vorasius)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3177;
+};
+
+GTFO.SpellID["1251213"] = {
+  --desc = "Twilight Spikes (Fallen-King Salhadaar)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3179;
+};
+
+GTFO.SpellID["1245592"] = {
+  --desc = "Torturous Extract (Fallen-King Salhadaar)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3179;
+};
+
+GTFO.SpellID["1260030"] = {
+  --desc = "Umbral Beams (Fallen-King Salhadaar)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3179;
+};
+
+GTFO.SpellID["1244672"] = {
+  --desc = "Nullzone (Vaelgor)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3178;
+};
+
+GTFO.SpellID["1245421"] = {
+  --desc = "Gloomfield (Ezzorak)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3178;
+};
+
+GTFO.SpellID["1276982"] = {
+  --desc = "Divine Consecration (General Amias Bellamy)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3180;
+};
+
+GTFO.SpellID["1246158"] = {
+  --desc = "Consecration (General Amias Bellamy)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3180;
+};
+
+GTFO.SpellID["1272324"] = {
+  --desc = "Divine Tempest (Commander Venel Lightblood)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3180;
+};
+
+GTFO.SpellID["1272324"] = {
+  --desc = "Void Remnants (Alleria Windrunner)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3181;
+};
+
+GTFO.SpellID["1238206"] = {
+  --desc = "Volatile Fissure (Alleria Windrunner)";
+  sound = 1;
+  --instance = 2912; 
+  encounter = 3181;
+};
+
+
 
 
 --- ***************************
