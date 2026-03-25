789ac5601be61647912fe21982fdf0eeee15d1a7
Dololo <class2u@users.noreply.github.com>
2026-03-25 09:50:20 +0800

    更新 任務對話 (羊皮紙) 1.0.3 c

--- AddOns/DialogueUI/Code/SupportedAddOns/Roleplay.lua
+++ AddOns/DialogueUI/Code/SupportedAddOns/Roleplay.lua
@@ -10,18 +10,23 @@ local function EnableUseRoleplayName()
 end
 
 
-do  --Total RP 3: RP Name in Quest Text
-    local ADDON_NAME = "tRP3_RPNameInQuests";
-    --https://www.curseforge.com/wow/addons/trp3-rpnameinquests
+do  --Eavesdropper
+    local ADDON_NAME = "Eavesdropper";
+    --https://www.curseforge.com/wow/addons/eavesdropper
 
     local function OnAddOnLoaded()
-        if TRP3_RPNameInQuests_CompleteRename then
-            local testRun = TRP3_RPNameInQuests_CompleteRename("Test");     --In case something went wrong
+        local questModifier = addon.API.GetGlobalObject("ED.QuestText.SubstitutePlayerPreferredName");
+        if questModifier then
+            local testRun = questModifier("Test");
             if testRun and testRun ~= "" then
                 INSTALLED_RP_ADDON = ADDON_NAME;
-                addon.SetDialogueTextModifier(TRP3_RPNameInQuests_CompleteRename);
+                addon.SetDialogueTextModifier(questModifier);
+                local chatModifier = addon.API.GetGlobalObject("ED.NPCDialogue.SubstitutePlayerPreferredName");
+                addon.SetChatTextModifier(chatModifier or questModifier);
                 EnableUseRoleplayName();
             end
+
+            addon.GetRoleplayName = addon.API.GetGlobalObject("ED.QuestText.GetPlayerPreferredName");
         end
     end
 
@@ -29,21 +34,20 @@ do  --Total RP 3: RP Name in Quest Text
 end
 
 
-do  --Eavesdropper
-    local ADDON_NAME = "Eavesdropper";
-    --https://www.curseforge.com/wow/addons/eavesdropper
+do  --Total RP 3: RP Name in Quest Text
+    local ADDON_NAME = "tRP3_RPNameInQuests";
+    --https://www.curseforge.com/wow/addons/trp3-rpnameinquests
 
     local function OnAddOnLoaded()
-        local func = addon.API.GetGlobalObject("ED.QuestText.SubstitutePlayerPreferredName");
-        if func then
-            local testRun = func("Test");
+        local questModifier = TRP3_RPNameInQuests_CompleteRename;
+        if questModifier then
+            local testRun = questModifier("Test");     --In case something went wrong
             if testRun and testRun ~= "" then
                 INSTALLED_RP_ADDON = ADDON_NAME;
-                addon.SetDialogueTextModifier(func);
+                addon.SetDialogueTextModifier(questModifier);
+                addon.SetChatTextModifier(questModifier);
                 EnableUseRoleplayName();
             end
-
-            addon.GetRoleplayName = addon.API.GetGlobalObject("ED.QuestText.GetPlayerPreferredName");
         end
     end
 
