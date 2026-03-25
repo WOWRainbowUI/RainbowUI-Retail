07ec2489f3c78742040d43911e37101dd7837277
Dololo <class2u@users.noreply.github.com>
2026-03-25 09:49:45 +0800

    更新 Details! 戰鬥統計 20260324.14811.171
    # Conflicts:
    #	AddOns/Details/Libs/DF/dropdown.lua

--- AddOns/Details/Libs/DF/dropdown.lua
+++ AddOns/Details/Libs/DF/dropdown.lua
@@ -702,7 +702,11 @@ function DropDownMetaFunctions:Selected(thisOption)
 		self.label:SetFont(thisOption.font, 14)
 
 	else
-		self.label:SetFont("GameFontHighlight", 14)
+		if select(4, GetBuildInfo()) >= 120005 then
+			self.label:SetFont(GameFontHighlightSmall:GetFont(), 14)
+		else
+			self.label:SetFont("GameFontHighlightSmall", 14)
+		end
 	end
 
 	self:SetValue(thisOption.value)
