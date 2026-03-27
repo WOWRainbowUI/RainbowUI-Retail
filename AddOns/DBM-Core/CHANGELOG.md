2571cbc2577ad1a99b697d025af5315cb3e60a09
Dololo <class2u@users.noreply.github.com>
2026-03-27 21:44:12 +0800

    更新 DBM 12.0.34

--- AddOns/DBM-Core/CHANGELOG.md
+++ AddOns/DBM-Core/CHANGELOG.md
@@ -1,32 +1,17 @@
 # DBM - Core
 
-## [12.0.33](https://github.com/DeadlyBossMods/DeadlyBossMods/tree/12.0.33) (2026-03-25)
-[Full Changelog](https://github.com/DeadlyBossMods/DeadlyBossMods/compare/12.0.32...12.0.33) [Previous Releases](https://github.com/DeadlyBossMods/DeadlyBossMods/releases)
+## [12.0.34](https://github.com/DeadlyBossMods/DeadlyBossMods/tree/12.0.34) (2026-03-26)
+[Full Changelog](https://github.com/DeadlyBossMods/DeadlyBossMods/compare/12.0.33...12.0.34) [Previous Releases](https://github.com/DeadlyBossMods/DeadlyBossMods/releases)
 
-- quick fix bad merge from RU update  
-- Update koKR (#1983)  
-    * Update koKR  
-- Update translations (#1985)  
-- Upd RU locale (#1984)  
-    * Create localization.ru.lua  
-    * Update DBM-Raids-Midnight\_Mainline.toc  
-    * Update commonlocal.ru.lua  
+- Update translations (#1987)  
+    * Update translations  
+    * Update translations  
 - prep new tag  
-- Preliminary hardcode for vanguard (sorry this wasn't done last week, i actually forgot to log it last week)  
-    Added a few renames and better option defaults too and pruned one alert that never came to fruition  
-- Fix double soak warning protection not resetting after each cycle, causing the fix to only work for first time bug occurs, not each additional time.  
-- Use precise passthrough on rest too  
-- reapply some fixes that got lost due to merge conflicts being resolved incorrectly to crown (such as wrong spellname for hand and some sound updates that got lost)  
-    Change crown to no longer pass through unknown timers when they occur and instead just full fallback to non hardcoded api if unexpected results occur so it doesn't end up in a state where it just starts resolving entirely wrong once any context becomes desynced.  
-    Also changed overly complicated stage 2 logic to be more direct simple count/event order logic.  
-    always use exact timer for crown and vaelgor instead of rounded timer for start. rounded should only be used for context logic.  
-- Further update Double dragon hardcode with routing updates based on greater data  
-- fix last  
-- improve debug so shortnames don't have to be disabled when doing timeline debugging and spellname matching  
-- Redo logic on Vorasius to better handle alternating slam timer spellids  
-- Refactor double dragons to account for ~1 second drift and rakfang and Vaelwing now always being cast in stage 1 and 2 rather than only being cast by one of dragons.  
-- Fix work around for timer bug on Averzian  
-    Add timer workaround for Vorasius  
-    Of course, it'd be nice if blizzard knew what they were doing and could actually start valid timers for their bosses instead.  
-- NA vs british english  
+- Also fix and close https://github.com/DeadlyBossMods/DeadlyBossMods/issues/1988  
+- fix another bug and improve debug  
+- Fix bug with hardcoded timers not firing pause/resume and stop callbacks  
+- missed default  
+- Add option to explictely disable timeline/warning override colors. Closes https://github.com/DeadlyBossMods/DeadlyBossMods/issues/1986  
+- fix varaible names  
+- Tighten Averzian fix  
 - bump alpha  
--- AddOns/DBM-Core/DBM-Core.lua
+++ AddOns/DBM-Core/DBM-Core.lua
@@ -77,16 +77,16 @@ local function showRealDate(curseDate)
 	end
 end
 
-DBM.Revision = parseCurseDate("20260325092947")
+DBM.Revision = parseCurseDate("20260326092056")
 DBM.TaintedByTests = false -- Tests may mess with some internal state, you probably don't want to rely on DBM for an important boss fight after running it in test mode
 
 private.fakeBWVersion, private.fakeBWHash = 407, "a0f5bf5"--407.0
 
 -- The string that is shown as version
-DBM.DisplayVersion = "12.0.33"--Core version
+DBM.DisplayVersion = "12.0.34"--Core version
 DBM.classicSubVersion = 0
 DBM.dungeonSubVersion = 0
-DBM.ReleaseRevision = releaseDate(2026, 3, 24) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+DBM.ReleaseRevision = releaseDate(2026, 3, 25) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
 -- support for github downloads, which doesn't support curse keyword expansion
@@ -135,7 +135,7 @@ DBM.DefaultOptions = {
 	CountdownVoice3 = "Smooth",
 	CountSize = 5,
 	PullVoice = "Corsica",
-	ChosenVoicePack2 = (GetLocale() == "enUS" or GetLocale() == "enGB") and "VEM" or "None",
+	ChosenVoicePack2 = (GetLocale() == "enUS" or GetLocale() == "enGB") and "VEM" or "Saha",
 	VPReplacesAnnounce = true,
 	VPReplacesSADefault = true,
 	EventSoundVictory2 = "Interface\\AddOns\\DBM-Core\\sounds\\Victory\\SmoothMcGroove_Fanfare.ogg",
@@ -370,6 +370,7 @@ DBM.DefaultOptions = {
 	CastNPIconGlowBehavior = 1,
 	CastNPIconGlowType2 = 4,--Button Default
 	DontPlayCountdowns = false,
+	DontSetTimelineColors = false,
 	DontSendYells = false,
 	BlockNoteShare = false,
 	DontAutoGossip = false,
@@ -1789,11 +1790,13 @@ do
 			if self:IsPostMidnight() then
 				C_CVar.SetCVar("encounterTimelineShowSequenceCount", "1")--Enable count on timers
 				C_EncounterWarnings.SetPlayCustomSoundsWhenHidden(true)--Allows DBM sounds to play even when blizzard frames aren't shown
-				--Apply user bar color to all bars by default, since blizzard applies white (or red) to all of them by default now
-				local timerRed, timerGreen, timerBlue = DBT:GetColorForType(0)
-				--https://wago.tools/db2/EncounterEvent?page=25
-				for i = 1, 658 do
-					C_EncounterEvents.SetEventColor(i, {r = timerRed, g = timerGreen, b = timerBlue})
+				if not self.Options.DontSetTimelineColors then
+					--Apply user bar color to all bars by default, since blizzard applies white (or red) to all of them by default now
+					local timerRed, timerGreen, timerBlue = DBT:GetColorForType(0)
+					--https://wago.tools/db2/EncounterEvent?page=25
+					for i = 1, 733 do
+						C_EncounterEvents.SetEventColor(i, {r = timerRed, g = timerGreen, b = timerBlue})
+					end
 				end
 				if self.Options.HideBossEmoteFrame2 then
 					C_EncounterWarnings.SetWarningsShown(false)
@@ -7982,7 +7985,7 @@ function bossModPrototype:ReceiveSync(event, sender, revision, ...)
 	end
 end
 
----@param revision number|string Either a number in the format "202101010000" (year, month, day, hour, minute) or string "20260325092235" to be auto set by packager
+---@param revision number|string Either a number in the format "202101010000" (year, month, day, hour, minute) or string "20260326092009" to be auto set by packager
 function bossModPrototype:SetRevision(revision)
 	revision = parseCurseDate(revision or "")
 	if not revision or type(revision) == "string" then
