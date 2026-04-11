6a4cbca44fb214390c91be2fba9d8a823e2d7538
Dololo <class2u@users.noreply.github.com>
2026-04-11 20:22:37 +0800

    更新 稀有怪獸與牠們的產地 v2026.19.2

--- AddOns/SilverDragon/outputs/ldb.lua
+++ AddOns/SilverDragon/outputs/ldb.lua
@@ -474,7 +474,7 @@ do
 				else
 					lootwindow:SetPoint("TOPLEFT", cell, "BOTTOMRIGHT")
 				end
-				lootwindow:SetAutoHideDelay(0.25, cell, cleanup_lootwindow)
+				lootwindow:SetAutoHideDelay(0.25, {cell}, cleanup_lootwindow)
 			end
 		end
 		function click_items_tooltip(cell, mobid)
