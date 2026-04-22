bba75bdec4d04ef02577a9bae9c02cbabcdfc4ff
Dololo <class2u@users.noreply.github.com>
2026-04-22 08:06:06 +0800

    更新 多角色背包 797
    # Conflicts:
    #	AddOns/Baganator/Baganator.toc

--- AddOns/Baganator/API/ItemButton.lua
+++ AddOns/Baganator/API/ItemButton.lua
@@ -339,7 +339,13 @@ addonTable.Utilities.OnAddonLoaded("CanIMogIt", function()
       CIMI_SetIcon(self, CIMI_Update, CanIMogIt:GetTooltipText(details.itemLink, details.itemLocation and details.itemLocation.bagID, details.itemLocation and details.itemLocation.slotIndex))
     end
     CIMI_SetIcon(CIMIOverlay, CIMI_Update, CanIMogIt:GetTooltipText(details.itemLink, details.itemLocation and details.itemLocation.bagID, details.itemLocation and details.itemLocation.slotIndex))
-    return (IsEquipment(details.itemLink) or (C_ToyBox ~= nil and C_ToyBox.GetToyInfo(details.itemID) ~= nil) or IsPet(details.itemID) or (C_MountJournal ~= nil and C_MountJournal.GetMountFromItem(details.itemID) ~= nil) or (C_Item.IsDecorItem ~= nil and C_Item.IsDecorItem(details.itemID)))
+    return (
+         IsEquipment(details.itemLink)
+      or (C_ToyBox ~= nil and C_ToyBox.GetToyInfo(details.itemID) ~= nil)
+      or IsPet(details.itemID)
+      or (C_MountJournal ~= nil and C_MountJournal.GetMountFromItem(details.itemID) ~= nil)
+      or (C_Item.IsDecorItem ~= nil and C_Item.IsDecorItem(details.itemID))
+      or CanIMogIt:IsItemEnsemble(details.itemLink))
   end,
   function(itemButton)
     CIMI_AddToFrame(itemButton, function() end)
--- AddOns/Baganator/CHANGELOG.md
+++ AddOns/Baganator/CHANGELOG.md
@@ -1,9 +1,7 @@
 # Baganator
 
-## [796](https://github.com/TheMouseNest/Baganator/tree/796) (2026-04-08)
-[Full Changelog](https://github.com/TheMouseNest/Baganator/compare/795...796) 
+## [797](https://github.com/TheMouseNest/Baganator/tree/797) (2026-04-21)
+[Full Changelog](https://github.com/TheMouseNest/Baganator/compare/796...797) 
 
-- Fix guild bank item auto ungrouping not applying properly on guild bank close  
-- Support spaces in `/bgr search [query]` slash command  
-- Keep analytics gone  
-- Restore Wago to build script  
+- Update toc for 12.0.5  
+- Update CIMI integration to show icon on ensembles  
