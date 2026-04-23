3944270b75e28b1b342b2830cdd4d89f2fac97af
Dololo <class2u@users.noreply.github.com>
2026-04-23 10:09:19 +0800

    更新 Details! 傷害統計 20260422.15002.171

--- AddOns/Details/CHANGELOG.md
+++ AddOns/Details/CHANGELOG.md
@@ -1,20 +1,6 @@
 # Details! Damage Meter
 
-## [Details.20260411.14930.171](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.20260411.14930.171) (2026-04-11)
-[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.20260408.14900.171...Details.20260411.14930.171) 
+## [Details.20260422.15002.171](https://github.com/Tercioo/Details-Damage-Meter/tree/Details.20260422.15002.171) (2026-04-22)
+[Full Changelog](https://github.com/Tercioo/Details-Damage-Meter/compare/Details.20260422.15001.171...Details.20260422.15002.171) 
 
-- Version Up - Change log in the description  
-    - Fix error when hovering over a spec icon while in a boss fight.  
-    - Fix Potion Usage display showing secret errors.  
-    - Fix Left Text padding which wasn't working in retail.  
-    - Patch 12.0.5 preparations.  
-    - Add Wago API.  
-- Wago API  
-- Wago API  
-- Wago API  
-- Fix player name Y offset  
-- Fix tooltip icon and potion usage secret while in combat  
-- PTR 12.0.5 fix  
-- Bug fix  
-- Boot.lua update  
-- PTR 12.0.5, use Ambiguate for player name.  
+- Fix icon tooltips  
--- AddOns/Details/functions/profiles.lua
+++ AddOns/Details/functions/profiles.lua
@@ -1189,6 +1189,7 @@ local default_profile = {
 
 Details.default_profile = default_profile
 
+
 -- aqui fica as propriedades do jogador que n�o ser�o armazenadas no profile
 local default_player_data = {
 		coach = {
@@ -1196,6 +1197,8 @@ local default_player_data = {
 			welcome_panel_pos = {},
 			last_coach_name = false,
 		},
+
+		apocalypse_savedsegments = {},
 
 		arena_data_headers = {},
 		arena_data_compressed = {}, --store data for arena the character did
@@ -1394,6 +1397,7 @@ local default_global_data = {
 
 	--profile pool
 		__profiles = {},
+		__char_profiles = {}, --table<name or guid> = profile name
 		latest_news_saw = "",
 		always_use_profile = false,
 		always_use_profile_name = "",
@@ -1926,7 +1930,7 @@ function Details:RestoreState_CurrentMythicDungeonRun()
 				print("D! (debug) mythic level isn't equal.", mythicLevel, savedTable.level)
 			end
 		else
-			print("D! (debug) zone name or zone Id isn't the same:", zoneName, savedTable.dungeon_name, currentZoneID, savedTable.dungeon_zone_id)
+			--print("D! (debug) zone name or zone Id isn't the same:", zoneName, savedTable.dungeon_name, currentZoneID, savedTable.dungeon_zone_id)
 		end
 
 		--mythic run is over
@@ -2150,7 +2154,8 @@ function Details:ImportProfile (profileString, newProfileName, bImportAutoRunCod
 		--transfer instance data to the new created profile
 		profileObject.instances = DetailsFramework.table.copy({}, profileData.instances)
 
-		Details:ApplyProfile (newProfileName)
+		local shouldSkipSave = overwriteExisting and Details:GetCurrentProfileName() == newProfileName
+		Details:ApplyProfile (newProfileName, shouldSkipSave)
 
 		--reset automation settings (due to user not knowing why some windows are disappearing)
 		for instanceId, instance in Details:ListInstances() do
