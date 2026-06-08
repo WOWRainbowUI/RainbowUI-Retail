c1fad4395aae5310e73c003006ce895635a17368
Dololo <class2u@users.noreply.github.com>
2026-06-08 20:20:35 +0800

    更新 按鈕外觀-內建快捷列 12.0.7.0

---
+++ AddOns/MasqueBlizzBars/Locales/koKR.lua
@@ -0,0 +1,46 @@
+--
+-- Masque Blizzard Bars
+--
+-- Locales\koKR.lua -- ruRU Localization File
+--
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file or at
+-- https://opensource.org/licenses/MIT.
+--
+
+-- Please use CurseForge to submit localization content for another language:
+-- https://www.curseforge.com/wow/addons/masque-blizz-bars-revived/localization
+
+-- luacheck: no max line length
+
+local Locale = GetLocale()
+if Locale ~= "koKR" then return end
+
+local _, Shared = ...
+local L = Shared.Locale
+
+L = L or {}
+L["Action Bar 1"] = "액션 바 1"
+L["Action Bar 2"] = "액션 바 2"
+L["Action Bar 3"] = "액션 바 3"
+L["Action Bar 4"] = "액션 바 4"
+L["Action Bar 5"] = "액션 바 5"
+L["Action Bar 6"] = "액션 바 6"
+L["Action Bar 7"] = "액션 바 7"
+L["Action Bar 8"] = "액션 바 8"
+L["Essential Cooldowns"] = "필수 쿨기"
+L["Extra Ability Buttons"] = "엑스트라 버튼"
+L["Pet Bar"] = "소환수 액션 바"
+L["Pet Battle Bar"] = "애완동물 대전 바"
+L["Possess Bar"] = "정신 지배 액션 바"
+L["Spell Flyouts"] = "팝업 액션 바"
+L["Stance Bar"] = "태세 액션 바"
+L["Tracked Bars"] = "추적된 액션 바"
+L["Tracked Buffs"] = "추적된 버프"
+L["Utility Cooldowns"] = "유틸기"
+L["Vehicle Bar"] = "차량 액션 바"
+
+L["NOTES_EXTRA_ABILITY_BUTTONS"] = "이 그룹은 보스전, 퀘스트 도중에 표시되는 엑스트라 액션 버튼과 특정 지역에 쓰이는 모든 지역 능력 버튼을 포함하고 있습니다. 일부 버튼은 배경 이미지를 추가로 보유하고 있으므로 사각형 스킨일 때 제대로 표시됩니다."
+L["NOTES_SPELL_FLYOUTS"] = "이 그룹은 게임 모든 곳에서 표시되는 돌출 버튼을 포함하고 있으며 액션 바나 마법책 등이 해당됩니다."
+L["NOTES_VEHICLE_BAR"] = "이 액션 바는 스킬을 사용하는 차량에 탑승하면 표시됩니다. 나가기 버튼은 현재 스킨을 입힐 수 없습니다."
+
