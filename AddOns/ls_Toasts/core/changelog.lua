3a7ac2723f0bbe46e17164dfab6dfcf5f941cce6
Dololo <class2u@users.noreply.github.com>
2026-07-03 08:48:58 +0800

    更新 通知面板增強 120007.01
    # Conflicts:
    #	AddOns/ls_Toasts/ls_Toasts.toc

--- AddOns/ls_Toasts/core/changelog.lua
+++ AddOns/ls_Toasts/core/changelog.lua
@@ -6,5 +6,10 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Fixed an issue where profile import/export leaked into other addons. TIL.
+- Added 12.0.7 support.
+- Updated Traditional Chinese translation. Translated by BNS333@Curse.
+
+### Loot (Currency)
+
+- Fixed the Ritual Site scoreboard spam.
 ]]
