466ef17cc36cb48ff89dd75741c7bb24c9404a5d
Dololo <class2u@users.noreply.github.com>
2026-08-18 20:45:44 +0800

    更新 編輯模式擴充包 12.1-007

--- AddOns/EditModeExpanded/FrameHandlers/TargetFrame.lua
+++ AddOns/EditModeExpanded/FrameHandlers/TargetFrame.lua
@@ -39,11 +39,7 @@ function addon:initTargetFrame()
         
         local realFrame = TargetFrameSpellBar
         
-        
-        -- TODO: pass fake frame settings down to the real target cast bar
-        
-        --lib:RegisterResizable(fakeFrame, 10, 500)
-        --lib:RegisterHideable(fakeFrame)
+        addon:registerSecureFrameHideable(fakeFrame)
         
         local width, height
         lib:RegisterSlider(fakeFrame, HUD_EDIT_MODE_SETTING_CHAT_FRAME_WIDTH, "Width",
@@ -81,8 +77,6 @@ function addon:initTargetFrame()
     end
     
     if db.EMEOptions.targetFrameBuffs then
-            
-        --addon:registerFrame(TargetFrame:GetAuraContainer(), "Target Buffs", db.TargetBuffs)
         local fakeFrame = CreateFrame("Frame", "EMETargetFrameBuffs", TargetFrame)
         fakeFrame:SetPoint("TOPLEFT", TargetFrame, "BOTTOMLEFT", 5, -10)
         fakeFrame:SetSize(100, 70)
@@ -96,5 +90,7 @@ function addon:initTargetFrame()
             realFrame:ClearAllPoints()
             realFrame:SetPoint("TOPLEFT", fakeFrame, "TOPLEFT")
         end)
+        
+        addon:registerSecureFrameHideable(fakeFrame)
     end
 end
