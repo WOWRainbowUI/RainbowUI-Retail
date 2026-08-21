6cd588505170a1e210c863cbf1ca002f7f27b05e
Dololo <class2u@users.noreply.github.com>
2026-08-21 21:36:14 +0800

    移除 至暗之夜頭像

--- AddOns/MidnightSimpleUnitFrames/Shell/EditMode/MSUF_EditMode_HUD.lua
+++
@@ -1,3 +0,0 @@
---- EditMode/MSUF_EditMode_HUD.lua - loader for shared HUD implementation.
-local install = _G.MSUF_InstallEditModeHUD
-if type(install) == "function" then install(...) end
--- AddOns/MidnightSimpleUnitFrames/Shell/EditMode/MSUF_EditMode_Layout.lua
+++
@@ -1,3 +0,0 @@
---- EditMode/MSUF_EditMode_Layout.lua - loader for shared layout implementation.
-local install = _G.MSUF_InstallEditLayoutUI
-if type(install) == "function" then install(...) end
