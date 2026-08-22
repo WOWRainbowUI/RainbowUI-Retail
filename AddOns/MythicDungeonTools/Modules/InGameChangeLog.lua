d28883395fa86b73ab4bb09211102b7c6a3d46dd
Dololo <class2u@users.noreply.github.com>
2026-08-22 20:26:20 +0800

    更新 M+地城規劃工具  6.2.6
    # Conflicts:
    #	AddOns/MythicDungeonTools/Locales/esES.lua
    #	AddOns/MythicDungeonTools/Locales/esMX.lua

--- AddOns/MythicDungeonTools/Modules/InGameChangeLog.lua
+++ AddOns/MythicDungeonTools/Modules/InGameChangeLog.lua
@@ -2,26 +2,30 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
+    tag = "6.2.6",
+    date = "2026-08-22",
+    notes = {
+      "Corrected Temple of Sethraliss's total enemy forces from 689 to 649.",
+    },
+  },
+  {
+    tag = "6.2.5",
+    date = "2026-08-21",
+    notes = {
+      "Corrected enemy groupings, names, boss data, and map positions multiple dungeons",
+      "Corrected Temple of Sethraliss's total enemy forces from 649 to 689.",
+      "Corrected enemy health values",
+      "Added / corrected dungeon entrance markers",
+      "Corrected Spanish dungeon short names",
+      "Automatic combat logging no longer posts start and stop messages in chat.",
+      "Selecting no Focus Marker no longer clears an existing target marker when \"Don't overwrite existing target markers\" is enabled.",
+    },
+  },
+  {
     tag = "6.2.4",
     date = "2026-08-18",
     notes = {
       "Corrected Murder Row's total enemy forces from 690 to 655.",
-    },
-  },
-  {
-    tag = "6.2.3",
-    date = "2026-08-17",
-    notes = {
-      "Improved map loading and responsiveness when displaying large numbers of enemies.",
-      "Corrected enemy positions and icon sizes throughout The Blinding Vale.",
-      "Bosses in The Blinding Vale no longer contribute enemy forces.",
-    },
-  },
-  {
-    tag = "6.2.2",
-    date = "2026-08-14",
-    notes = {
-      "Corrected enemy data and map positions for Altar of Fangs, Den of Nalorakk, Murder Row, and The Blinding Vale.",
     },
   },
 }
