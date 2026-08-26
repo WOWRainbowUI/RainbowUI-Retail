0c6c2d512091a6aa48c6cb8c260aae9072d1cfb6
Dololo <class2u@users.noreply.github.com>
2026-08-26 10:19:49 +0800

    更新 M+地城規劃工具 6.2.9

--- AddOns/MythicDungeonTools/Modules/InGameChangeLog.lua
+++ AddOns/MythicDungeonTools/Modules/InGameChangeLog.lua
@@ -2,30 +2,29 @@ local _, MDT = ...
 
 MDT.changeLog = {
   {
-    tag = "6.2.6",
+    tag = "6.2.9",
+    date = "2026-08-25",
+    notes = {
+      "Corrected total enemy forces in Temple of Sethraliss from 689 to 687 and Ruby Life Pools from 553 to 551.",
+      "Corrected The Blinding Vale's total enemy forces from 655 to 686.",
+      "Corrected the Potatoad Matriarch's enemy forces from 30 to 60.",
+    },
+  },
+  {
+    tag = "6.2.8",
+    date = "2026-08-25",
+    notes = {
+      "Corrected enemy map positions in The Blinding Vale, Den of Nalorakk, Murder Row, Altar of Fangs, and Voidscar Arena.",
+      "Corrected pull groupings and floor assignments in The Blinding Vale, Den of Nalorakk, Murder Row, and Voidscar Arena.",
+      "Added missing enemy placements in The Blinding Vale, Den of Nalorakk, and Murder Row.",
+      "Corrected the models for Ula'tek's Chosen, Twinfang Harrower, and Blade of the Altar in Altar of Fangs.",
+    },
+  },
+  {
+    tag = "6.2.7",
     date = "2026-08-22",
     notes = {
-      "Corrected Temple of Sethraliss's total enemy forces from 689 to 649.",
-    },
-  },
-  {
-    tag = "6.2.5",
-    date = "2026-08-21",
-    notes = {
-      "Corrected enemy groupings, names, boss data, and map positions multiple dungeons",
       "Corrected Temple of Sethraliss's total enemy forces from 649 to 689.",
-      "Corrected enemy health values",
-      "Added / corrected dungeon entrance markers",
-      "Corrected Spanish dungeon short names",
-      "Automatic combat logging no longer posts start and stop messages in chat.",
-      "Selecting no Focus Marker no longer clears an existing target marker when \"Don't overwrite existing target markers\" is enabled.",
-    },
-  },
-  {
-    tag = "6.2.4",
-    date = "2026-08-18",
-    notes = {
-      "Corrected Murder Row's total enemy forces from 690 to 655.",
     },
   },
 }
