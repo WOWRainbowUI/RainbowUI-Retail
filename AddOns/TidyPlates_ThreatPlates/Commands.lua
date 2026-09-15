66b706af01356839b0a0fbec063f4b5fe3503e3e
Dololo <class2u@users.noreply.github.com>
2026-09-15 09:57:29 +0800

    更新 威力血條 13.2.0
    # Conflicts:
    #	AddOns/TidyPlates_ThreatPlates/Locales/deDE.lua
    #	AddOns/TidyPlates_ThreatPlates/Locales/esES.lua
    #	AddOns/TidyPlates_ThreatPlates/Locales/esMX.lua

--- AddOns/TidyPlates_ThreatPlates/Commands.lua
+++ AddOns/TidyPlates_ThreatPlates/Commands.lua
@@ -6,7 +6,7 @@
 local L = Addon.L
 local GetSpecialization = C_SpecializationInfo and C_SpecializationInfo.GetSpecialization or _G.GetSpecialization
 
-Addon.DEBUG = Addon.Meta("version") == "13.1.1"
+Addon.DEBUG = Addon.Meta("version") == "13.2.0"
 
 local function toggleDPS()
   if Addon.db.profile.optionRoleDetectionAutomatic then
