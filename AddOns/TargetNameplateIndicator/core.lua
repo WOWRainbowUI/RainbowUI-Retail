0f0797b6c3ed030e82e2e37b7a6438bb818b8c2e
Dololo <class2u@users.noreply.github.com>
2026-09-15 09:57:04 +0800

    更新 目標指示箭頭 1.67
    # Conflicts:
    #	AddOns/TargetNameplateIndicator/Libs/AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceDB-3.0/AceDB-3.0.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceDBOptions-3.0/AceDBOptions-3.0.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceGUI-3.0/widgets/AceGUIWidget-CheckBox.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceGUI-3.0/widgets/AceGUIWidget-Heading.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceGUI-3.0/widgets/AceGUIWidget-Icon.lua
    #	AddOns/TargetNameplateIndicator/Libs/AceGUI-3.0/widgets/AceGUIWidget-Label.lua
    #	AddOns/TargetNameplateIndicator/Locales/deDE.lua
    #	AddOns/TargetNameplateIndicator/Locales/esES.lua
    #	AddOns/TargetNameplateIndicator/Locales/esMX.lua
    #	AddOns/TargetNameplateIndicator/Locales/frFR.lua
    #	AddOns/TargetNameplateIndicator/Locales/koKR.lua
    #	AddOns/TargetNameplateIndicator/Locales/ptBR.lua
    #	AddOns/TargetNameplateIndicator/Locales/ruRU.lua
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator.toc
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator_Cata.toc
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator_Mists.toc
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator_TBC.toc
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator_Vanilla.toc
    #	AddOns/TargetNameplateIndicator/TargetNameplateIndicator_Wrath.toc
    #	AddOns/TargetNameplateIndicator/core.lua

--- AddOns/TargetNameplateIndicator/core.lua
+++ AddOns/TargetNameplateIndicator/core.lua
@@ -19,6 +19,12 @@ local function debugprint(...)
 end
 
 _G.TNI = TNI
+
+local changeThrottleUnits = {
+	mouseover = true,
+	softenemy = true,
+	softfriend = true,
+}
 --@end-alpha@]=]
 
 
@@ -67,161 +73,28 @@ do
 		}
 	end
 
-	local function CreateUnitDefaults()
+	---
+	---@param includeSelf? boolean
+	---@param includeFriendly? boolean
+	---@param includeHostile? boolean
+	---@return table
+	local function CreateUnitDefaults(includeSelf, includeFriendly, includeHostile)
 		return {
 			enable = true,
-			self = CreateUnitReactionTypeDefaults(),
-			friendly = CreateUnitReactionTypeDefaults(),
-			hostile = CreateUnitReactionTypeDefaults(),
+			self = includeSelf ~= false and CreateUnitReactionTypeDefaults() or nil,
+			friendly = includeFriendly ~= false and CreateUnitReactionTypeDefaults() or nil,
+			hostile = includeHostile ~= false and CreateUnitReactionTypeDefaults() or nil,
 		}
 	end
 
 	defaults = {
 		profile = {
-			target = {
-				enable = true,
-				self = {
-					enable = false,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\NeonWhiteArrow",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -5,
-				},
-				friendly = {
-					enable = false,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\NeonGreenArrow",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -5,
-				},
-				hostile = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\NeonRedArrow",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = 30,
-				}
-			},
-			mouseover = {
-				enable = true,
-				self = {
-					enable = false,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\whitearrow1",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -10,
-				},
-				friendly = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\greenarrow1",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -10,
-				},
-				hostile = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\redarrow1",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = 5,
-				}
-			},
-			focus = {
-				enable = true,
-				self = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\Q_WhiteTarget",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -10,
-				},
-				friendly = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\Q_GreenTarget",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -12,
-				},
-				hostile = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\Q_RedTarget",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = 16,
-				}
-			},
-			targettarget = {
-				enable = true,
-				self = {
-					enable = false,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\whitearrow1",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -10,
-				},
-				friendly = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\bluearrow1",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = -10,
-				},
-				hostile = {
-					enable = true,
-					texture = "Interface\\AddOns\\TargetNameplateIndicator\\Textures\\PurpleArrow",
-					height = 70,
-					width = 70,
-					opacity = 1,
-					texturePoint = "BOTTOM",
-					anchorPoint = "TOP",
-					xOffset = 0,
-					yOffset = 5,
-				}
-			}
+			target = CreateUnitDefaults(),
+			softenemy = CreateUnitDefaults(false, false, true),
+			softfriend = CreateUnitDefaults(true, true, false),
+			mouseover = CreateUnitDefaults(),
+			focus = CreateUnitDefaults(),
+			targettarget = CreateUnitDefaults(),
 		}
 	}
 end
@@ -334,19 +207,19 @@ end
 -- - If a lower priority indicator is displaying, it is hidden and this returns true.
 -- - If an equal or higher priority indicator is displaying, this returns false.
 function Indicator:CheckAndHideLowerPriorityIndicators()
+	local shouldDisplay = true
+
 	for unit, indicator in pairs(TNI.Indicators) do
 		if indicator.enabled and self.unit ~= indicator.unit and UnitIsUnit(self.unit, unit) then -- If the indicator is for a different unit token but it's the same unit,
-			if self.priority > indicator.priority then                                      -- If this indicator is a higher priority, hide the other indicator and return true
+			if self.priority > indicator.priority then                                      -- If this indicator is a higher priority, hide the other indicator
 				indicator:Update()
-				return true
-			else -- If this indicator is a lower or equal priority, return false
-				return false
+			else                                                                            -- If this indicator is a lower or equal priority, hide it
+				shouldDisplay = false
 			end
 		end
 	end
 
-	-- No other indicator is displaying, return true
-	return true
+	return shouldDisplay
 end
 
 local GetNamePlateUnit
@@ -450,7 +323,17 @@ function NonTargetIndicator:OnUpdate()
 	local shouldDisplay = self:CheckAndHideLowerPriorityIndicators()
 
 	--[=[@alpha@
-	if self.unit ~= "mouseover" then
+	local shouldPrint = true
+	if changeThrottleUnits[self.unit] then
+		local currentTime = GetTime()
+		if not self.lastChangeTime or (currentTime - self.lastChangeTime) > 30 then
+			self.lastChangeTime = currentTime
+		else
+			shouldPrint = false
+		end
+	end
+
+	if shouldPrint then
 		debugprint(self.unit, "changed", nameplate, "shouldDisplay?", shouldDisplay)
 	end
 	--@end-alpha@]=]
@@ -543,6 +426,17 @@ TargetIndicator:LNR_RegisterCallback("LNR_ON_TARGET_PLATE_ON_SCREEN", "OnTargetP
 
 ---@diagnostic disable-next-line: unused-local
 local MouseoverIndicator = CreateNonTargetIndicator("mouseover", 10)
+
+
+------
+-- Soft Target Indicators
+------
+
+---@diagnostic disable-next-line: unused-local
+local SoftEnemyIndicator = CreateNonTargetIndicator("softenemy", 70)
+
+---@diagnostic disable-next-line: unused-local
+local SoftFriendIndicator = CreateNonTargetIndicator("softfriend", 80)
 
 
 ------
